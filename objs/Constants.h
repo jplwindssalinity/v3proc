1e839d8ed44cd4b1e075c0cd14e3f9c3bcca2185
hudd <>
1997-03-19 02:15:31 +0000

    moved constant values into the Constants.h

--- objs/Constants.h
+++ objs/Constants.h
@@ -9,15 +9,17 @@
 static const char rcs_id_constants_h[] =
 	"@(#) $Id$";
 
+#include <math.h>
+
 //======================================================================
 // DESCRIPTION
 //		A bunch of "universal" constants.
 //======================================================================
 
-extern const double pi;
-extern const double two_pi;
-extern const double dtr;
-extern const double rtd;
-extern const double rpm_to_radps;
+static const double pi = M_PI;
+static const double two_pi = 2.0 * M_PI;
+static const double dtr = 1.745329252e-2;
+static const double rtd = 5.729577951e1;
+static const double rpm_to_radps = M_PI / 30.0;
 
 #endif
