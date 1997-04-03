a7d09f6e1cc2b1a2660b83a0c28ea541cbd2543e
hudd <>
1997-04-03 00:50:26 +0000

    latest version

--- programs/gmf.C
+++ programs/gmf.C
@@ -75,6 +75,8 @@ static const char rcs_id[] =
 
 #define INC_STEP		2
 
+#define RTD			(180.0 / M_PI)
+
 //--------//
 // MACROS //
 //--------//
@@ -88,6 +90,8 @@ static const char rcs_id[] =
 //-----------------------//
 
 int term_vs_inc(const char* command, const char* gmf_file,
+	double term[POLS][INCS][SPDS], char* name);
+int term_vs_spd(const char* command, const char* gmf_file,
 	double term[POLS][INCS][SPDS], char* name);
 
 //------------------//
@@ -118,7 +122,7 @@ main(
 
 	const char* command = no_path(argv[0]);
 
-	if (argc == 0)
+	if (argc == 1)
 		usage(command, usage_array, 1);
 
 	int c;
@@ -162,7 +166,9 @@ main(
 
 	double a0[POLS][INCS][SPDS];
 	double a1[POLS][INCS][SPDS];
+	double a1_phase[POLS][INCS][SPDS];
 	double a2[POLS][INCS][SPDS];
+	double a2_phase[POLS][INCS][SPDS];
 
 	//-----------------//
 	// read in the gmf //
@@ -190,11 +196,20 @@ main(
 				for (int spd = 1; spd < SPDS; spd += 1)
 				{
 					double dspd = (double)spd;
-					gmf.GetCoefs(pol, dinc, dspd, &(a0[pol][inc][spd]),
-						&(a1[pol][inc][spd]), &(a2[pol][inc][spd]));
-					a0[pol][inc][spd] = 10.0 * log10(a0[pol][inc][spd]);
-					a1[pol][inc][spd] = 10.0 * log10(a1[pol][inc][spd]);
-					a2[pol][inc][spd] = 10.0 * log10(a2[pol][inc][spd]);
+					double A0, A1, A1_phase, A2, A2_phase;
+					gmf.GetCoefs(pol, dinc, dspd, &A0, &A1, &A1_phase, &A2,
+						&A2_phase);
+					a0[pol][inc][spd] = A0;
+					a1[pol][inc][spd] = A1 / A0;
+					a1_phase[pol][inc][spd] = fmod(A1_phase * RTD + 360.0,
+						360.0);
+					if (a1_phase[pol][inc][spd] > 270.0)
+						a1_phase[pol][inc][spd] -= 360.0;
+					a2[pol][inc][spd] = A2 / A0;
+					a2_phase[pol][inc][spd] = fmod(A2_phase * RTD + 360.0,
+						360.0);
+					if (a2_phase[pol][inc][spd] > 270.0)
+						a2_phase[pol][inc][spd] -= 360.0;
 				}
 			}
 		}
@@ -216,7 +231,9 @@ main(
 		int size = POLS * INCS * SPDS * sizeof(double);
 		if (read(ifd, a0, size) != size ||
 			read(ifd, a1, size) != size ||
-			read(ifd, a2, size) != size)
+			read(ifd, a1_phase, size) != size ||
+			read(ifd, a2, size) != size ||
+			read(ifd, a2_phase, size) != size)
 		{
 			fprintf(stderr, "%s: error reading coefficient file %s\n",
 				command, read_coef);
@@ -241,7 +258,9 @@ main(
 		int size = POLS * INCS * SPDS * sizeof(double);
 		if (write(ofd, a0, size) != size ||
 			write(ofd, a1, size) != size ||
-			write(ofd, a2, size) != size)
+			write(ofd, a1_phase, size) != size ||
+			write(ofd, a2, size) != size ||
+			write(ofd, a2_phase, size) != size)
 		{
 			fprintf(stderr, "%s: error writing coefficient file %s\n",
 				command, write_coef);
@@ -250,18 +269,24 @@ main(
 		close(ofd);
 	}
 
-	//------------//
-	// a0 vs. inc //
-	//------------//
-
 	term_vs_inc(command, base, a0, "a0");
 	term_vs_inc(command, base, a1, "a1");
+	term_vs_inc(command, base, a1_phase, "a1p");
 	term_vs_inc(command, base, a2, "a2");
+	term_vs_inc(command, base, a2_phase, "a2p");
+
+	term_vs_spd(command, base, a0, "a0");
+	term_vs_spd(command, base, a1, "a1");
+	term_vs_spd(command, base, a1_phase, "a1p");
+	term_vs_spd(command, base, a2, "a2");
+	term_vs_spd(command, base, a2_phase, "a2p");
 
 	return (0);
 }
 
+//-------------//
 // term_vs_inc //
+//-------------//
 
 int
 term_vs_inc(
@@ -285,10 +310,10 @@ term_vs_inc(
 		}
 		fprintf(ofp, "@ title %c%s, %s pol%c\n", QUOTES, name, pol_map[pol],
 			QUOTES);
-		fprintf(ofp, "@ legend on\n");
+//		fprintf(ofp, "@ legend on\n");
 		fprintf(ofp, "@ xaxis label %cIncidence Angle (deg)%c\n", QUOTES,
 			QUOTES);
-		fprintf(ofp, "@ yaxis label %c%s (dB)%c\n", QUOTES, name,
+		fprintf(ofp, "@ yaxis label %c%s%c\n", QUOTES, name,
 			QUOTES);
 
 		int spds[] = { 1, 3, 5, 8, 15, 20, 30, 40, -1 };
@@ -303,6 +328,57 @@ term_vs_inc(
 			}
 			spd_idx++;
 			if (spds[spd_idx] > 0)
+				fprintf(ofp, "&\n");
+		}
+	}
+	fclose(ofp);
+	return(1);
+}
+
+//-------------//
+// term_vs_spd //
+//-------------//
+
+int
+term_vs_spd(
+	const char*	command,
+	const char*	gmf_file,
+	double	term[POLS][INCS][SPDS],
+	char*	name)
+{
+	char filename[1024];
+	FILE* ofp;
+
+	for (int pol = 0; pol < POLS; pol++)
+	{
+		sprintf(filename, "%s.%s.spd.%s", gmf_file, pol_map[pol], name);
+		ofp = fopen(filename, "w");
+		if (ofp == NULL)
+		{
+			fprintf(stderr, "%s: error opening output file %s\n",
+				command, filename);
+			exit(1);
+		}
+		fprintf(ofp, "@ title %c%s, %s pol%c\n", QUOTES, name, pol_map[pol],
+			QUOTES);
+//		fprintf(ofp, "@ legend on\n");
+		fprintf(ofp, "@ xaxis label %cWind Speed (m/s)%c\n", QUOTES,
+			QUOTES);
+		fprintf(ofp, "@ yaxis label %c%s%c\n", QUOTES, name,
+			QUOTES);
+
+		int incs[] = { 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, -1 };
+		int inc_idx = 0;
+		while (incs[inc_idx] > 0)
+		{
+			fprintf(ofp, "@ legend string %d %c%d deg%c\n", inc_idx, QUOTES,
+				incs[inc_idx], QUOTES);
+			for (int spd = 1; spd < SPDS; spd++)
+			{
+				fprintf(ofp, "%d %g\n", spd, term[pol][incs[inc_idx]][spd]);
+			}
+			inc_idx++;
+			if (incs[inc_idx] > 0)
 				fprintf(ofp, "&\n");
 		}
 	}
