2b99b0d54a582892c0c949d70d833a1d6e3487a2
hudd <>
1997-04-07 16:53:10 +0000

    Initial revision

---
+++ objs/L1B.C
@@ -0,0 +1,142 @@
+//==========================================================//
+// Copyright (C) 1997, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+static const char rcs_id_l15_c[] =
+	"@(#) $Id$";
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <memory.h>
+#include "L15.h"
+
+
+//=========//
+// L15File //
+//=========//
+
+L15File::L15File()
+{
+	return;
+}
+
+L15File::~L15File()
+{
+	return;
+}
+
+
+//=====//
+// L15 //
+//=====//
+
+L15::L15()
+:	time(0), gcAltitude(0.0), gcLongitude(0.0), gcLatitude(0.0), gcX(0.0),
+	gcY(0.0), gcZ(0.0), velX(0.0), velY(0.0), velZ(0.0),
+	antennaPosition(0.0), beam(NONE)
+{
+	AllocateFrame(L15_DATA_REC_SIZE);
+	return;
+}
+
+L15::~L15()
+{
+	return;
+}
+
+//----------------//
+// L15::PackFrame //
+//----------------//
+
+int
+L15::PackFrame()
+{
+	int idx = 0;
+
+	memcpy((void *)(_frame + idx), (void *)&time, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&gcAltitude, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&gcLongitude, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&gcLatitude, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&gcX, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&gcY, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&gcZ, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&velX, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&velY, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&velZ, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&antennaPosition, sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)(_frame + idx), (void *)&beam, sizeof(L15BeamE));
+	idx += sizeof(L15BeamE);
+
+	return(1);
+}
+
+//------------------//
+// L15::UnpackFrame //
+//------------------//
+
+int
+L15::UnpackFrame()
+{
+	int idx = 0;
+
+	memcpy((void *)&time, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&gcAltitude, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&gcLongitude, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&gcLatitude, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&gcX, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&gcY, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&gcZ, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&velX, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&velY, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&velZ, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&antennaPosition, (void *)(_frame + idx), sizeof(double));
+	idx += sizeof(double);
+
+	memcpy((void *)&beam, (void *)(_frame + idx), sizeof(L15BeamE));
+	idx += sizeof(L15BeamE);
+
+	return(1);
+}
---
+++ objs/L1B.h
@@ -0,0 +1,95 @@
+//==========================================================//
+// Copyright (C) 1997, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+#ifndef L15_H
+#define L15_H
+
+static const char rcs_id_l15_h[] =
+	"@(#) $Id$";
+
+#include "Product.h"
+
+//======================================================================
+// CLASSES
+//		L15File, L15FileList, L15
+//======================================================================
+
+#define L15_DATA_REC_SIZE	92
+
+
+//======================================================================
+// CLASS
+//		L15File
+//
+// DESCRIPTION
+//		The L15File object is used to read and write Level 1.5 data.
+//======================================================================
+
+class L15File : public ProductFile
+{
+public:
+
+	//--------------//
+	// construction //
+	//--------------//
+
+	L15File();
+	~L15File();
+};
+
+
+//======================================================================
+// CLASS
+//		L15
+//
+// DESCRIPTION
+//		The L15 object is used to manipulate Level 1.5 data.
+//======================================================================
+
+class L15 : public Product
+{
+public:
+
+	//-------//
+	// enums //
+	//-------//
+
+	enum L15BeamE { NONE, SCATTEROMETER_BEAM_A, SCATTEROMETER_BEAM_B };
+
+	//--------------//
+	// construction //
+	//--------------//
+
+	L15();
+	~L15();
+
+	//-------------------//
+	// data manipulation //
+	//-------------------//
+ 
+	int		PackFrame();
+	int		UnpackFrame();
+
+	//-------------------//
+	// product variables //
+	//-------------------//
+
+	double		time;
+
+	double		gcAltitude;
+	double		gcLongitude;
+	double		gcLatitude;
+	double		gcX;
+	double		gcY;
+	double		gcZ;
+	double		velX;
+	double		velY;
+	double		velZ;
+
+	double		antennaPosition;
+	L15BeamE	beam;
+};
+
+#endif
