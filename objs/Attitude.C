004661c21b702e768b61fb41b5391381af37b7fc
hudd <>
1997-04-21 17:09:39 +0000

    *** empty log message ***

--- objs/Attitude.C
+++ objs/Attitude.C
@@ -14,12 +14,44 @@ static const char rcs_id_attitude_c[] =
 //==========//
 
 Attitude::Attitude()
-:	roll(0.0), pitch(0.0), yaw(0.0)
+:	_roll(0.0), _pitch(0.0), _yaw(0.0)
 {
+	for (int i = 0; i < 3; i++)
+	{
+		_order[i] = 0;
+	}
 	return;
 }
 
 Attitude::~Attitude()
 {
 	return;
+}
+
+//---------------//
+// Attitude::Set //
+//---------------//
+
+int
+Attitude::Set(
+	double		roll,
+	double		pitch,
+	double		yaw,
+	int			order_1,
+	int			order_2,
+	int			order_3)
+{
+	if (order_1 < 1 || order_1 > 3 ||
+		order_2 < 1 || order_2 > 3 ||
+		order_3 < 1 || order_3 > 3)
+	{
+		return(0);
+	}
+	_roll = roll;
+	_pitch = pitch;
+	_yaw = yaw;
+	_order[0] = order_1;
+	_order[1] = order_2;
+	_order[2] = order_3;
+	return(1);
 }
--- objs/Attitude.h
+++ objs/Attitude.h
@@ -34,13 +34,28 @@ public:
 	Attitude();
 	~Attitude();
 
+	//---------------------//
+	// setting and getting //
+	//---------------------//
+
+	int			Set(double roll, double pitch, double yaw, int order_1,
+					int order_2, int order_3);
+
+	double		GetRoll() { return(_roll); };
+	double		GetPitch() { return(_pitch); };
+	double		GetYaw() { return(_yaw); };
+	int*		GetOrderIndicies() { return(_order); };
+
+protected:
+
 	//-----------//
 	// variables //
 	//-----------//
 
-	double		roll;
-	double		pitch;
-	double		yaw;
+	double		_roll;
+	double		_pitch;
+	double		_yaw;
+	int			_order[3];
 };
 
 #endif
