559511d0c58cc4cd852f463b4fe431408318e621
richw <>
1997-06-18 02:04:30 +0000

    basic methods created

--- objs/Grid.C
+++ objs/Grid.C
@@ -17,12 +17,38 @@ static const char rcs_id_grid_c[] =
 
 Grid::Grid()
 {
+	_gridarray = NULL;
+	_grid = NULL;
+	_ephemeris = NULL;
 	return;
 }
 
 Grid::~Grid()
 {
+	if (_gridarray != NULL) delete _gridarray;
 	return;
+}
+
+int
+Grid::OpenOutputFile(const char* filename)
+{
+	int err = l17.SetFilename(filename);
+	if (err == 0) return(0);
+	return(l17.file.OpenForOutput());
+}
+
+int
+Grid::SetEphemeris(Ephemeris *ephemeris)
+{
+	_ephemeris = ephemeris;
+	return(1);
+}
+
+int
+Grid::SetStartTime(double start_time)
+{
+	_start_time = start_time;
+	return(1);
 }
 
 //----------------//
@@ -31,27 +57,130 @@ Grid::~Grid()
 
 int
 Grid::Allocate(
-	int		cross_track_bins,
-	int		along_track_bins)
+double	crosstrack_res,
+double	alongtrack_res,
+double	crosstrack_size,
+double	alongtrack_size)
 {
-	_grid = (MeasList ***)malloc(cross_track_bins * sizeof(MeasList **));
-	if (_grid == NULL)
-		return(0);
+	_alongtrack_res = alongtrack_res;
+	_crosstrack_res = crosstrack_res;
+	_alongtrack_size = alongtrack_size;
+	_crosstrack_size = crosstrack_size;
+    _alongtrack_bins = (int) (_alongtrack_size / _alongtrack_res);
+    _crosstrack_bins = (int) (_crosstrack_size / _crosstrack_res);
 
-	for (int i = 0; i < cross_track_bins; i++)
-	{
-		MeasList** ptr =
-			(MeasList **)malloc(along_track_bins * sizeof(MeasList *));
-		if (ptr == NULL)
-			return(0);
-		*(_grid + i) = ptr;
-		for (int j = 0; j < along_track_bins; j++)
-		{
-			MeasList* ml = new MeasList();
-			if (ml == NULL)
-				return(0);
-			*(*(_grid + i) + j) = ml;
-		}
-	}
+	//
+	// Set circular buffer tracking parameters to the beginning of the
+	// virtual grid.
+	//
+
+	_ati_start = 0;
+	_ati_offset = 0;
+
+	_gridarray = new Array<MeasList>(_alongtrack_bins,_crosstrack_bins);
+	if (_gridarray == NULL) return(0);
+	_grid = (MeasList**) (_gridarray -> ptr);
+
 	return(1);
+}
+
+//
+// Grid::Add
+//
+// Add one measurment to the grid of measurements.
+//
+// meas = pointer to the Meas object to grid.
+// meas_time = time at which the measurement was made.
+//
+
+int
+Grid::Add(Meas *meas, double meas_time)
+{
+
+EarthPosition rground(meas->center.latitude,meas->center.longitude,
+                      EarthPosition::GEODETIC);
+float ctd,atd;
+_ephemeris->GetSubtrackCoordinates(rground,_start_time,meas_time,&ctd,&atd);
+
+//
+// Compute grid indices, noting that the cross track grid starts on the left
+// side at cti = 0.
+//
+
+int cti = (int) ((ctd + _crosstrack_size/2.0)/_crosstrack_res);
+int vati = (int) (atd/_alongtrack_res);	// virtual along track index.
+
+if ((cti >= _crosstrack_bins) || (cti < 0))
+{
+	printf("Error: crosstrack index = %d out of range in Grid::Add\n",cti);
+	return(0);
+}
+
+//
+// Note that reverse shifting is not implemented.
+// Thus, if vati falls before the earliest row in memory, it is considered
+// out of range instead of trying to back up the buffer.  This limitation
+// is imposed by BufferedList.
+//
+
+if (vati < _ati_offset)
+{
+	printf("Error: alongtrack index = %d out of range in Grid::Add\n",vati);
+	return(0);
+}
+
+//
+// Determine if the along track index is in memory or not.
+// If not, read and write rows until it is in range.
+//
+
+while (vati - _ati_offset >= _alongtrack_bins)
+{	// vati is beyond latest row, so need to shift the grid buffer
+	ShiftForward();
+}
+
+//
+// Convert the along track index into the virtual buffer (vati) to an
+// index into the grid in memory (ati).
+//
+
+int ati = vati - _ati_offset + _ati_start;
+if (ati >= _alongtrack_bins) ati -= _alongtrack_bins;
+
+// Add the measurement to the appropriate grid cell measurement list.
+_grid[ati][cti].Append(meas);
+
+return(1);
+
+}
+
+//
+// Grid::ShiftForward
+//
+// Shift the grid of measurements by one row.
+// The earliest row is written out to the output file and its storage
+// is reset to receive another row.  The circular buffer tracking indices
+// are updated appropriately so that the newly freed row is mapped to
+// the next along track index.
+//
+
+int
+Grid::ShiftForward()
+{
+
+// Construct level 1.7 frames from each grid cell
+
+// Write out the earliest row of measurement lists.
+for (int i=0; i < _crosstrack_bins; i++)
+{
+l17.frame.measList = _grid[_ati_start][i];
+l17.WriteDataRec();
+_grid[_ati_start][i].FreeContents();	// prepare for new data
+}
+
+// Update buffer indices.
+_ati_start++;
+if (_ati_start >= _alongtrack_bins) _ati_start = 0;
+_ati_offset++;
+
 }
--- objs/Grid.h
+++ objs/Grid.h
@@ -9,6 +9,8 @@
 static const char rcs_id_grid_h[] =
 	"@(#) $Id$";
 
+#include "Array.h"
+#include "L17.h"
 #include "Meas.h"
 
 
@@ -37,23 +39,70 @@ public:
 	Grid();
 	~Grid();
 
-	int		Allocate(int cross_track_bins, int along_track_bins);
+	int Allocate(
+	double  crosstrack_res,
+	double  alongtrack_res,
+	double  crosstrack_size,
+	double  alongtrack_size);
+	int OpenOutputFile(const char* filename);
+	int SetEphemeris(Ephemeris *ephemeris);
+	int SetStartTime(double start_time);
 
 	//---------------------//
 	// adding measurements //
 	//---------------------//
 
+	int Add(Meas *meas, double meas_time);
+
 	//--------------//
 	// input/output //
 	//--------------//
 
-protected:
+	int Grid::ShiftForward();
 
 	//-----------//
 	// variables //
 	//-----------//
 
-	MeasList***		_grid;
+	L17 l17;	// handles output to a level 1.7 data file.
+
+protected:
+
+	// Pointer to the ephemeris object that defines the grid location.
+	Ephemeris *_ephemeris;
+
+	// resolution and sizes are in km
+	double _crosstrack_res;
+	double _alongtrack_res;
+	double _crosstrack_size;
+	double _alongtrack_size;
+
+	int _crosstrack_bins;
+	int _alongtrack_bins;
+
+	//
+	// start_time specifies the zero point of the along track axis.
+	//
+
+	double _start_time;
+
+	//
+	// _ati_start tracks the current earliest point in the circular buffer
+	// of grid rows stored in _grid.  Thus, _ati_start = n means that
+	// _grid[n] is the first row in memory, and _grid[n-1] is the latest
+	// row in memory. (Using arithmetic modulus the length of the grid
+	// in memory.)
+	// ati_offset gives the number of rows that the grid in memory is
+	// displaced from the virtual grid specifed by the ephemeris object
+	// and the grid size parameters. Thus, _ati_offset = n means that
+	// _grid[0] is actually row n of the virtual grid (also zero offset).
+	//
+
+	int _ati_start;
+	int _ati_offset;
+
+	Array<MeasList>* _gridarray;
+	MeasList** _grid;
 };
 
 #endif
