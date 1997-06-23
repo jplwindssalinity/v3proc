92c6ea46af1367ccdcb67ce1a585b63a4ca87d71
richw <>
1997-06-23 21:09:51 +0000

    Initial revision

---
+++ objs/L1BToL2A.C
@@ -0,0 +1,70 @@
+//==========================================================//
+// Copyright (C) 1997, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+static const char rcs_id_l15tol17_c[] =
+	"@(#) $Id$";
+
+#include "L15ToL17.h"
+#include "Antenna.h"
+#include "Ephemeris.h"
+#include "InstrumentGeom.h"
+#include "GenericGeom.h"
+
+
+//==========//
+// L15ToL17 //
+//==========//
+
+L15ToL17::L15ToL17()
+{
+	return;
+}
+
+L15ToL17::~L15ToL17()
+{
+	return;
+}
+
+//-----------------//
+// L15ToL17::Group //
+//-----------------//
+
+int
+L15ToL17::Group(
+	L15*		l15,
+	Grid*		grid)
+{
+   	// position at start of list of spots
+   	MeasSpot* mspot = l15->frame.spotList.GetHead();
+   	// remove spot before gridding
+  	mspot = l15->frame.spotList.RemoveCurrent();
+
+    //------------------//
+    // for each spot... //
+    //------------------//
+
+	while (mspot != NULL)
+    {
+		//------------------------------//
+		// ...Grid the measurement list //
+		//------------------------------//
+
+   		// position at start of list of slices
+   		Meas* meas = mspot->slices.GetHead();
+   		// remove slice before gridding
+   		meas = mspot->slices.RemoveCurrent();
+    	while (meas != NULL)
+    	{   // grid each slice in this measurement spot
+        	grid->Add(meas,mspot->time);
+ 			// move to next slice
+        	meas = mspot->slices.RemoveCurrent();
+    	}
+
+ 		// move to next spot
+  		mspot = l15->frame.spotList.RemoveCurrent();
+	}
+
+	return(1);
+}
---
+++ objs/L1BToL2A.h
@@ -0,0 +1,51 @@
+//==========================================================//
+// Copyright (C) 1997, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+#ifndef L15TOL17_H
+#define L15TOL17_H
+
+static const char rcs_id_l15tol17_h[] =
+	"@(#) $Id$";
+
+#include "L15.h"
+#include "Grid.h"
+#include "Antenna.h"
+#include "Ephemeris.h"
+
+
+//======================================================================
+// CLASSES
+//		L15ToL17
+//======================================================================
+
+//======================================================================
+// CLASS
+//		L15ToL17
+//
+// DESCRIPTION
+//		The L15ToL17 object is used to convert between Level 1.5 data
+//		and Level 1.7 data.  It performs co-location or grouping of the
+//		measurements.
+//======================================================================
+
+class L15ToL17
+{
+public:
+
+	//--------------//
+	// construction //
+	//--------------//
+
+	L15ToL17();
+	~L15ToL17();
+
+	//------------//
+	// conversion //
+	//------------//
+
+	int		Group(L15* l15, Grid* grid);
+};
+
+#endif
