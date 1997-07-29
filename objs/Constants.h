333c4053571958633ba3a22a7b507bc4d573d7c0
richw <>
1997-07-29 00:05:07 +0000

    added speed of light

--- objs/Constants.h
+++ objs/Constants.h
@@ -21,6 +21,7 @@ static const double two_pi = 2.0 * M_PI;
 static const double dtr = 1.745329252e-2;
 static const double rtd = 5.729577951e1;
 static const double rpm_to_radps = M_PI / 30.0;
+static const double speed_light = 2.99792458e8;
 
 //
 // Some fixed constants describing the Earth.
@@ -30,5 +31,7 @@ static const double rpm_to_radps = M_PI / 30.0;
 static const double r1_earth = 6378.1363;
 static const double r2_earth = 6356.751600562937;
 static const double eccentricity_earth = 8.1819221455523210E-02;
+// sidereal rotation rate (rad/sec)
+static const double w_earth = pi/180*4.1780746e-3;
 
 #endif
