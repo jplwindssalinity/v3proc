0c56f6b47475001dde402f051e6e94b0b89f2389
bstiles <>
1997-09-26 00:11:18 +0000

    Added RandomVelocity Attitude Control Models.

--- objs/ConfigSim.C
+++ objs/ConfigSim.C
@@ -96,37 +96,145 @@ ConfigAttitudeControlModel(SpacecraftSim* spacecraft_sim,
 	else if(strcmp(string,"Gaussian")==0 || strcmp(string,"GAUSSIAN")==0
 	   || strcmp(string,"gaussian")==0)
 	{
-		double variance;
+		double variance, mean;
 	 	if(! config_list->GetDouble(ROLL_CONTROL_VARIANCE_KEYWORD,
 		   &variance)) return(0);
-		roll=new Gaussian((float)variance,0);
+	 	if(! config_list->GetDouble(ROLL_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+		roll=new Gaussian((float)variance,(float)mean);
 	 	if(! config_list->GetDouble(PITCH_CONTROL_VARIANCE_KEYWORD,
 		   &variance)) return(0);
-		pitch=new Gaussian((float)variance,0);
+	 	if(! config_list->GetDouble(PITCH_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+		pitch=new Gaussian((float)variance,(float)mean);
 	 	if(! config_list->GetDouble(YAW_CONTROL_VARIANCE_KEYWORD,
 		   &variance)) return(0);
-		yaw=new Gaussian((float)variance,0);
+	 	if(! config_list->GetDouble(YAW_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+		yaw=new Gaussian((float)variance,(float)mean);
 
 	}
 	else if(strcmp(string,"Uniform")==0 || strcmp(string,"UNIFORM")==0
 	   || strcmp(string,"uniform")==0)
 	{
-		double radius;
+		double radius, mean;
 	 	if(! config_list->GetDouble(ROLL_CONTROL_RADIUS_KEYWORD,
 		   &radius)) return(0);
-		roll=new Uniform((float)radius,0);
+	 	if(! config_list->GetDouble(ROLL_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+		roll=new Uniform((float)radius,float(mean));
 	 	if(! config_list->GetDouble(PITCH_CONTROL_RADIUS_KEYWORD,
 		   &radius)) return(0);
-		pitch=new Uniform((float)radius,0);
+	 	if(! config_list->GetDouble(PITCH_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+		pitch=new Uniform((float)radius,float(mean));
 	 	if(! config_list->GetDouble(YAW_CONTROL_RADIUS_KEYWORD,
 		   &radius)) return(0);
-		yaw=new Uniform((float)radius,0);
+	 	if(! config_list->GetDouble(YAW_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+		yaw=new Uniform((float)radius,float(mean));
 	}
+	else if(strcmp(string,"Gaussian_Random_Velocity")==0
+           || strcmp(string,"GAUSSIAN_RANDOM_VELOCITY")==0
+	   || strcmp(string,"gaussian_random_velocity")==0)
+	{
+		double variance, mean, sample_rate, bound;
+		GenericDist *roll_velocity, *pitch_velocity, *yaw_velocity;
+
+		if(! config_list->GetDouble(VELOCITY_SAMPLE_RATE_KEYWORD,
+		   &sample_rate)) return(0);
+
+	 	if(! config_list->GetDouble(ROLL_CONTROL_VARIANCE_KEYWORD,
+		   &variance)) return(0);
+	 	if(! config_list->GetDouble(ROLL_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+	 	if(! config_list->GetDouble(ROLL_CONTROL_BOUND_KEYWORD,
+		   &bound)) return(0);
+		roll_velocity=new Gaussian((float)variance,0.0);
+		roll=new RandomVelocity(roll_velocity, (float)sample_rate,
+			(float)bound, (float)mean);
+
+
+	 	if(! config_list->GetDouble(PITCH_CONTROL_VARIANCE_KEYWORD,
+		   &variance)) return(0);
+	 	if(! config_list->GetDouble(PITCH_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+	 	if(! config_list->GetDouble(PITCH_CONTROL_BOUND_KEYWORD,
+		   &bound)) return(0);
+		pitch_velocity=new Gaussian((float)variance,0.0);
+		pitch=new RandomVelocity(pitch_velocity, (float)sample_rate,
+			(float)bound, (float)mean);
+
+
+
+	 	if(! config_list->GetDouble(YAW_CONTROL_VARIANCE_KEYWORD,
+		   &variance)) return(0);
+	 	if(! config_list->GetDouble(YAW_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+	 	if(! config_list->GetDouble(YAW_CONTROL_BOUND_KEYWORD,
+		   &bound)) return(0);
+		yaw_velocity=new Gaussian((float)variance,0.0);
+		yaw=new RandomVelocity(yaw_velocity, (float)sample_rate,
+			(float)bound, (float)mean);
+
+
+
+	}
+	else if(strcmp(string,"Uniform_Random_Velocity")==0
+           || strcmp(string,"UNIFORM_RANDOM_VELOCITY")==0
+	   || strcmp(string,"uniform_random_velocity")==0)
+	{
+		double radius, mean, sample_rate, bound;
+		GenericDist *roll_velocity, *pitch_velocity, *yaw_velocity;
+
+		if(! config_list->GetDouble(VELOCITY_SAMPLE_RATE_KEYWORD,
+		   &sample_rate)) return(0);
+
+	 	if(! config_list->GetDouble(ROLL_CONTROL_RADIUS_KEYWORD,
+		   &radius)) return(0);
+	 	if(! config_list->GetDouble(ROLL_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+	 	if(! config_list->GetDouble(ROLL_CONTROL_BOUND_KEYWORD,
+		   &bound)) return(0);
+		roll_velocity=new Uniform((float)radius,0.0);
+		roll=new RandomVelocity(roll_velocity, (float)sample_rate,
+			(float)bound, (float)mean);
+
+
+	 	if(! config_list->GetDouble(PITCH_CONTROL_RADIUS_KEYWORD,
+		   &radius)) return(0);
+	 	if(! config_list->GetDouble(PITCH_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+	 	if(! config_list->GetDouble(PITCH_CONTROL_BOUND_KEYWORD,
+		   &bound)) return(0);
+		pitch_velocity=new Uniform((float)radius,0.0);
+		pitch=new RandomVelocity(pitch_velocity, (float)sample_rate,
+			(float)bound, (float)mean);
+
+		
+
+
+	 	if(! config_list->GetDouble(YAW_CONTROL_RADIUS_KEYWORD,
+		   &radius)) return(0);
+	 	if(! config_list->GetDouble(YAW_CONTROL_MEAN_KEYWORD,
+		   &mean)) return(0);
+	 	if(! config_list->GetDouble(YAW_CONTROL_BOUND_KEYWORD,
+		   &bound)) return(0);
+		yaw_velocity=new Uniform((float)radius,0.0);
+		yaw=new RandomVelocity(yaw_velocity, (float)sample_rate,
+			(float)bound, (float)mean);
+
+
+
+		
+	}
+
 
 	else{
            fprintf(stderr,"No such Attitude Control Model. \n");
-	   fprintf(stderr,"Implemented models are GAUSSIAN and UNIFORM or");
-	   fprintf(stderr,"use NONE for zero control error. \n");
+	   fprintf(stderr,"Implemented models are GAUSSIAN, UNIFORM, \n");
+	   fprintf(stderr,"GAUSSIAN_RANDOM_VELOCITY,\n");
+	   fprintf(stderr,"UNIFORM_RANDOM_VELOCITY, and NONE. \n");
 	   return(0);
 	}
         ACEM=new AttDist(roll,pitch,yaw);
