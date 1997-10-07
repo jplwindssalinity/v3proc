41ea410583e4956a83ef5f127d56b0fd03abb760
hudd <>
1997-10-07 17:51:54 +0000

    *** empty log message ***

--- objs/L1BToL2A.C
+++ objs/L1BToL2A.C
@@ -36,34 +36,35 @@ L15ToL17::Group(
 	L15*		l15,
 	Grid*		grid)
 {
-   	// position at start of list of spots
-   	MeasSpot* mspot = l15->frame.spotList.GetHead();
-   	// remove spot before gridding
-  	mspot = l15->frame.spotList.RemoveCurrent();
+	// position at start of list of spots
+	MeasSpot* mspot = l15->frame.spotList.GetHead();
+	// remove spot before gridding
+	mspot = l15->frame.spotList.RemoveCurrent();
 
-    //------------------//
-    // for each spot... //
-    //------------------//
+	//------------------//
+	// for each spot... //
+	//------------------//
 
 	while (mspot != NULL)
-    {
+	{
 		//------------------------------//
 		// ...Grid the measurement list //
 		//------------------------------//
 
-   		// position at start of list of slices
-   		Meas* meas = mspot->slices.GetHead();
-   		// remove slice before gridding
-   		meas = mspot->slices.RemoveCurrent();
-    	while (meas != NULL)
-    	{   // grid each slice in this measurement spot
-        	grid->Add(meas,mspot->time);
+		// position at start of list of slices
+		Meas* meas = mspot->slices.GetHead();
+		// remove slice before gridding
+		meas = mspot->slices.RemoveCurrent();
+		while (meas != NULL)
+		{
+			// grid each slice in this measurement spot
+			grid->Add(meas,mspot->time);
  			// move to next slice
-        	meas = mspot->slices.RemoveCurrent();
-    	}
+			meas = mspot->slices.RemoveCurrent();
+		}
 
  		// move to next spot
-  		mspot = l15->frame.spotList.RemoveCurrent();
+		mspot = l15->frame.spotList.RemoveCurrent();
 	}
 
 	return(1);
