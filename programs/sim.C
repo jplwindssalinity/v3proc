e5cda90e0413fa2e4fa8df50eea636f5432b1291
hudd <>
1997-10-27 23:08:42 +0000

    *** empty log message ***

--- programs/sim.C
+++ programs/sim.C
@@ -81,6 +81,7 @@ template class List<MeasSpot>;
 template class BufferedList<OrbitState>;
 template class List<OrbitState>;
 template class List<WindVectorPlus>;
+template class List<long>;
 
 //-----------//
 // CONSTANTS //
