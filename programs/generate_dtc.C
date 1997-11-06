0b14876698568927717d9fbdbe48e9521455f681
hudd <>
1997-11-06 00:04:22 +0000

    Initial revision

---
+++ programs/generate_dtc.C
@@ -0,0 +1,405 @@
+//==========================================================//
+// Copyright (C) 1997, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+//----------------------------------------------------------------------
+// NAME
+//		generate_dtc
+//
+// SYNOPSIS
+//		generate_dtc <sim_config_file> <RGC_file> <DTC_file>
+//
+// DESCRIPTION
+//		Generates a set of Doppler Tracking Constants based upon the
+//		parameters in the simulation configuration file and the given
+//		Receiver Gate Constants.
+//
+// OPTIONS
+//		None.
+//
+// OPERANDS
+//		The following operands are supported:
+//		<sim_config_file>	The sim_config_file needed listing
+//								all input parameters, input files, and
+//								output files.
+//
+//		<RGC_file>			The RGC input file.
+//
+//		<DTC_file>			The DTC output file.
+//
+// EXAMPLES
+//		An example of a command line is:
+//			% generate_constants sws1b.cfg rgc.dat dtc.dat
+//
+// ENVIRONMENT
+//		Not environment dependent.
+//
+// EXIT STATUS
+//		The following exit values are returned:
+//		0	Program executed successfully
+//		>0	Program had an error
+//
+// NOTES
+//		None.
+//
+// AUTHOR
+//		James N. Huddleston
+//		hudd@acid.jpl.nasa.gov
+//----------------------------------------------------------------------
+
+//-----------------------//
+// Configuration Control //
+//-----------------------//
+
+static const char rcs_id[] =
+	"@(#) $Id$";
+
+//----------//
+// INCLUDES //
+//----------//
+
+#include <stdio.h>
+#include "Misc.h"
+#include "ConfigList.h"
+#include "List.h"
+#include "List.C"
+#include "Spacecraft.h"
+#include "ConfigSim.h"
+#include "Tracking.h"
+#include "InstrumentGeom.h"
+#include "BufferedList.h"
+#include "BufferedList.C"
+
+//-----------//
+// TEMPLATES //
+//-----------//
+
+template class List<EarthPosition>;
+template class List<StringPair>;
+template class List<Meas>;
+template class List<WindVectorPlus>;
+template class List<MeasSpot>;
+template class List<long>;
+template class List<OrbitState>;
+template class BufferedList<OrbitState>;
+
+//-----------//
+// CONSTANTS //
+//-----------//
+
+#define DOPPLER_ORBIT_STEPS		256
+#define DOPPLER_AZIMUTH_STEPS	360		// used for fitting
+
+//--------//
+// MACROS //
+//--------//
+
+//------------------//
+// TYPE DEFINITIONS //
+//------------------//
+
+//-----------------------//
+// FUNCTION DECLARATIONS //
+//-----------------------//
+
+int doppler_fit(int count, double* baseband_freq, double* a, double* p,
+	double* c);
+
+//------------------//
+// OPTION VARIABLES //
+//------------------//
+
+//------------------//
+// GLOBAL VARIABLES //
+//------------------//
+
+const char* usage_array[] = { "<sim_config_file>", "<RGC_file>",
+	"<DTC_FILE>", 0};
+
+//--------------//
+// MAIN PROGRAM //
+//--------------//
+
+int
+main(
+	int		argc,
+	char*	argv[])
+{
+	//------------------------//
+	// parse the command line //
+	//------------------------//
+
+	const char* command = no_path(argv[0]);
+
+	if (argc != 4)
+		usage(command, usage_array, 1);
+
+	int arg_idx = 1;
+	const char* config_file = argv[arg_idx++];
+	const char* rgc_file = argv[arg_idx++];
+	const char* dtc_file = argv[arg_idx++];
+
+	//--------------------------------//
+	// read in simulation config file //
+	//--------------------------------//
+
+	ConfigList config_list;
+	config_list.LogErrors();
+	if (! config_list.Read(config_file))
+	{
+		fprintf(stderr, "%s: error reading sim config file %s\n",
+			command, config_file);
+		exit(1);
+	}
+
+	//----------------------------------------------//
+	// create a spacecraft and spacecraft simulator //
+	//----------------------------------------------//
+
+	Spacecraft spacecraft;
+	if (! ConfigSpacecraft(&spacecraft, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring spacecraft simulator\n",
+			command);
+		exit(1);
+	}
+
+	SpacecraftSim spacecraft_sim;
+	if (! ConfigSpacecraftSim(&spacecraft_sim, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring spacecraft simulator\n",
+			command);
+		exit(1);
+	}
+	spacecraft_sim.LocationToOrbit(0.0, 0.0, 1);
+
+	//-----------------------------------------------//
+	// create an instrument and instrument simulator //
+	//-----------------------------------------------//
+
+	Instrument instrument;
+	if (! ConfigInstrument(&instrument, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring instrument\n", command);
+		exit(1);
+	}
+	Antenna* antenna = &(instrument.antenna);
+
+	InstrumentSim instrument_sim;
+	if (! ConfigInstrumentSim(&instrument_sim, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring instrument simulator\n",
+			command);
+		exit(1);
+	}
+
+	//--------------------//
+	// read range tracker //
+	//--------------------//
+
+	RangeTracker range_tracker;
+	if (! range_tracker.ReadBinary(rgc_file))
+	{
+		fprintf(stderr, "%s: error reading RGC file %s\n", command,
+			rgc_file);
+		exit(1);
+	}
+
+	//--------------------------//
+	// allocate Doppler tracker //
+	//--------------------------//
+
+	DopplerTracker doppler_tracker;
+	if (! doppler_tracker.Allocate(antenna->numberOfBeams,
+		DOPPLER_ORBIT_STEPS))
+	{
+		fprintf(stderr, "%s: error allocating Doppler tracker\n",
+			command);
+	}
+
+	//----------------//
+	// allocate terms //
+	//----------------//
+
+	// terms are [0] = amplitude, [1] = phase, [2] = bias
+	double*** terms;
+	terms = (double ***)make_array(sizeof(double), 3,
+		antenna->numberOfBeams, DOPPLER_ORBIT_STEPS, 3);
+
+	//------------//
+	// initialize //
+	//------------//
+
+	double start_time = spacecraft_sim.GetEpoch();
+	double orbit_period = spacecraft_sim.GetPeriod();
+
+	if (! instrument_sim.Initialize(&(instrument.antenna)))
+	{
+		fprintf(stderr, "%s: error initializing instrument simulator\n",
+			command);
+		exit(1);
+	}
+	instrument.Eqx(start_time);
+
+	if (! spacecraft_sim.Initialize(start_time))
+	{
+		fprintf(stderr, "%s: error initializing spacecraft simulator\n",
+			command);
+		exit(1);
+	}
+
+	//-----------//
+	// variables //
+	//-----------//
+
+	OrbitState* orbit_state = &(spacecraft.orbitState);
+	Attitude* attitude = &(spacecraft.attitude);
+	Vector3 vector;
+
+	//--------------------//
+	// loop through orbit //
+	//--------------------//
+
+	double orbit_step_size = orbit_period / (double)DOPPLER_ORBIT_STEPS;
+	double azimuth_step_size = two_pi / (double)DOPPLER_AZIMUTH_STEPS;
+
+	for (int orbit_step = 0; orbit_step < DOPPLER_ORBIT_STEPS; orbit_step++)
+	{
+		//--------------------//
+		// calculate the time //
+		//--------------------//
+
+		// addition of 0.5 centers on orbit_step
+		double time = start_time +
+			orbit_step_size * ((double)orbit_step + 0.5);
+
+		//-----------------------//
+		// locate the spacecraft //
+		//-----------------------//
+
+		spacecraft_sim.UpdateOrbit(time, &spacecraft);
+
+		//--------------------//
+		// step through beams //
+		//--------------------//
+
+		for (int beam_idx = 0; beam_idx < antenna->numberOfBeams;
+			beam_idx++)
+		{
+			antenna->currentBeamIdx = beam_idx;
+			Beam* beam = antenna->GetCurrentBeam();
+
+			//------------------------------//
+			// calculate receiver gate info //
+			//------------------------------//
+
+			range_tracker.SetInstrument(&instrument);
+
+			//--------------------------------//
+			// calculate baseband frequencies //
+			//--------------------------------//
+
+			double baseband_freq[DOPPLER_AZIMUTH_STEPS];
+
+			for (int azimuth_step = 0; azimuth_step < DOPPLER_AZIMUTH_STEPS;
+				azimuth_step++)
+			{
+				antenna->azimuthAngle = azimuth_step_size *
+					((double)azimuth_step + 0.5);
+
+				CoordinateSwitch antenna_frame_to_gc =
+					AntennaFrameToGC(orbit_state, attitude, antenna);
+
+				double look, azimuth;
+				if (! GetTwoWayPeakGain2(&antenna_frame_to_gc, &spacecraft,
+					beam, antenna->spinRate, &look, &azimuth))
+				{
+					fprintf(stderr, "%s: error finding two-way peak gain\n",
+						command);
+					exit(1);
+				}
+				vector.SphericalSet(1.0, look, azimuth);
+
+				IdealCommandedDoppler(&antenna_frame_to_gc, &spacecraft,
+					&instrument, vector);
+
+				baseband_freq[azimuth_step] = instrument.commandedDoppler;
+			}
+
+			//------------------------//
+			// fit doppler parameters //
+			//------------------------//
+
+			double a, p, c;
+			doppler_fit(DOPPLER_AZIMUTH_STEPS, baseband_freq, &a, &p, &c);
+			*(*(*(terms + beam_idx) + orbit_step) + 0) = a;
+			*(*(*(terms + beam_idx) + orbit_step) + 1) = p;
+			*(*(*(terms + beam_idx) + orbit_step) + 2) = c;
+		}
+	}
+
+	//---------------------------//
+	// set the doppler constants //
+	//---------------------------//
+
+	doppler_tracker.Set(terms);
+
+	if (dtc_file)
+	{
+		free_array((void *)terms, 3, antenna->numberOfBeams,
+			DOPPLER_ORBIT_STEPS, 3);
+	}
+
+	//------------------------------------------//
+	// write out the Doppler tracking constants //
+	//------------------------------------------//
+
+	if (dtc_file)
+	{
+		if (! doppler_tracker.WriteBinary(dtc_file))
+		{
+			fprintf(stderr, "%s: error writing DTC file %s\n", command,
+				dtc_file);
+			exit(1);
+		}
+	}
+
+	return (0);
+}
+
+//-------------//
+// doppler_fit //
+//-------------//
+
+int
+doppler_fit(
+	int			count,
+	double*		baseband_freq,
+	double*		a,
+	double*		p,
+	double*		c)
+{
+	double wn = two_pi / (double) count;
+	double real[2], imag[2];
+ 
+	for (int i = 0; i < 2; i++)
+	{
+		real[i] = 0.0;
+		imag[i] = 0.0;
+		for (int j = 0; j < count; j++)
+		{
+			double arg = wn * (double)i * (double)j;
+			double c = cos(arg);
+			double s = sin(arg);
+			real[i] += baseband_freq[j] * c;
+			imag[i] += baseband_freq[j] * s;
+		}
+	}
+ 
+	*a = 2.0 * sqrt(real[1] * real[1] + imag[1] * imag[1]) / (double)count;
+	*p = -atan2(imag[1], real[1]);
+	*c = real[0] / (double)count;
+
+	return(1);
+}
---
+++ programs/generate_rgc.C
@@ -0,0 +1,330 @@
+//==========================================================//
+// Copyright (C) 1997, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+//----------------------------------------------------------------------
+// NAME
+//		generate_rgc
+//
+// SYNOPSIS
+//		generate_rgc <sim_config_file> <RGC_file>
+//
+// DESCRIPTION
+//		Generates a set of Receiver Gate Constants based upon the
+//		parameters in the simulation configuration file.
+//
+// OPTIONS
+//		None.
+//
+// OPERANDS
+//		The following operands are supported:
+//		<sim_config_file>	The sim_config_file needed listing
+//								all input parameters.
+//
+//		<RGC_file>			The RGC output file.
+//
+// EXAMPLES
+//		An example of a command line is:
+//			% generate_constants sws1b.cfg rgc.dat
+//
+// ENVIRONMENT
+//		Not environment dependent.
+//
+// EXIT STATUS
+//		The following exit values are returned:
+//		0	Program executed successfully
+//		>0	Program had an error
+//
+// NOTES
+//		None.
+//
+// AUTHOR
+//		James N. Huddleston
+//		hudd@acid.jpl.nasa.gov
+//----------------------------------------------------------------------
+
+//-----------------------//
+// Configuration Control //
+//-----------------------//
+
+static const char rcs_id[] =
+	"@(#) $Id$";
+
+//----------//
+// INCLUDES //
+//----------//
+
+#include <stdio.h>
+#include "Misc.h"
+#include "ConfigList.h"
+#include "List.h"
+#include "List.C"
+#include "Spacecraft.h"
+#include "ConfigSim.h"
+#include "Tracking.h"
+#include "InstrumentGeom.h"
+#include "BufferedList.h"
+#include "BufferedList.C"
+
+//-----------//
+// TEMPLATES //
+//-----------//
+
+template class List<EarthPosition>;
+template class List<StringPair>;
+template class List<Meas>;
+template class List<WindVectorPlus>;
+template class List<MeasSpot>;
+template class List<long>;
+template class List<OrbitState>;
+template class BufferedList<OrbitState>;
+
+//-----------//
+// CONSTANTS //
+//-----------//
+
+#define RANGE_ORBIT_STEPS		256
+#define RANGE_AZIMUTH_STEPS		90		// used for averaging
+
+//--------//
+// MACROS //
+//--------//
+
+//------------------//
+// TYPE DEFINITIONS //
+//------------------//
+
+//-----------------------//
+// FUNCTION DECLARATIONS //
+//-----------------------//
+
+//------------------//
+// OPTION VARIABLES //
+//------------------//
+
+//------------------//
+// GLOBAL VARIABLES //
+//------------------//
+
+const char* usage_array[] = { "<sim_config_file>", "<RGC_file>", 0 };
+
+//--------------//
+// MAIN PROGRAM //
+//--------------//
+
+int
+main(
+	int		argc,
+	char*	argv[])
+{
+	//------------------------//
+	// parse the command line //
+	//------------------------//
+
+	const char* command = no_path(argv[0]);
+
+	if (argc != 3)
+		usage(command, usage_array, 1);
+
+	int arg_idx = 1;
+	const char* config_file = argv[arg_idx++];
+	const char* rgc_file = argv[arg_idx++];
+
+	//--------------------------------//
+	// read in simulation config file //
+	//--------------------------------//
+
+	ConfigList config_list;
+	config_list.LogErrors();
+	if (! config_list.Read(config_file))
+	{
+		fprintf(stderr, "%s: error reading sim config file %s\n",
+			command, config_file);
+		exit(1);
+	}
+
+	//----------------------------------------------//
+	// create a spacecraft and spacecraft simulator //
+	//----------------------------------------------//
+
+	Spacecraft spacecraft;
+	if (! ConfigSpacecraft(&spacecraft, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring spacecraft simulator\n",
+			command);
+		exit(1);
+	}
+
+	SpacecraftSim spacecraft_sim;
+	if (! ConfigSpacecraftSim(&spacecraft_sim, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring spacecraft simulator\n",
+			command);
+		exit(1);
+	}
+	spacecraft_sim.LocationToOrbit(0.0, 0.0, 1);
+
+	//-----------------------------------------------//
+	// create an instrument and instrument simulator //
+	//-----------------------------------------------//
+
+	Instrument instrument;
+	if (! ConfigInstrument(&instrument, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring instrument\n", command);
+		exit(1);
+	}
+	Antenna* antenna = &(instrument.antenna);
+
+	InstrumentSim instrument_sim;
+	if (! ConfigInstrumentSim(&instrument_sim, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring instrument simulator\n",
+			command);
+		exit(1);
+	}
+
+	//------------------------//
+	// allocate range tracker //
+	//------------------------//
+
+	RangeTracker range_tracker;
+	if (! range_tracker.Allocate(antenna->numberOfBeams, RANGE_ORBIT_STEPS))
+	{
+		fprintf(stderr, "%s: error allocating range tracker\n", command);
+		exit(1);
+	}
+
+	//------------//
+	// initialize //
+	//------------//
+
+	double start_time = spacecraft_sim.GetEpoch();
+	double orbit_period = spacecraft_sim.GetPeriod();
+
+	if (! instrument_sim.Initialize(&(instrument.antenna)))
+	{
+		fprintf(stderr, "%s: error initializing instrument simulator\n",
+			command);
+		exit(1);
+	}
+	instrument.Eqx(start_time);
+
+	if (! spacecraft_sim.Initialize(start_time))
+	{
+		fprintf(stderr, "%s: error initializing spacecraft simulator\n",
+			command);
+		exit(1);
+	}
+
+	//-----------//
+	// variables //
+	//-----------//
+
+	OrbitState* orbit_state = &(spacecraft.orbitState);
+	Attitude* attitude = &(spacecraft.attitude);
+	Vector3 vector;
+
+	//--------------------//
+	// loop through orbit //
+	//--------------------//
+
+	double orbit_step_size = orbit_period / (double)RANGE_ORBIT_STEPS;
+	double azimuth_step_size = two_pi / (double)RANGE_AZIMUTH_STEPS;
+
+	for (int orbit_step = 0; orbit_step < RANGE_ORBIT_STEPS; orbit_step++)
+	{
+printf("%d of %d\n", orbit_step, RANGE_ORBIT_STEPS);
+		//--------------------//
+		// calculate the time //
+		//--------------------//
+
+		// addition of 0.5 centers on orbit_step
+		double time = start_time +
+			orbit_step_size * ((double)orbit_step + 0.5);
+
+		//-----------------------//
+		// locate the spacecraft //
+		//-----------------------//
+
+		spacecraft_sim.UpdateOrbit(time, &spacecraft);
+
+		//--------------------//
+		// step through beams //
+		//--------------------//
+
+		for (int beam_idx = 0; beam_idx < antenna->numberOfBeams;
+			beam_idx++)
+		{
+			antenna->currentBeamIdx = beam_idx;
+			Beam* beam = antenna->GetCurrentBeam();
+
+			double rtt_sum = 0.0;
+			for (int azimuth_step = 0; azimuth_step < RANGE_AZIMUTH_STEPS;
+				azimuth_step++)
+			{
+				antenna->azimuthAngle = azimuth_step_size *
+					((double)azimuth_step + 0.5);
+
+				CoordinateSwitch antenna_frame_to_gc =
+					AntennaFrameToGC(orbit_state, attitude, antenna);
+
+				//---------------------------------------------------------//
+				// calculate the round trip time for the two-way peak gain //
+				//---------------------------------------------------------//
+
+				double look, azimuth;
+				if (! GetTwoWayPeakGain2(&antenna_frame_to_gc, &spacecraft,
+					beam, antenna->spinRate, &look, &azimuth))
+				{
+					fprintf(stderr, "%s: error finding two-way peak gain\n",
+						command);
+					exit(1);
+				}
+				vector.SphericalSet(1.0, look, azimuth);
+				TargetInfoPackage tip;
+				RangeAndRoundTrip(&antenna_frame_to_gc, &spacecraft,
+					vector, &tip);
+
+				//-----//
+				// sum //
+				//-----//
+
+				rtt_sum += tip.roundTripTime;
+			}
+
+			double avg_rtt = rtt_sum / (double)RANGE_AZIMUTH_STEPS;
+
+			//-----------//
+			// set delay //
+			//-----------//
+
+			range_tracker.SetDelay(beam_idx, orbit_step,
+				beam->receiverGateWidth, beam->pulseWidth, (float)avg_rtt);
+		}
+	}
+
+	//---------------//
+	// set durations //
+	//---------------//
+
+	for (int beam_idx = 0; beam_idx < antenna->numberOfBeams; beam_idx++)
+	{
+		antenna->currentBeamIdx = beam_idx;
+		Beam* beam = antenna->GetCurrentBeam();
+		range_tracker.SetDuration(beam_idx, beam->receiverGateWidth);
+	}
+
+	//---------------------------------------//
+	// write out the receiver gate constants //
+	//---------------------------------------//
+
+	if (! range_tracker.WriteBinary(rgc_file))
+	{
+		fprintf(stderr, "%s: error writing RGC file %s\n", command, rgc_file);
+		exit(1);
+	}
+
+	return (0);
+}
