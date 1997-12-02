534421aca0e9afeef23429b8e6ef8e8e3895809d
bstiles <>
1997-12-02 21:32:36 +0000

    Added residual_delay_error parameter to dopplerTracker and rangeTracker setInstrument routines. BWS

--- objs/AccurateGeom.C
+++ objs/AccurateGeom.C
@@ -66,9 +66,11 @@ IntegrateSlices(
 	// calculate commanded receiver gate delay and duration //
 	//------------------------------------------------------//
 
+	float residual_delay_error=0.0;
 	if (instrument->useRgc)
 	{
-		if (! instrument->rangeTracker.SetInstrument(instrument))
+		if (! instrument->rangeTracker.SetInstrument(instrument,
+					  &residual_delay_error))
 		{
 			fprintf(stderr,
 			"IntegrateSlices: error setting instrument using range tracker\n");
@@ -88,7 +90,8 @@ IntegrateSlices(
 	//---------------------------------------//
 	if (instrument->useDtc)
 	{
-		if (! instrument->dopplerTracker.SetInstrument(instrument))
+		if (! instrument->dopplerTracker.SetInstrument(instrument,
+					  residual_delay_error))
 		{
 			fprintf(stderr,
 			"IntegrateSlices: error setting instrument using Doppler tracker\n");
