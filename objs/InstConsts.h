ded48786d410f31b205fe20a414a740595fea5b5
hudd <>
1998-01-16 01:33:17 +0000

    Initial revision

---
+++ objs/InstConsts.h
@@ -0,0 +1,19 @@
+//==========================================================//
+// Copyright (C) 1998, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+#ifndef INSTCONSTS_H
+#define INSTCONSTS_H
+
+static const char rcs_id_instconsts_h[] =
+	"@(#) $Id$";
+
+#define ORBIT_TICKS_PER_SECOND			32
+#define INSTRUMENT_TICKS_PER_SECOND		32
+#define RX_GATE_DELAY_RESOLUTION		4.9903E-5	// seconds (~0.05 ms)
+#define RX_GATE_WIDTH_RESOLUTION		4.9903E-5	// seconds (~0.05 ms)
+#define TX_PULSE_WIDTH_RESOLUTION		4.9903E-5	// seconds (~0.05 ms)
+#define PRF_CLOCK_RESOLUTION			9.99806E-5	// seconds (~0.1 ms)
+
+#endif
