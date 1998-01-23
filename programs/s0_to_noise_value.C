9f3669d32b5bc50914b3fbcef50466bf90a8ec83
bstiles <>
1998-01-23 21:12:44 +0000

    *** empty log message ***

--- programs/s0_to_noise_value.C
+++ programs/s0_to_noise_value.C
@@ -120,14 +120,17 @@ main(
 		exit(1);
 	}
 
-
+        printf("Tables of values for each wind vector in grid\n");
+        printf("true_wind_speed, s0_bias, s0_rms_error\n");
         /********** Open input and output files ***********/
         FILE* ifp= fopen(argv[2],"r");
         FILE* ofp= fopen(argv[3],"w");
 
 	char string[100], *value;
 	char* file_still_there; 
-        float lon, lat;       
+        float lon, lat;     
+        double bias=0, ave_bias=0, var=0, ave_std=0;  
+        int num_grids=0, num_s0=0;
   while(1){
     file_still_there=fgets(string,100,ifp);   
     if(!file_still_there) break;
@@ -144,6 +147,23 @@ main(
       LonLat lon_lat;
       lon_lat.longitude=lon;
       lon_lat.latitude=lat;
+      if(num_s0 > 20){
+	bias=bias/num_s0;
+        var=var/num_s0;
+        double std;
+        std=sqrt(var);
+        printf("%g %g %g\n",wv.spd,bias,std);
+	ave_std+=fabs(std);
+        ave_bias+=fabs(bias);
+	num_grids++;
+      }
+      else if(num_s0>0){
+	// printf("Too few s0s to calculate grid bias\n\n");
+      }
+      bias=0;
+      num_s0=0;
+      var=0;
+  
       if (! windfield.InterpolatedWindVector(lon_lat, &wv))
 	{
 	  wv.spd = 0.0;
@@ -180,12 +200,28 @@ main(
 				chi, &sigma0_true);
 
       noise_val=(sigma0-sigma0_true)/sigma0_true;
-      printf("%g %g %g\n",sigma0,sigma0_true,noise_val);
+      num_s0++;
+      bias+=noise_val;
+      var+=noise_val*noise_val;
+      //      printf("%g %g %g\n",sigma0,sigma0_true,noise_val);
       fprintf(ofp,"%c %g %g %g\n",polc,incidenceAngle*rtd,eastAzimuth*rtd,noise_val);
     }
   }
+  if(num_s0 > 20){
+    bias=bias/num_s0;
+    var=var/num_s0;
+    double std=sqrt(var);
+    ave_bias+=fabs(bias);
+    ave_std+=std;
+    num_grids++;
+  }
+  ave_bias=ave_bias/num_grids;
+  ave_std=ave_std/num_grids;
+  printf("Average magnitude of the grid-wise noise bias is %g\n",ave_bias);
+  printf("Average grid-wise noise RMS is %g\n",ave_std);
  fclose(ifp);
  fclose(ofp);
+ 
  exit(0);
 }
 
