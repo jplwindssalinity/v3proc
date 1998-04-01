7b82fb37340754d8cf490aef98186f23fe58d76f
richw <>
1998-04-01 22:25:27 +0000

    Initial revision

---
+++ objs/GSparameters.h
@@ -0,0 +1,20 @@
+// Ground System parameters
+#define	wind_start_speed		8.0
+#define	lower_speed_bound		0
+#define	upper_speed_bound		50
+#define	min_incidence_index		40
+#define	max_incidence_index		59
+#define	lower_azimuth_bound		0
+#define	upper_azimuth_bound		90
+#define	wind_max_solutions		10
+#define	wind_speed_band			4
+#define	wind_speed_intv_init	0.5
+#define	wind_dir_intv_init		8.0
+#define wind_dir_intv_opti		2.5
+#define	wind_speed_intv_opti	0.25
+#define	wind_variance_limit		1.0e-9
+#define	MAX_DIR_SAMPLES 		800
+
+#define wind_speed_delta		0.1
+#define wind_dir_delta			5.0
+#define wind_likelihood_delta	0.5
