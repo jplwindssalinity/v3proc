d582c7dd0d95cb8c6fb64bd2aaa5c311a3c2c117
sally <>
1998-10-28 23:17:12 +0000

    Initial revision

---
+++ objs/L1BHdf.C
@@ -0,0 +1,156 @@
+//==============================================================//
+// Copyright (C) 1997-1998, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.					//
+//==============================================================//
+
+static const char rcs_id_l1b_c[] =
+	"@(#) $Id$";
+
+#include <assert.h>
+#include <memory.h>
+
+#include "L1BHdf.h"
+#include "ParTab.h"
+
+
+//========//
+// L1BHdf //
+//========//
+
+L1BHdf::L1BHdf(
+const char*               filename,                 // IN
+HdfFile::StatusE&         returnStatus,             // OUT
+const Itime               startTime,
+const Itime               endTime)
+: L1BHdfFile(filename, returnStatus, startTime, endTime), L1B()
+{
+    // make sure all parent classes are instanciated ok
+    if (HdfFile::_status != HdfFile::OK)
+        return;
+
+    for (int i=0; i < l1bMeasTableSize; i++)
+    {
+        Parameter* param = l1bMeasTable[i].param;
+        param = ParTabAccess::GetParameter(SOURCE_L1B, 
+                          l1bMeasTable[i].paramId, l1bMeasTable[i].unitId);
+        if (param == 0)
+        {
+            returnStatus = HdfFile::_status = ERROR_INVALID_DATASET_NAME;
+            return;
+        }
+
+        // get the first data set name string
+        if (param->sdsIDs == 0)
+        {
+            returnStatus = HdfFile::_status = ERROR_INVALID_DATASET_NAME;
+            return;
+        }
+        char tempString[BIG_SIZE];
+        (void)strncpy(tempString, param->sdsNames, BIG_SIZE);
+        char *oneSdsName= (char*)strtok(tempString, ",");
+        if (oneSdsName == 0)
+        {
+            returnStatus = HdfFile::_status = ERROR_INVALID_DATASET_NAME;
+            return;
+        }
+        int32 dataType=0, startIndex=0, dataLength=0, numDimensions=0;
+        param->sdsIDs[0] = SelectDataset( oneSdsName,  dataType,
+                                 startIndex, dataLength, numDimensions);
+        if (param->sdsIDs[0] == HDF_FAIL)
+        {
+            fprintf(stderr, "Select dataset %s failed\n", oneSdsName);
+            returnStatus = HdfFile::_status = ERROR_SELECTING_DATASET;
+            return;
+        }
+        param->data = (char *)malloc(param->byteSize);
+        if (param->data == 0)
+        {
+            fprintf(stderr, "Out of Memory\n");
+            returnStatus = HdfFile::_status = ERROR_OUT_OF_MEMORY;
+            return;
+        }
+        l1bMeasTable[i].param = param;
+    }
+    returnStatus = HdfFile::_status = HdfFile::OK;
+    return;
+
+} // L1BHdf::L1BHdf
+
+L1BHdf::~L1BHdf()
+{
+    Parameter* param=0;
+    for (int i=0; i < l1bMeasTableSize; i++)
+    {
+        param = l1bMeasTable[i].param;
+        if (param == 0)
+        {
+            (void) CloseDataset(param->sdsIDs[0]);
+            if (param->data != 0)
+            {
+                free((void*) param->data);
+                param->data = 0;
+            }
+            delete param;
+            param = 0;
+        }
+    }
+	return;
+
+} // L1BHdf::~L1BHdf
+
+Parameter*
+L1BHdf::GetParameter(
+ParamIdE      paramId,
+UnitIdE       unitId)
+{
+    for (int i=0; i < l1bMeasTableSize; i++)
+    {
+        if (l1bMeasTable[i].paramId == paramId &&
+                    l1bMeasTable[i].unitId == unitId)
+            return(l1bMeasTable[i].param);
+    }
+    return 0;
+
+} // L1BHdf::GetParameter
+
+int
+L1BHdf::ReadL1BHdfDataRec(void)
+{
+    // get next index if more data
+    int32 index;
+    if (GetNextIndex(index) != HdfFile::OK)
+        return 0;
+
+    return(ReadL1BHdfDataRec(index));
+
+} // L1BHdf::ReadL1BHdfDataRec
+
+int
+L1BHdf::ReadL1BHdfDataRec(
+int32   index)   // next HDF data index
+{
+    Parameter* param = 0;
+    for (int i=0; i < l1bMeasTableSize; i++)
+    {
+        param = l1bMeasTable[i].param;
+        if (param == 0)
+        {
+            fprintf(stderr, "NULL parameter: id = %d, unit = %d\n",
+                           l1bMeasTable[i].paramId, l1bMeasTable[i].unitId);
+            return 0;
+        }
+        int rc = param->extractFunc(this, param->sdsIDs, index,
+                               1, 1, param->data, 0);
+        if (rc <= 0)
+        {
+            fprintf(stderr, "Error occured when extracting L1B: %s\n",
+                               param->paramName);
+            return 0;
+        }
+    }
+printf("bandwidth = %f\n", configBandwidth);
+printf("txPulseWidth = %f\n", configTxPulseWidth);
+
+    return(frame.spotList.UnpackL1BHdf(this, index));
+
+} // L1BHdf::ReadL1BHdfDataRec
---
+++ objs/L1BHdf.h
@@ -0,0 +1,117 @@
+//==============================================================//
+// Copyright (C) 1997-1998, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.					//
+//==============================================================//
+
+#ifndef HDF_L1B_H
+#define HDF_L1B_H
+
+static const char rcs_id_HDF_l1b_h[] =
+	"@(#) $Id$";
+
+#include "L1B.h"
+#include "L1BHdfFile.h"
+
+#ifndef MAX_L1BHDF_NUM_PULSES
+#define MAX_L1BHDF_NUM_PULSES   100
+#endif
+
+#ifndef MAX_L1BHDF_NUM_SLICES
+#define MAX_L1BHDF_NUM_SLICES   8
+#endif
+
+class Parameter;
+
+struct L1BParamEntry
+{
+    ParamIdE    paramId;
+    UnitIdE     unitId;
+    Parameter*  param;
+};
+
+static L1BParamEntry l1bMeasTable[] =
+{
+    { ORBIT_TIME, UNIT_COUNTS },
+    { NUM_PULSES, UNIT_DN },
+    { SC_LAT, UNIT_RADIANS },
+    { SC_LON, UNIT_RADIANS },
+    { SC_ALT, UNIT_KILOMETERS },
+    { X_POS, UNIT_KILOMETERS },
+    { Y_POS, UNIT_KILOMETERS },
+    { Z_POS, UNIT_KILOMETERS },
+    { X_VEL, UNIT_KMPS },
+    { Y_VEL, UNIT_KMPS },
+    { Z_VEL, UNIT_KMPS },
+    { ROLL, UNIT_RADIANS },
+    { PITCH, UNIT_RADIANS },
+    { YAW, UNIT_RADIANS },
+    { BANDWIDTH_RATIO, UNIT_DB },
+    { X_CAL_A, UNIT_DB },
+    { X_CAL_B, UNIT_DB },
+    { CELL_LAT, UNIT_RADIANS },
+    { CELL_LON, UNIT_RADIANS },
+    { SIGMA0, UNIT_DB },
+    { FREQUENCY_SHIFT, UNIT_HZ },
+    { CELL_AZIMUTH, UNIT_RADIANS },
+    { CELL_INCIDENCE, UNIT_RADIANS },
+    { ANTENNA_AZIMUTH, UNIT_RADIANS },
+    { SNR, UNIT_DB },
+    { KPC_A, UNIT_DN },
+    { SLICE_LAT, UNIT_RADIANS },
+    { SLICE_LON, UNIT_RADIANS },
+    { SLICE_SIGMA0, UNIT_DB },
+    { X_FACTOR, UNIT_DB },
+    { SLICE_AZIMUTH, UNIT_RADIANS },
+    { SLICE_INCIDENCE, UNIT_RADIANS },
+    { SLICE_SNR, UNIT_DB },
+    { SLICE_KPC_A, UNIT_DN },
+    { FRAME_INST_STATUS_02, UNIT_MAP },
+    { FRAME_INST_STATUS_02, UNIT_MAP },
+    { SLICE_QUAL_FLAG, UNIT_DN },
+};
+
+const int l1bMeasTableSize = ElementNumber(l1bMeasTable);
+
+//======================================================================
+// CLASS
+//		L1BHdf
+//
+// DESCRIPTION
+//		The L1BHdf object allows for the easy writing, reading, and
+//		manipulating of Level 1B HDF data.
+//
+//      input file:  HDF L1B file
+//      output file: SVT L1B file
+//======================================================================
+
+class L1BHdf : public L1BHdfFile, public L1B
+{
+public:
+
+	//--------------//
+	// construction //
+	//--------------//
+
+    L1BHdf(const char*         filename,                 // IN
+           HdfFile::StatusE&   returnStatus,             // OUT
+           const Itime         startTime = INVALID_TIME, // IN: BOF if invalid
+           const Itime         endTime = INVALID_TIME);  // IN: EOF if invalid
+
+	virtual ~L1BHdf();
+
+    Parameter*  GetParameter(ParamIdE, UnitIdE);
+
+	//--------------//
+	// input/output //
+	//--------------//
+
+	int		ReadL1BHdfDataRec(void);        // get data at next index
+	int		ReadL1BHdfDataRec(int32 index); // get data at specific index
+
+    float   configBandwidth;                // band width from config file
+    float   configTxPulseWidth;             // tx pulse width from config file
+
+protected:
+};
+
+#endif
