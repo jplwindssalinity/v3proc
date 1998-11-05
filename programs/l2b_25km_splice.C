6cf27adb59a2bdd6c2eff78d2c8c4ffb6ec34e8c
bstiles <>
1998-11-05 16:33:49 +0000

    Initial revision

---
+++ programs/l2b_25km_splice.C
@@ -0,0 +1,230 @@
+//==============================================================//
+// Copyright (C) 1997-1998, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.					//
+//==============================================================//
+
+//----------------------------------------------------------------------
+// NAME
+//		l2b_splice
+//
+// SYNOPSIS
+//		l2b_splice inner_swath_file outer_swath_file output_file
+//                         threshold_distance
+//
+// DESCRIPTION
+//              Puts together two l2b files taking form one for the
+//              inner swath and the other for the outer swath
+//
+// OPTIONS
+//		None.
+//
+// OPERANDS
+//		
+//		First three are self-explanatory.
+//              threshold_distance: swath is broken into inner between
+//              -threshold_distance and + threshold distance and outer
+//              everything else
+//
+// EXAMPLES
+//		An example of a command line is:
+//			% l2b_splice  in.l2b.dat out.l2b.dat l2b.dat 500
+//
+// ENVIRONMENT
+//		Not environment dependent.
+//
+// EXIT STATUS
+//		The following exit values are returned:
+//		0	Program executed successfully
+//		>0	Program had an error
+//
+// NOTES
+//		None.
+//
+// AUTHOR
+//		James N. Huddleston
+//		hudd@acid.jpl.nasa.gov
+//----------------------------------------------------------------------
+
+//-----------------------//
+// Configuration Control //
+//-----------------------//
+
+static const char rcs_id[] =
+	"@(#) $Id$";
+
+//----------//
+// INCLUDES //
+//----------//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include "List.h"
+#include "List.C"
+#include "BufferedList.h"
+#include "BufferedList.C"
+#include "Misc.h"
+#include "ConfigList.h"
+#include "L2A.h"
+#include "ConfigSim.h"
+#include "L2B.h"
+#include "L2AToL2B.h"
+#include "Tracking.h"
+#include "Tracking.C"
+
+
+//-----------//
+// TEMPLATES //
+//-----------//
+
+// Class declarations needed for templates
+// eliminates need to include the entire header file
+class AngleInterval;
+
+template class List<AngleInterval>;
+template class List<StringPair>;
+template class List<Meas>;
+template class List<EarthPosition>;
+template class List<WindVectorPlus>;
+template class List<MeasSpot>;
+template class BufferedList<OrbitState>;
+template class List<OrbitState>;
+template class List<long>;
+template class List<OffsetList>;
+template class TrackerBase<unsigned char>;
+template class TrackerBase<unsigned short>;
+
+//-----------//
+// CONSTANTS //
+//-----------//
+
+//--------//
+// MACROS //
+//--------//
+
+//------------------//
+// TYPE DEFINITIONS //
+//------------------//
+
+//-----------------------//
+// FUNCTION DECLARATIONS //
+//-----------------------//
+
+//------------------//
+// OPTION VARIABLES //
+//------------------//
+
+//------------------//
+// GLOBAL VARIABLES //
+//------------------//
+
+const char* usage_array[] = { "<inner_swath_file>", "<output_swath_file>", 
+                              "<output_file>","<threshold_distance>"};
+
+
+//--------------//
+// MAIN PROGRAM //
+//--------------//
+
+int
+main(
+	int		argc,
+	char*	argv[])
+{
+	//------------------------//
+	// parse the command line //
+	//------------------------//
+
+	const char* command = no_path(argv[0]);
+	if (argc != 5)
+		usage(command, usage_array, 1);
+
+	int clidx = 1;
+	const char* inner_file = argv[clidx++];
+	const char* outer_file = argv[clidx++];
+        const char* output_file = argv[clidx++];
+        float threshold=atof(argv[clidx++]);
+
+
+	//-------------------------------------//
+	// create and configure level products //
+	//-------------------------------------//
+
+	L2B l2b_in, l2b_out;
+        
+
+	// Overwrite with new name
+        l2b_out.SetOutputFilename(output_file);
+        l2b_in.SetInputFilename(inner_file);
+        l2b_out.SetInputFilename(outer_file);
+
+
+        //------------//
+	// open files //
+	//------------//
+
+	if(!l2b_out.OpenForReading() ||
+	   !l2b_out.OpenForWriting() ||
+	   !l2b_in.OpenForReading()){
+	  fprintf(stderr,"%s: Error opening file\n",command);
+	  exit(1);
+	}
+
+	//---------------------------------//
+	// read the headers to set up swath //
+	//---------------------------------//
+
+	if (! l2b_in.ReadHeader())
+	{
+		fprintf(stderr, "%s: error reading Level 2B header\n", command); 
+		exit(1);
+	}
+
+	if (! l2b_out.ReadHeader())
+	{
+		fprintf(stderr, "%s: error reading Level 2B header\n", command); 
+		exit(1);
+	}
+
+
+	//--------------------------------//
+	// read the level 2B data records //
+	//--------------------------------//
+
+	if (! l2b_in.ReadDataRec())
+	  {
+		fprintf(stderr, "%s: error reading Level 2B data\n", command);
+		exit(1);
+	  }
+
+	if (! l2b_out.ReadDataRec())
+	  {
+		fprintf(stderr, "%s: error reading Level 2B data\n", command);
+		exit(1);
+	  }
+
+	//----------------------------------------------------//
+	// splice  Right now only works for 25km resolution   //
+	//----------------------------------------------------//
+        // Determine edge_indices
+        float radius=threshold/25.0;
+        int left=(int)ceil(39.5-radius);
+        int right=(int)floor(39.5+radius);
+ 	for (int cti = left; cti <= right; cti++)
+	  {   
+	    for(int ati=0;ati<1603;ati++){
+              WVC* wvc=l2b_out.frame.swath.Remove(cti,ati);
+              if(wvc) delete wvc;
+              wvc=l2b_in.frame.swath.Remove(cti,ati);
+              if(wvc) l2b_out.frame.swath.Add(cti,ati,wvc);
+	    }
+	  }
+       
+	l2b_out.WriteHeader();
+	l2b_out.WriteDataRec();
+        l2b_in.Close();	
+	l2b_out.Close();
+	
+	return(0);
+}
---
+++ programs/l2b_extract_worst.C
@@ -0,0 +1,217 @@
+//==============================================================//
+// Copyright (C) 1997-1998, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.					//
+//==============================================================//
+
+//----------------------------------------------------------------------
+// NAME
+//		l2b_extract_worst
+//
+// SYNOPSIS
+//		l2b_diff_from_truth <config_file> 
+//
+// DESCRIPTION
+//              Locates worst wind vectors in the file 
+//              for each cross_track_index.
+//
+// OPTIONS
+//		None.
+//
+// OPERANDS
+//		The following operands are supported:
+//		<config_file>	Simulation Configuration file
+//
+// EXAMPLES
+//		An example of a command line is:
+//			% l2b_extract_worst quikscat.cfg
+//
+// ENVIRONMENT
+//		Not environment dependent.
+//
+// EXIT STATUS
+//		The following exit values are returned:
+//		0	Program executed successfully
+//		>0	Program had an error
+//
+// NOTES
+//		None.
+//
+// AUTHOR
+//		James N. Huddleston
+//		hudd@acid.jpl.nasa.gov
+//----------------------------------------------------------------------
+
+//-----------------------//
+// Configuration Control //
+//-----------------------//
+
+static const char rcs_id[] =
+	"@(#) $Id$";
+
+//----------//
+// INCLUDES //
+//----------//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include "List.h"
+#include "List.C"
+#include "BufferedList.h"
+#include "BufferedList.C"
+#include "Misc.h"
+#include "ConfigList.h"
+#include "L2A.h"
+#include "ConfigSim.h"
+#include "ConfigSimDefs.h"
+#include "L2B.h"
+#include "L2AToL2B.h"
+#include "Tracking.h"
+#include "Tracking.C"
+
+
+//-----------//
+// TEMPLATES //
+//-----------//
+
+// Class declarations needed for templates
+// eliminates need to include the entire header file
+class AngleInterval;
+
+template class List<AngleInterval>;
+template class List<StringPair>;
+template class List<Meas>;
+template class List<EarthPosition>;
+template class List<WindVectorPlus>;
+template class List<MeasSpot>;
+template class BufferedList<OrbitState>;
+template class List<OrbitState>;
+template class List<long>;
+template class List<OffsetList>;
+template class TrackerBase<unsigned char>;
+template class TrackerBase<unsigned short>;
+
+
+
+//-----------//
+// CONSTANTS //
+//-----------//
+
+//--------//
+// MACROS //
+//--------//
+
+//------------------//
+// TYPE DEFINITIONS //
+//------------------//
+
+//-----------------------//
+// FUNCTION DECLARATIONS //
+//-----------------------//
+
+//------------------//
+// OPTION VARIABLES //
+//------------------//
+
+//------------------//
+// GLOBAL VARIABLES //
+//------------------//
+
+const char* usage_array[] = { "<config_file>", 0};
+
+//--------------//
+// MAIN PROGRAM //
+//--------------//
+
+int
+main(
+	int		argc,
+	char*	argv[])
+{
+	//------------------------//
+	// parse the command line //
+	//------------------------//
+
+	const char* command = no_path(argv[0]);
+	if (argc != 2)
+		usage(command, usage_array, 1);
+
+	int clidx = 1;
+	const char* config_file = argv[clidx++];
+
+	//---------------------//
+	// read in config file //
+	//---------------------//
+
+	ConfigList config_list;
+	if (! config_list.Read(config_file))
+	{
+		fprintf(stderr, "%s: error reading sim config file %s\n",
+			command, config_file);
+		exit(1);
+	}
+
+	//-------------------------------------//
+	// create and configure level products //
+	//-------------------------------------//
+
+	L2B l2b;
+	if (! ConfigL2B(&l2b, &config_list))
+	{
+		fprintf(stderr, "%s: error configuring Level 2B Product\n", command);
+		exit(1);
+	}
+
+	//--------------------------------------//
+	// read in  truth windfield             //
+	//--------------------------------------//
+	char* truth_type = config_list.Get(WINDFIELD_TYPE_KEYWORD);
+	if (truth_type == NULL)
+	  {
+	    fprintf(stderr, "%s: must specify truth windfield type\n",
+				command);
+	    exit(1);
+	  }
+	
+        char* truth_file = config_list.Get(WINDFIELD_FILE_KEYWORD);
+	if (truth_file == NULL)
+	  {
+	    fprintf(stderr, "%s: must specify truth windfield file\n",
+		    command);
+	    exit(1);
+	  }
+	WindField truth;
+	truth.ReadType(truth_file, truth_type);
+
+
+	//------------------//
+	// read in l2b file //
+	//------------------//
+
+	if (! l2b.OpenForReading())
+	{
+		fprintf(stderr, "%s: error opening L2B file\n",command);
+		exit(1);
+	}
+	if (! l2b.ReadHeader())
+	{
+		fprintf(stderr, "%s: error reading L2B header from file \n",
+			command);
+		exit(1);
+	}
+
+	if (! l2b.ReadDataRec())
+	{
+		fprintf(stderr, "%s: error reading L2B data record from file \n",
+			command);
+		exit(1);
+	}
+
+	//----------------------//
+        // compute difference & //
+	// write out vctr files //
+	//----------------------//
+        l2b.frame.swath.WriteMaxDirErrIndices(&truth,stdout);
+	return (0);
+}
