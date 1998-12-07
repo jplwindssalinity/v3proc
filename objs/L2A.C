65a7bd0f3e2ba62a4c08a541ffe0e9f48907d468
hudd <>
1998-12-07 22:32:32 +0000

    *** empty log message ***

--- objs/L2A.C
+++ objs/L2A.C
@@ -96,6 +96,7 @@ L2AHeader::WriteAscii(
 //==========//
 
 L2AFrame::L2AFrame()
+:   rev(0), ati(0), cti(0)
 {
     return;
 }
@@ -482,14 +483,14 @@ L2AFrame::CopyFrame(
 //=====//
 
 L2A::L2A()
-:	_status(OK), _headerRead(0), _headerWritten(0)
+:   _status(OK), _headerRead(0), _headerWritten(0), idx(0)
 {
-	return;
+    return;
 }
 
 L2A::~L2A()
 {
-	return;
+    return;
 }
 
 //------------------//
@@ -499,14 +500,14 @@ L2A::~L2A()
 int
 L2A::WriteHeader()
 {
-	if (_outputFp == NULL)
-		return(0);
+    if (_outputFp == NULL)
+        return(0);
 
-	if (! header.Write(_outputFp))
-		return(0);
+    if (! header.Write(_outputFp))
+        return(0);
 
-	_headerWritten = 1;
-	return(1);
+    _headerWritten = 1;
+    return(1);
 }
 
 //------------------//
@@ -516,14 +517,14 @@ L2A::WriteHeader()
 int
 L2A::WriteHeaderAscii()
 {
-	if (_outputFp == NULL)
-		return(0);
+    if (_outputFp == NULL)
+        return(0);
 
-	if (! header.WriteAscii(_outputFp))
-		return(0);
+    if (! header.WriteAscii(_outputFp))
+        return(0);
 
-	_headerWritten = 1;
-	return(1);
+    _headerWritten = 1;
+    return(1);
 }
 
 //-----------------//
@@ -533,14 +534,14 @@ L2A::WriteHeaderAscii()
 int
 L2A::ReadHeader()
 {
-	if (_inputFp == NULL)
-		return(0);
+    if (_inputFp == NULL)
+        return(0);
 
-	if (! header.Read(_inputFp))
-		return(0);
+    if (! header.Read(_inputFp))
+        return(0);
 
-	_headerRead = 1;
-	return(1);
+    _headerRead = 1;
+    return(1);
 }
 
 //------------------//
@@ -550,83 +551,80 @@ L2A::ReadHeader()
 int
 L2A::ReadDataRec()
 {
-	if (_inputFp == NULL) return(0);
+    if (_inputFp == NULL) return(0);
 
-	if (! _headerRead)
-	{
-		if (! header.Read(_inputFp))
-			return(0);
-		_headerRead = 1;
-	}
-	if (! frame.Read(_inputFp))
-		return(0);
+    if (! _headerRead)
+    {
+        if (! header.Read(_inputFp))
+            return(0);
+        _headerRead = 1;
+    }
+    if (! frame.Read(_inputFp))
+        return(0);
 
-	return(1);
+    return(1);
 }
 
 //-------------------//
 // L2A::ReadGroupRec //
 //-------------------//
 
-int 
+int
 L2A::ReadGroupRec(
      int rset,
      L2AFrame* frameGroup25,
      L2AFrame* frameGroup50)
-  
+
 {
+    if (_inputFp == NULL)
+        return(0);
 
-  if (_inputFp == NULL) return(0);
+    // read header
 
-  // read header
-
-  if (! _headerRead)
+    if (! _headerRead)
     {
-      if (! header.Read(_inputFp))
-	  return(0);
-      _headerRead = 1;
+        if (! header.Read(_inputFp))
+            return(0);
+        _headerRead = 1;
     }
 
-  // previous reads or first call?
-  
+    // previous reads or first call?
 
-  if (! rset) 
+    if (! rset)
     {
-      if (! frameGroup25[0].Read(_inputFp))
-	return(0);
-      rset=1;
+        if (! frameGroup25[0].Read(_inputFp))
+            return(0);
+        rset = 1;
     }
 
-  // read frames 1 to ctb-1 for this ati row and next ati row
+    // read frames 1 to ctb-1 for this ati row and next ati row
 
-  idx = 0;
-  L2AFrame readFrame;
-      
-  while (frameGroup25[idx].ati < frameGroup25[0].ati + 2)
+    idx = 0;
+    L2AFrame readFrame;
+
+    while (frameGroup25[idx].ati < frameGroup25[0].ati + 2)
     {
-      idx++;
-      if (! readFrame.Read(_inputFp))
-	return(0);
-      frame.CopyFrame(&frameGroup25[idx], &readFrame);
-
-    }
-  
-  // CombineFrames returns the number of 50km frames (and the combined frames)
-
-  int nFrames=frame.CombineFrames(frameGroup25, frameGroup50);
-
-  if (nFrames == 0)
-    {
-      nFrames=1;
-      return(0);
+        idx++;
+        if (! readFrame.Read(_inputFp))
+            return(0);
+        frame.CopyFrame(&frameGroup25[idx], &readFrame);
     }
 
-  // copy over next frame (from next ati row) and return
+    // CombineFrames returns the number of 50km frames (and the combined frames)
 
-  frame.CopyFrame(&frameGroup25[0], &frameGroup25[idx]);
+    int nFrames = frame.CombineFrames(frameGroup25, frameGroup50);
 
-  return(nFrames);
+    if (nFrames == 0)
+    {
+        nFrames=1;
+        return(0);
+    }
 
+    // copy over next frame (from next ati row) and return
+
+    frame.CopyFrame(&frameGroup25[0], &frameGroup25[idx]);
+
+    return(nFrames);
 }
 
 //-------------------//
@@ -636,19 +634,19 @@ L2A::ReadGroupRec(
 int
 L2A::WriteDataRec()
 {
-	if (_outputFp == NULL) return(0);
+    if (_outputFp == NULL) return(0);
 
-	if (! _headerWritten)
-	{
-		if (! header.Write(_outputFp))
-			return(0);
-		_headerWritten = 1;
-	}
+    if (! _headerWritten)
+    {
+        if (! header.Write(_outputFp))
+            return(0);
+        _headerWritten = 1;
+    }
 
-	if (! frame.Write(_outputFp))
-		return(0);
+    if (! frame.Write(_outputFp))
+        return(0);
 
-	return(1);
+    return(1);
 }
 
 //------------------------//
@@ -658,19 +656,19 @@ L2A::WriteDataRec()
 int
 L2A::WriteDataRecAscii()
 {
-	if (_outputFp == NULL) return(0);
+    if (_outputFp == NULL) return(0);
 
-	if (! _headerWritten)
-	{
-		if (! header.WriteAscii(_outputFp))
-			return(0);
-		_headerWritten = 1;
-	}
+    if (! _headerWritten)
+    {
+        if (! header.WriteAscii(_outputFp))
+            return(0);
+        _headerWritten = 1;
+    }
 
-	if (! frame.WriteAscii(_outputFp))
-		return(0);
+    if (! frame.WriteAscii(_outputFp))
+        return(0);
 
-	return(1);
+    return(1);
 }
 
 //--------------------//
@@ -680,10 +678,10 @@ L2A::WriteDataRecAscii()
 int
 L2A::ReadGSDataRec()
 {
-	if (_inputFp == NULL) return(0);
+    if (_inputFp == NULL) return(0);
 
-	if (! frame.ReadGS(_inputFp))
-		return(0);
+    if (! frame.ReadGS(_inputFp))
+        return(0);
 
-	return(1);
+    return(1);
 }
