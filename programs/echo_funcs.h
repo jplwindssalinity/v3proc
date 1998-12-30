a201e20de9b3c4c1d162aec4ac8cd1902a57ecc3
hudd <>
1998-12-30 01:22:46 +0000

    *** empty log message ***

--- programs/echo_funcs.h
+++ programs/echo_funcs.h
@@ -444,7 +444,7 @@ gfit_eval2(
     double fy_sum = 0.0;
 
     for (int i = 0; i < points; i++)
-    { 
+    {
         double ex = (x[i] - c[0]) / c[1];
         double val = exp(-ex * ex);
         hold[i] = val;
