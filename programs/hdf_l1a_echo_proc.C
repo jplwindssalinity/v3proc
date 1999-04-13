069e9aae85648a77cc487d0ed8fea225081d8fb1
hudd <>
1999-04-13 17:02:42 +0000

    *** empty log message ***

--- programs/hdf_l1a_echo_proc.C
+++ programs/hdf_l1a_echo_proc.C
@@ -68,6 +68,12 @@ static const char rcs_id[] =
 #include "BufferedList.C"
 #include "Tracking.h"
 #include "Tracking.C"
+#include "InstrumentGeom.h"
+#include "ConfigSim.h"
+#include "QscatConfig.h"
+#include "Qscat.h"
+#include "QscatSim.h"
+#include "echo_funcs.h"
 
 //-----------//
 // TEMPLATES //
@@ -89,6 +95,9 @@ template class TrackerBase<unsigned char>;
 //-----------//
 // CONSTANTS //
 //-----------//
+
+// if data is within KM_RANGE of land, flag it as land
+#define KM_RANGE  100.0
 
 //--------//
 // MACROS //
@@ -114,8 +123,6 @@ int ExtractAntSpinRateDnPer99(TlmHdfFile* l1File, int32* sdsIDs,
 // GLOBAL VARIABLES //
 //------------------//
 
-const char* usage_array[] = { "<sim_config_file>", "<output_echo_file>", 0};
-
 ArgInfo tlm_files_arg = TLM_FILES_ARG;
 ArgInfo l1a_files_arg = L1A_FILES_ARG;
 ArgInfo start_time_arg = START_TIME_ARG;
@@ -140,6 +147,10 @@ ArgInfo* arg_info_array[] =
     &ins_config_arg,
     0
 };
+
+// approximate ranges used for land flag check
+float dlon_km[4] = { KM_RANGE, 0.0, -KM_RANGE, 0.0 };
+float dlat_km[4] = { 0.0, KM_RANGE, 0.0, -KM_RANGE };
 
 //--------------//
 // MAIN PROGRAM //
@@ -170,7 +181,7 @@ main(
     char* start_time_string = args_plus.Get(start_time_arg);
     char* end_time_string = args_plus.Get(end_time_arg);
     char* filter_string = args_plus.Get(filter_arg);
-//    char* output_file_string = args_plus.Get(output_file_arg);
+    char* output_file_string = args_plus.Get(output_file_arg);
     char* poly_table_string = args_plus.Get(poly_table_arg);
     char* ins_config_string = args_plus.Get(ins_config_arg);
 
@@ -188,12 +199,13 @@ main(
     // use arguments //
     //---------------//
 
+    int output_fd = args_plus.OutputFdOrStdout(output_file_string);
     TlmFileList* tlm_file_list = args_plus.TlmFileListOrExit(tlm_type,
         tlm_files, start_time, end_time);
-//    FILE* output_fp = args_plus.OutputFileOrStdout(output_file_string);
     FilterSet* filter_set = args_plus.FilterSetOrNull(tlm_type, filter_string);
     PolynomialTable* polyTable =
         args_plus.PolynomialTableOrNull(poly_table_string);
+
 
     char* no_gr_header_string = args_plus.Get(no_gr_header_arg);
     int no_gr_header = 0;
@@ -206,7 +218,6 @@ main(
     // read in config file //
     //---------------------//
 
-/*
     if (! ins_config_string)
     {
         fprintf(stderr, "%s: missing instrument configuration file\n",
@@ -220,7 +231,15 @@ main(
             command, ins_config_string);
         exit(1);
     }
-*/
+
+    //-----------//
+    // variables //
+    //-----------//
+
+    // calculate energy for all 12, but only fit to center 10
+    double signal_energy[12];
+    double slice_number[10] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,
+        10.0 };    // ignore slice 0 and slice 11
 
     //---------------------------------//
     // create and configure spacecraft //
@@ -228,6 +247,30 @@ main(
 
     Spacecraft spacecraft;
     spacecraft.attitude.SetOrder(2, 1, 3);
+    Attitude* attitude = &(spacecraft.attitude);
+
+    //----------------------------//
+    // create and configure Qscat //
+    //----------------------------//
+
+    Qscat qscat;
+    if (! ConfigQscat(&qscat, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring QSCAT\n", command);
+        exit(1);
+    }
+    Antenna* antenna = &(qscat.sas.antenna);
+    double Bn = qscat.ses.noiseBandwidth;
+    double Be = qscat.ses.GetTotalSignalBandwidth();
+    double Bs = qscat.ses.scienceSliceBandwidth;
+
+    QscatSim qscat_sim;
+    if (! ConfigQscatSim(&qscat_sim, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring instrument simulator\n",
+            command);
+        exit(1);
+    }
 
     //--------------------------//
     // get necessary parameters //
@@ -255,11 +298,32 @@ main(
         UNIT_RADIANS);
 
     Parameter* ant_pos_p = ParTabAccess::GetParameter(SOURCE_L1A,
-        ANT_SPIN_RATE, UNIT_DN);
+        ANTENNA_POS, UNIT_DN);
+
+    Parameter* orbit_time_p = ParTabAccess::GetParameter(SOURCE_L1A,
+        ORBIT_TIME, UNIT_COUNTS);
+    Parameter* orbit_step_p = ParTabAccess::GetParameter(SOURCE_L1A,
+        DOPPLER_ORBIT_STEP, UNIT_COUNTS);
+    Parameter* pri_of_orbit_step_change_p =
+        ParTabAccess::GetParameter(SOURCE_L1A, PRF_ORBIT_STEP_CHANGE,
+        UNIT_COUNTS);
+
+    Parameter* cal_pulse_pos_p = ParTabAccess::GetParameter(SOURCE_L1A,
+        CAL_PULSE_POS, UNIT_DN);
+
+    Parameter* slice_powers_p = ParTabAccess::GetParameter(SOURCE_L1A,
+        POWER_DN, UNIT_DN);
+    Parameter* noise_meas_p = ParTabAccess::GetParameter(SOURCE_L1A,
+        NOISE_DN, UNIT_DN);
+
+    Parameter* prf_cycle_time_p = ParTabAccess::GetParameter(SOURCE_L1A,
+        PRF_CYCLE_TIME, UNIT_DN);
 
     //---------------------------//
     // process data file by file //
     //---------------------------//
+
+    EchoInfo echo_info;
 
 //    int dataRecCount = 0;
     for (TlmHdfFile* tlmFile = tlm_file_list->GetHead(); tlmFile;
@@ -279,10 +343,20 @@ main(
         status = tlmFile->OpenParamDatasets(roll_p);
         status = tlmFile->OpenParamDatasets(pitch_p);
         status = tlmFile->OpenParamDatasets(yaw_p);
+        status = tlmFile->OpenParamDatasets(ant_pos_p);
+        status = tlmFile->OpenParamDatasets(orbit_time_p);
+        status = tlmFile->OpenParamDatasets(orbit_step_p);
+        status = tlmFile->OpenParamDatasets(pri_of_orbit_step_change_p);
+        status = tlmFile->OpenParamDatasets(cal_pulse_pos_p);
+        status = tlmFile->OpenParamDatasets(slice_powers_p);
+        status = tlmFile->OpenParamDatasets(prf_cycle_time_p);
+        status = tlmFile->OpenParamDatasets(noise_meas_p);
 
         //------------------------//
         // process frame by frame //
         //------------------------//
+
+        int in_first_frame = 1;
 
         for (int i = 0; i < tlmFile->GetDataLength(); i++)
         {
@@ -316,33 +390,245 @@ main(
 
             spacecraft.orbitState.rsat.Set(xpos, ypos, zpos);
             spacecraft.orbitState.vsat.Set(xvel, yvel, zvel);
-            spacecraft.attitude.SetRPY(roll, pitch, yaw);
+            attitude->SetRPY(roll, pitch, yaw);
+
+            //------------------------//
+            // command the instrument //
+            //------------------------//
+
+            unsigned char pri_dn;
+            prf_cycle_time_p->extractFunc(tlmFile, prf_cycle_time_p->sdsIDs,
+                i, 1, 1, &pri_dn, polyTable);
+
+            qscat.ses.CmdPriDn(pri_dn);
 
             //------------------------//
             // estimate the spin rate //
             //------------------------//
 
-            unsigned short spin_rate;
-            ExtractAntSpinRateDnPer99(tlmFile, ant_pos_p->sdsIDs, i, 1, 1,
-                &spin_rate, polyTable);
-printf("%d\n", (int)spin_rate);
+            unsigned int ant_pos[100];
+            ant_pos_p->extractFunc(tlmFile, ant_pos_p->sdsIDs, i, 1, 1,
+                ant_pos, polyTable);
 
-/*
-            unsigned int theta_max =
-                *(frame->antennaPosition + frame->spotsPerFrame - 1);
-        unsigned int theta_min =
-            *(frame->antennaPosition + 0);
-        while (theta_max < theta_min)
-        {
-            theta_max += ENCODER_N;
-        }
-        unsigned int delta_theta = theta_max - theta_min;
-        double delta_theta_rad = two_pi * (double)delta_theta /
-            (double)ENCODER_N;
-        double delta_t = (double)(frame->spotsPerFrame - 1) * qscat.ses.pri;
-        double omega = delta_theta_rad / delta_t;
-        qscat.sas.antenna.spinRate = omega;
-*/
+            unsigned int theta_max = ant_pos[99];
+            unsigned int theta_min = ant_pos[0];
+            if (theta_max < theta_min)
+            {
+                theta_max += ENCODER_N;
+            }
+            unsigned int dn_per_99 = theta_max - theta_min;
+            double rad_per_99 = two_pi * (double)dn_per_99 / (double)ENCODER_N;
+            double sec_per_99 = PRI_CMD_RESOLUTION * qscat.ses.pri * 99.0;
+            double omega = rad_per_99 / sec_per_99;
+
+            //-----------------------//
+            // get other information //
+            //-----------------------//
+
+            unsigned int orbit_time;
+            orbit_time_p->extractFunc(tlmFile, orbit_time_p->sdsIDs, i, 1, 1,
+                &orbit_time, polyTable);
+
+            unsigned char orbit_step;
+            orbit_step_p->extractFunc(tlmFile, orbit_step_p->sdsIDs, i, 1, 1,
+                &orbit_step, polyTable);
+
+            unsigned char pri_of_orbit_step_change;
+            pri_of_orbit_step_change_p->extractFunc(tlmFile,
+                pri_of_orbit_step_change_p->sdsIDs, i, 1, 1,
+                &pri_of_orbit_step_change, polyTable);
+
+            unsigned char cal_pulse_pos;
+            cal_pulse_pos_p->extractFunc(tlmFile, cal_pulse_pos_p->sdsIDs,
+                i, 1, 1, &cal_pulse_pos, polyTable);
+
+            unsigned int slice_powers[1200];
+            slice_powers_p->extractFunc(tlmFile, slice_powers_p->sdsIDs, i,
+                1, 1, slice_powers, polyTable);
+            unsigned int noise_powers[100];
+            noise_meas_p->extractFunc(tlmFile, noise_meas_p->sdsIDs, i, 1, 1,
+                noise_powers, polyTable);
+
+            //-----------------------------------------------//
+            // set the ephemeris, orbit time, and orbit step //
+            //-----------------------------------------------//
+
+            echo_info.gcX = xpos;
+            echo_info.gcY = ypos;
+            echo_info.gcZ = zpos;
+            echo_info.velX = xvel;
+            echo_info.velY = yvel;
+            echo_info.velZ = zvel;
+            echo_info.roll = roll;
+            echo_info.pitch = pitch;
+            echo_info.yaw = yaw;
+            echo_info.orbitTicks = orbit_time;
+            echo_info.orbitStep = orbit_step;
+            echo_info.priOfOrbitStepChange = pri_of_orbit_step_change;
+            echo_info.spinRate = omega;
+
+            //--------------------//
+            // step through spots //
+            //--------------------//
+
+            for (int spot_idx = 0; spot_idx < 100; spot_idx++)
+            {
+                echo_info.flag[spot_idx] = EchoInfo::OK;
+
+                // determine beam index and beam
+                int beam_idx = spot_idx % NUMBER_OF_QSCAT_BEAMS;
+                echo_info.beamIdx[spot_idx] = (unsigned char)beam_idx;
+                qscat.cds.currentBeamIdx = beam_idx;
+                Beam* beam = qscat.GetCurrentBeam();
+
+                // determine starting slice index
+                int base_slice_idx = spot_idx * 12;
+
+                // determine orbit step
+                qscat.cds.orbitTime = orbit_time;
+                if (pri_of_orbit_step_change != 255 &&
+                    spot_idx < pri_of_orbit_step_change)
+                {
+                    if (orbit_step == 0)
+                        orbit_step = ORBIT_STEPS - 1;
+                    else
+                        orbit_step--;
+                }
+                qscat.cds.orbitStep = orbit_step;
+
+                // set the encoder for tracking
+                unsigned short held_encoder = ant_pos[spot_idx];
+                qscat.cds.heldEncoder = held_encoder;
+
+                // get the ideal encoder for output
+                unsigned short ideal_encoder = qscat.cds.EstimateIdealEncoder();
+                echo_info.idealEncoder[spot_idx] = ideal_encoder;
+
+                //-------------------------------//
+                // do range and Doppler tracking //
+                //-------------------------------//
+
+                qscat.SetEncoderAzimuth(held_encoder, 1);
+                qscat.SetOtherAzimuths(&spacecraft);
+                echo_info.txCenterAzimuthAngle[spot_idx] =
+                    InRange(antenna->txCenterAzimuthAngle);
+
+                SetDelayAndFrequency(&spacecraft, &qscat);
+
+                echo_info.txDoppler[spot_idx] = qscat.ses.txDoppler;
+                echo_info.rxGateDelay[spot_idx] = qscat.ses.rxGateDelay;
+
+                //------------------------------//
+                // skip pulses from first frame //
+                //------------------------------//
+
+                if (in_first_frame)
+                    continue;
+
+                //-------------------------//
+                // skip calibration pulses //
+                //-------------------------//
+
+                if (spot_idx == cal_pulse_pos - 2 ||
+                    spot_idx == cal_pulse_pos - 1)
+                {
+                    echo_info.flag[spot_idx] = EchoInfo::CAL_OR_LOAD_PULSE;
+                    continue;
+                }
+
+                //-----------//
+                // flag land //
+                //-----------//
+
+                CoordinateSwitch antenna_frame_to_gc =
+                    AntennaFrameToGC(&(spacecraft.orbitState),
+                    attitude, antenna, antenna->groundImpactAzimuthAngle);
+
+                double look, azim;
+                if (! beam->GetElectricalBoresight(&look, &azim))
+                    return(0);
+                Vector3 vector;
+                vector.SphericalSet(1.0, look, azim);
+                QscatTargetInfo qti;
+                if (! qscat.TargetInfo(&antenna_frame_to_gc, &spacecraft,
+                    vector, &qti))
+                {
+                    fprintf(stderr, "%s: error finding target information\n",
+                        command);
+                    exit(1);
+                }
+                double alt, lon, lat;
+                if (! qti.rTarget.GetAltLonGCLat(&alt, &lon, &lat))
+                {
+                    fprintf(stderr, "%s: error finding alt/lon/lat\n",
+                        command);
+                    exit(1);
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    LonLat use_lon_lat;
+                    use_lon_lat.Set(lon, lat);
+                    use_lon_lat.ApproxApplyDelta(dlon_km[i], dlat_km[i]);
+                    if (qscat_sim.landMap.IsLand(&use_lon_lat))
+                    {
+                        echo_info.flag[spot_idx] = EchoInfo::LAND;
+                        break;
+                    }
+                }
+
+                //-------------------------------//
+                // determine total signal energy //
+                //-------------------------------//
+
+                double En = noise_powers[spot_idx];
+                double Es = 0.0;
+                for (int slice_idx = 0; slice_idx < 12; slice_idx++)
+                {
+                    Es += slice_powers[base_slice_idx + slice_idx];
+                }
+
+                double rho = 1.0;
+                double beta = qscat.ses.rxGainNoise / qscat.ses.rxGainEcho;
+                double alpha = Bn/Be*beta;
+                double noise = Bs / Be * (rho / beta * En - Es) /
+                    (alpha * rho / beta - 1.0);
+
+                double total_signal_energy = 0.0;
+                for (int slice_idx = 0; slice_idx < 12; slice_idx++)
+                {
+                    signal_energy[slice_idx] =
+                        slice_powers[base_slice_idx + slice_idx] - noise;
+                    total_signal_energy += signal_energy[slice_idx];
+                }
+                echo_info.totalSignalEnergy[spot_idx] = total_signal_energy;
+
+                //---------------//
+                // find the peak //
+                //---------------//
+
+                float meas_spec_peak_slice, meas_spec_peak_freq, width;
+                if (! gaussian_fit2(&qscat, slice_number, signal_energy + 1,
+                    10, &meas_spec_peak_slice, &meas_spec_peak_freq, &width))
+                {
+                    echo_info.flag[spot_idx] = EchoInfo::BAD_PEAK;
+                    continue;
+                }
+                echo_info.measSpecPeakFreq[spot_idx] = meas_spec_peak_freq;
+            }
+
+            //---------------------//
+            // write out the frame //
+            //---------------------//
+
+            if (! echo_info.Write(output_fd))
+            {
+                fprintf(stderr,
+                    "%s: error writing frame to output echo file %s\n",
+                    command, output_file_string);
+                exit(1);
+            }
+
+            in_first_frame = 0;
         }
 
         //----------------//
@@ -350,6 +636,21 @@ printf("%d\n", (int)spin_rate);
         //----------------//
 
         tlmFile->CloseParamDatasets(xpos_p);
+        tlmFile->CloseParamDatasets(ypos_p);
+        tlmFile->CloseParamDatasets(zpos_p);
+        tlmFile->CloseParamDatasets(xvel_p);
+        tlmFile->CloseParamDatasets(yvel_p);
+        tlmFile->CloseParamDatasets(zvel_p);
+        tlmFile->CloseParamDatasets(roll_p);
+        tlmFile->CloseParamDatasets(pitch_p);
+        tlmFile->CloseParamDatasets(yaw_p);
+        tlmFile->CloseParamDatasets(ant_pos_p);
+        tlmFile->CloseParamDatasets(orbit_time_p);
+        tlmFile->CloseParamDatasets(orbit_step_p);
+        tlmFile->CloseParamDatasets(pri_of_orbit_step_change_p);
+        tlmFile->CloseParamDatasets(slice_powers_p);
+        tlmFile->CloseParamDatasets(prf_cycle_time_p);
+        tlmFile->CloseParamDatasets(noise_meas_p);
     }
 
     //----------------//
@@ -361,52 +662,4 @@ printf("%d\n", (int)spin_rate);
         delete filter_set;
 
     return (0);
-}
-
-//----------------------------------------------------------------------
-// Function: ExtractAntSpinRateDnPer99
-// Extracts: UINT2
-//----------------------------------------------------------------------
-
-int
-ExtractAntSpinRateDnPer99(
-    TlmHdfFile*       l1File,
-    int32*            sdsIDs,
-    int32             start,
-    int32             stride,
-    int32             length,
-    VOIDP             buffer,
-    PolynomialTable*  poly_table)    // unused
-{
-    // extract one at a time
-    if (length != 1)
-    {
-        return(-1);
-    }
-
-    assert(l1File != 0);
-
-    // find out the raw antenna position
-    unsigned short antPos[100];;
-    int32 antposSdsIds[1];
-    antposSdsIds[0] = sdsIDs[0];
-    int rc = ExtractData2D_100(l1File, antposSdsIds, start, 0, 1, antPos);
-    if (rc <= 0)
-    {
-        return(rc);
-    }
-
-    // spin rate is the delta antenna position
-    unsigned int theta_max = antPos[99];
-    unsigned int theta_min = antPos[0];
-    if (theta_max < theta_min)
-    {
-        theta_max += ENCODER_N;
-    }
-    unsigned int delta_theta = theta_max - theta_min;
-
-    unsigned short* ptr = (unsigned short *)buffer;
-    *ptr = (unsigned short)delta_theta;
-
-    return(1);
 }
