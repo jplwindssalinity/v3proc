f0a80aec97fb1e102d4899fed387a221274944c8
hudd <>
1999-04-28 16:53:13 +0000

    Initial revision

---
+++ objs/ETime.C
@@ -0,0 +1,125 @@
+//==========================================================//
+// Copyright (C) 1999, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+static const char rcs_id_etime_c[] =
+	"@(#) $Id$";
+
+#include <stdlib.h>
+#include <math.h>
+#include "ETime.h"
+
+//=======//
+// ETime //
+//=======//
+
+ETime::ETime()
+:   _sec(0), _ms(0)
+{
+	return;
+}
+
+ETime::~ETime()
+{
+	return;
+}
+
+//---------------------//
+// ETime::FromStructTm //
+//---------------------//
+
+int
+ETime::FromStructTm(
+    struct tm*  tm_time)
+{
+    _sec = mktime(tm_time);
+    if (_sec == (time_t)-1)
+    {
+        return(0);
+    }
+    else
+    {
+        // mktime() is affected by timezone
+        // but we want UTC timezone
+        _sec -= timezone;
+        _ms = 0;
+        return(1);
+    }
+}
+
+//------------------//
+// ETime::FromCodeB //
+//------------------//
+
+int
+ETime::FromCodeB(
+    const char*  code_b_string)
+{
+    //------------------------------------------------//
+    // overlay code b string onto default code b time //
+    //------------------------------------------------//
+
+    int length = strlen(code_b_string);
+    if (length >= CODE_B_TIME_LENGTH)
+        return(0);    // string too big
+
+    char time_string[CODE_B_TIME_LENGTH];
+    memcpy(time_string, CODE_B_DEFAULT_TIME, CODE_B_TIME_LENGTH);
+    memcpy(time_string, code_b_string, length);
+
+    //----------------------//
+    // get the milliseconds //
+    //----------------------//
+
+    unsigned short ms;
+    int retval = sscanf(time_string, "%*19c.%hd", &ms);
+    if (retval != 1)
+        return(0);
+
+    //-------------------------------//
+    // put the rest into a struct tm //
+    //-------------------------------//
+
+    time_string[17] = '\0';
+    struct tm tm_time;
+    if (strptime(time_string, CODE_B_STRPTIME_FORMAT, &tm_time) == NULL)
+        return(0);
+
+    //---------//
+    // convert //
+    //---------//
+
+    if (! FromStructTm(&tm_time))
+        return(0);
+
+    _ms = ms;
+    return(1);
+}
+
+//----------------//
+// ETime::ToCodeB //
+//----------------//
+
+int
+ETime::ToCodeB(
+    char*  string)
+{
+    struct tm* tm_time = gmtime(&_sec);
+    sprintf(string, CODE_B_SCANF_FORMAT, tm_time->tm_year + 1900,
+        tm_time->tm_yday + 1, tm_time->tm_hour, tm_time->tm_min,
+        tm_time->tm_sec, _ms);
+    return(1);
+}
+
+//-----------//
+// operator> //
+//-----------//
+
+int
+operator>(
+    const ETime&  a,
+    const ETime&  b)
+{
+    return(a._sec > b._sec || (a._sec == b._sec && a._ms > b._ms));
+}
---
+++ objs/ETime.h
@@ -0,0 +1,70 @@
+//==========================================================//
+// Copyright (C) 1999, California Institute of Technology.	//
+// U.S. Government sponsorship acknowledged.				//
+//==========================================================//
+
+#ifndef ETIME_H
+#define ETIME_H
+
+static const char rcs_id_etime_h[] =
+	"@(#) $Id$";
+
+#include <stdio.h>
+#include <time.h>
+
+//======================================================================
+// CLASSES
+//		ETime
+//======================================================================
+
+//======================================================================
+// CLASS
+//		ETime
+//
+// DESCRIPTION
+//		The ETime object handles time.
+//======================================================================
+
+#define CODE_B_DEFAULT_TIME     "1993-001T00:00:00.000\0"
+#define CODE_B_TIME_LENGTH      22
+#define CODE_B_STRPTIME_FORMAT  "%Y-%m-%dT%H:%M:%S"
+#define CODE_B_SCANF_FORMAT     "%04d-%03dT%02d:%02d:%02d.%03d"
+
+class ETime
+{
+public:
+
+	//--------------//
+	// construction //
+	//--------------//
+
+	ETime();
+	~ETime();
+
+    //-------------//
+    // conversions //
+    //-------------//
+
+    int  FromStructTm(struct tm* tm_time);
+    int  FromCodeB(const char* code_b_string);
+    int  ToCodeB(char* string);
+
+    //-----------//
+    // operators //
+    //-----------//
+
+    friend int  operator>(const ETime& a, const ETime& b);
+
+protected:
+
+	//-----------//
+	// variables //
+	//-----------//
+
+    time_t          _sec;    // seconds since epoch
+    unsigned short  _ms;     // milliseconds
+};
+
+int  operator>(const ETime& a, const ETime& b);
+
+#endif
