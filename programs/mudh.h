a816552af1c23867874ed1c9b3653d70de2ace55
hudd <>
1999-12-03 19:24:10 +0000

    Initial revision

---
+++ programs/mudh.h
@@ -0,0 +1,24 @@
+//=========================================================//
+// Copyright (C) 1999, California Institute of Technology. //
+// U.S. Government sponsorship acknowledged.               //
+//=========================================================//
+
+#ifndef MUDH_H
+#define MUDH_H
+
+// these are for the compressed table
+#define NBD_DIM  16
+#define SPD_DIM  16
+#define DIR_DIM  16
+#define MLE_DIM  16
+
+#define NBD_MIN   -4.0
+#define NBD_MAX    6.0
+#define SPD_MIN    0.0
+#define SPD_MAX   30.0
+#define DIR_MIN    0.0
+#define DIR_MAX   90.0
+#define MLE_MIN  -10.0
+#define MLE_MAX    0.0
+
+#endif
