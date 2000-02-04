891b529360f888199639c4dea164a4856542e52e
hudd <>
2000-02-04 21:59:49 +0000

    cleaned up, no mods

--- objs/L2AToL2B.h
+++ objs/L2AToL2B.h
@@ -1,33 +1,32 @@
 //==============================================================//
-// Copyright (C) 1997-1998, California Institute of Technology.	//
-// U.S. Government sponsorship acknowledged.					//
+// Copyright (C) 1997-2000, California Institute of Technology. //
+// U.S. Government sponsorship acknowledged.                    //
 //==============================================================//
 
 #ifndef L2ATOL2B_H
 #define L2ATOL2B_H
 
 static const char rcs_id_l2atol2b_h[] =
-	"@(#) $Id$";
+    "@(#) $Id$";
 
 #include "L2A.h"
 #include "L2B.h"
 #include "GMF.h"
 
-#define DESIRED_SOLUTIONS		4
-
+#define DESIRED_SOLUTIONS  4
 
 //======================================================================
 // CLASSES
-//		L2AToL2B
+//     L2AToL2B
 //======================================================================
 
 //======================================================================
 // CLASS
-//		L2AToL2B
+//      L2AToL2B
 //
 // DESCRIPTION
-//		The L2AToL2B object is used to convert between Level 2A data
-//		and Level 2B data.  It performs wind retrieval.
+//      The L2AToL2B object is used to convert between Level 2A data
+//      and Level 2B data.  It performs wind retrieval.
 //======================================================================
 
 class L2AToL2B
@@ -38,47 +37,47 @@ public:
     // enum //
     //------//
 
-    enum WindRetrievalMethodE { GS, GS_FIXED, H1, H2, H3, S1, S2, S3,
-				S4, POLAR_SPECIAL, CHEAT };
+    enum WindRetrievalMethodE { GS, GS_FIXED, H1, H2, H3, S1, S2, S3, S4,
+        POLAR_SPECIAL, CHEAT };
 
     //--------------//
     // construction //
     //--------------//
-    
+
     L2AToL2B();
     ~L2AToL2B();
-    
+
     //---------//
     // setting //
     //---------//
 
-    int    SetWindRetrievalMethod(const char* wr_method);
-    
+    int  SetWindRetrievalMethod(const char* wr_method);
+
     //------------//
     // conversion //
     //------------//
-    
-    int		ConvertAndWrite(L2A* l2a, GMF* gmf, Kp* kp, L2B* l2b);
-    int		Flush(L2B* l2b);
-    
+
+    int  ConvertAndWrite(L2A* l2a, GMF* gmf, Kp* kp, L2B* l2b);
+    int  Flush(L2B* l2b);
+
     //------------------------------------------//
     // Routine for outputting the Nudge Field   //
     // wind vector                              //
     //------------------------------------------//
-	
-    int             Cheat(MeasList* meas_list, WVC* wvc);
+
+    int  Cheat(MeasList* meas_list, WVC* wvc);
 
     //-----------//
     // debugging //
     //-----------//
 
-    int		WriteSolutionCurves(L2A* l2a, GMF* gmf, Kp* kp,
-				    const char* output_file);
+    int  WriteSolutionCurves(L2A* l2a, GMF* gmf, Kp* kp,
+             const char* output_file);
 
     //----------------------//
     // processing variables //
     //----------------------//
-    
+
     int  medianFilterWindowSize;
     int  medianFilterMaxPasses;
     int  maxRankForNudging;
@@ -95,30 +94,31 @@ public:
     int                   useNudgeThreshold;
     int                   useNMF;
     int                   useRandomInit;
-    
+
     //-----------------------------------------//
     // Parameters for Peak Splitting Algorithm //
     //-----------------------------------------//
-    
-    float	onePeakWidth;
-    float	twoPeakSep;
-    float	probThreshold;
-    
+
+    float  onePeakWidth;
+    float  twoPeakSep;
+    float  probThreshold;
+
     //-------------------//
     // nudging variables //
     //-------------------//
-    
-    WindField	nudgeField;
-    WindVectorField nudgeVctrField;
-    float nudgeThresholds[2];
 
-    //------------------------------------------------//
-    // Auxiliary Variables For Hurricane Processing   //
-    //------------------------------------------------//
-    int        useHurricaneNudgeField;
-    WindField  hurricaneField;
-    float hurricaneRadius;  // km 
-    EarthPosition hurricaneCenter; 
+    WindField        nudgeField;
+    WindVectorField  nudgeVctrField;
+    float            nudgeThresholds[2];
+
+    //----------------------------------------------//
+    // Auxiliary Variables For Hurricane Processing //
+    //----------------------------------------------//
+
+    int            useHurricaneNudgeField;
+    WindField      hurricaneField;
+    float          hurricaneRadius;  // km
+    EarthPosition  hurricaneCenter;
 };
 
 #endif
