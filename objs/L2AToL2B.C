1b0ccdc5dedc8bec2e12376f040f5806bf9ef8f9
hudd <>
2000-02-04 21:50:37 +0000

    cleaned up, no mods

--- objs/L2AToL2B.C
+++ objs/L2AToL2B.C
@@ -1,10 +1,10 @@
 //==============================================================//
-// Copyright (C) 1997-1998, California Institute of Technology.	//
-// U.S. Government sponsorship acknowledged.					//
+// Copyright (C) 1997-1998, California Institute of Technology.    //
+// U.S. Government sponsorship acknowledged.                    //
 //==============================================================//
 
 static const char rcs_id_l2atol2b_c[] =
-	"@(#) $Id$";
+    "@(#) $Id$";
 
 #include <stdio.h>
 #include <string.h>
@@ -23,12 +23,12 @@ L2AToL2B::L2AToL2B()
     smartNudgeFlag(0), wrMethod(GS), onePeakWidth(0.0), twoPeakSep(181.0),
     probThreshold(0.0),useHurricaneNudgeField(0),hurricaneRadius(0)
 {
-	return;
+    return;
 }
 
 L2AToL2B::~L2AToL2B()
 {
-	return;
+    return;
 }
 
 //----------------------------------//
@@ -110,48 +110,48 @@ L2AToL2B::SetWindRetrievalMethod(
 
 int
 L2AToL2B::ConvertAndWrite(
-	L2A*	l2a,
-	GMF*	gmf,
-	Kp*		kp,
-	L2B*	l2b)
+    L2A*    l2a,
+    GMF*    gmf,
+    Kp*        kp,
+    L2B*    l2b)
 {
-	static int last_rev_number = 0;
+    static int last_rev_number = 0;
 
-	MeasList* meas_list = &(l2a->frame.measList);
+    MeasList* meas_list = &(l2a->frame.measList);
 
-	//-----------------------------------//
-	// check for missing wind field data //
-	//-----------------------------------//
-	// this should be handled by some kind of a flag!
+    //-----------------------------------//
+    // check for missing wind field data //
+    //-----------------------------------//
+    // this should be handled by some kind of a flag!
 
-	int any_zero = 0;
-	for (Meas* meas = meas_list->GetHead(); meas; meas = meas_list->GetNext())
-	{
-		if (! meas->value)
-		{
-			any_zero = 1;
-			break;
-		}
-	}
-	if (any_zero)
-	{
-		return(3);
-	}
+    int any_zero = 0;
+    for (Meas* meas = meas_list->GetHead(); meas; meas = meas_list->GetNext())
+    {
+        if (! meas->value)
+        {
+            any_zero = 1;
+            break;
+        }
+    }
+    if (any_zero)
+    {
+        return(3);
+    }
 
-	//-----------------------------------//
-	// check for wind retrieval criteria //
-	//-----------------------------------//
+    //-----------------------------------//
+    // check for wind retrieval criteria //
+    //-----------------------------------//
 
-	if (! gmf->CheckRetrieveCriteria(meas_list))
-	{
-		return(4);
-	}
+    if (! gmf->CheckRetrieveCriteria(meas_list))
+    {
+        return(4);
+    }
 
-	//---------------//
-	// retrieve wind //
-	//---------------//
+    //---------------//
+    // retrieve wind //
+    //---------------//
 
-	WVC* wvc = new WVC();
+    WVC* wvc = new WVC();
     float ctd, speed, dir;
     WindVectorPlus* wvp;
     static num = 1;
@@ -207,17 +207,19 @@ L2AToL2B::ConvertAndWrite(
                 delete wvc;
                 return(10);
             }
+
 #ifdef S2_DEBUG_INTERVAL
-            if(num%S2_DEBUG_INTERVAL==0){
-	      ctd=(l2a->frame.cti - l2a->header.zeroIndex)
-		*l2a->header.crossTrackResolution;
-	      wvp=wvc->ambiguities.GetHead();
-	      speed=wvp->spd;
-	      printf("CTD %g Speed First Rank %g\n",ctd,speed);
-	      fflush(stdout);
-	    }
+            if (num % S2_DEBUG_INTERVAL == 0)
+            {
+                ctd = (l2a->frame.cti - l2a->header.zeroIndex) *
+                    l2a->header.crossTrackResolution;
+                wvp = wvc->ambiguities.GetHead();
+                speed = wvp->spd;
+                printf("CTD %g Speed First Rank %g\n", ctd, speed);
+                fflush(stdout);
+            }
 #endif
-	    num++;
+            num++;
             break;
 
         case S3:
@@ -226,7 +228,7 @@ L2AToL2B::ConvertAndWrite(
                 delete wvc;
                 return(11);
             }
-	    break;
+        break;
 
         case S4:
             if (! gmf->RetrieveWinds_S3(meas_list, kp, wvc,1))
@@ -234,7 +236,7 @@ L2AToL2B::ConvertAndWrite(
                 delete wvc;
                 return(12);
             }
-	    break;
+        break;
 
         case POLAR_SPECIAL:
             if (! gmf->RetrieveWinds_GS(meas_list, kp, wvc,1))
@@ -242,179 +244,209 @@ L2AToL2B::ConvertAndWrite(
                 delete wvc;
                 return(13);
             }
-	    ctd=(l2a->frame.cti - l2a->header.zeroIndex)
-		*l2a->header.crossTrackResolution;
-            wvp=NULL;
-	    if(wvc) wvp=wvc->ambiguities.GetHead();
-            if(wvp){
-	      speed=wvp->spd;
-	      dir=wvp->dir*rtd;
-	      printf("%g %g %g %d %d\n",ctd,speed,dir,l2a->frame.cti, l2a->frame.ati);
-	      fflush(stdout);
-	    }
-	    break;
-
+            ctd = (l2a->frame.cti - l2a->header.zeroIndex) *
+                l2a->header.crossTrackResolution;
+            wvp = NULL;
+            if (wvc)
+                wvp = wvc->ambiguities.GetHead();
+            if (wvp)
+            {
+                speed = wvp->spd;
+                dir = wvp->dir*rtd;
+                printf("%g %g %g %d %d\n", ctd, speed, dir, l2a->frame.cti,
+                    l2a->frame.ati);
+                fflush(stdout);
+            }
+            break;
 
         case CHEAT:
-	  if(!Cheat(meas_list,wvc)){
-	    return(14);
-	  }
-	  break;
+            if (! Cheat(meas_list, wvc))
+            {
+                return(14);
+            }
+            break;
 
         default:
             return(15);
     }
 
-	if (wvc->ambiguities.NodeCount() == 0)
-	{
-		delete wvc;
-		return(16);
-	}
-	wvc->lonLat = meas_list->AverageLonLat();
+    if (wvc->ambiguities.NodeCount() == 0)
+    {
+        delete wvc;
+        return(16);
+    }
+    wvc->lonLat = meas_list->AverageLonLat();
 
-	//-------------------------//
-	// determine grid indicies //
-	//-------------------------//
+    //-------------------------//
+    // determine grid indicies //
+    //-------------------------//
 
-	int rev = (int)l2a->frame.rev;
-	int cti = (int)l2a->frame.cti;
-	int ati = (int)l2a->frame.ati;
+    int rev = (int)l2a->frame.rev;
+    int cti = (int)l2a->frame.cti;
+    int ati = (int)l2a->frame.ati;
 
-	//------------------------------//
-	// determine if rev is complete //
-	//------------------------------//
+    //------------------------------//
+    // determine if rev is complete //
+    //------------------------------//
 
-	if (rev != last_rev_number && last_rev_number)
-		Flush(l2b);	// process and write
+    if (rev != last_rev_number && last_rev_number)
+        Flush(l2b);    // process and write
 
-	//-------------------//
-	// add to wind swath //
-	//-------------------//
+    //-------------------//
+    // add to wind swath //
+    //-------------------//
 
-	if (! l2b->frame.swath.Add(cti, ati, wvc))
-		return(0);
+    if (! l2b->frame.swath.Add(cti, ati, wvc))
+        return(0);
 
-	return(1);
+    return(1);
 }
+
 //-----------------//
 // L2AToL2B::Cheat //
 //-----------------//
+
 int
-L2AToL2B::Cheat(MeasList* meas_list, WVC* wvc)
+L2AToL2B::Cheat(
+    MeasList*  meas_list,
+    WVC*       wvc)
 {
-  wvc->lonLat=meas_list->AverageLonLat();
-  WindVectorPlus* wvp = new WindVectorPlus;
-  if(! nudgeField.InterpolatedWindVector(wvc->lonLat,wvp)){
-    delete wvp;
-    return(0);
-  }
-  wvp->obj=0;
-  wvc->ambiguities.Append(wvp);
-  return(1);
+    wvc->lonLat = meas_list->AverageLonLat();
+    WindVectorPlus* wvp = new WindVectorPlus;
+    if (! nudgeField.InterpolatedWindVector(wvc->lonLat, wvp))
+    {
+        delete wvp;
+        return(0);
+    }
+    wvp->obj = 0;
+    wvc->ambiguities.Append(wvp);
+    return(1);
 }
 
-#define ONE_STAGE_WITHOUT_RANGES 1
-#define S3_WINDOW_SIZE  medianFilterWindowSize
-#define S3_NUDGE 0
 //-----------------//
 // L2AToL2B::Flush //
 //-----------------//
 
+#define ONE_STAGE_WITHOUT_RANGES 1
+#define S3_WINDOW_SIZE  medianFilterWindowSize
+#define S3_NUDGE 0
+
 int
 L2AToL2B::Flush(
-	L2B*	l2b)
+    L2B*  l2b)
 {
-        //-----------------------------------------------------//
-        // Copy Interpolated NudgeVectors to Wind Vector Cells //
-        //-----------------------------------------------------//
-        if(useNudging && !l2b->frame.swath.nudgeVectorsRead) l2b->frame.swath.GetNudgeVectors(&nudgeField);
-        if(useHurricaneNudgeField) l2b->frame.swath.GetHurricaneNudgeVectors(&hurricaneField,&hurricaneCenter,hurricaneRadius);
-       
-	//------------//
-	// initialize //
-	//------------//
+    //-----------------------------------------------------//
+    // Copy Interpolated NudgeVectors to Wind Vector Cells //
+    //-----------------------------------------------------//
 
-#define ALREADY_INITD 0
-#define USEBESTK 0
-#define BESTKPARAMETER  1000
-#define BESTKWINDOWSIZE 15
-        if(ALREADY_INITD){
-	  // Do Nothing 
-        }
-        else if (USEBESTK){
-	  l2b->frame.swath.BestKFilter(BESTKWINDOWSIZE,
-		BESTKPARAMETER);
-	}
-	else if (useNudging)
-	{
-          if (S3_NUDGE)
-	    l2b->frame.swath.S3Nudge();
+    if (useNudging && ! l2b->frame.swath.nudgeVectorsRead)
+    {
+        l2b->frame.swath.GetNudgeVectors(&nudgeField);
+    }
+    if (useHurricaneNudgeField)
+    {
+        l2b->frame.swath.GetHurricaneNudgeVectors(&hurricaneField,
+            &hurricaneCenter, hurricaneRadius);
+    }
 
-	  else if (useNudgeThreshold) 
-	    l2b->frame.swath.ThresNudge(maxRankForNudging,
-					nudgeThresholds);
-	  else 
+    //------------//
+    // initialize //
+    //------------//
+
+    #define ALREADY_INITD 0
+    #define USEBESTK 0
+    #define BESTKPARAMETER  1000
+    #define BESTKWINDOWSIZE 15
+
+    if (ALREADY_INITD)
+    {
+        // Do Nothing
+    }
+    else if (USEBESTK)
+    {
+        l2b->frame.swath.BestKFilter(BESTKWINDOWSIZE, BESTKPARAMETER);
+    }
+    else if (useNudging)
+    {
+        if (S3_NUDGE)
+            l2b->frame.swath.S3Nudge();
+        else if (useNudgeThreshold)
+            l2b->frame.swath.ThresNudge(maxRankForNudging, nudgeThresholds);
+        else
             l2b->frame.swath.Nudge(maxRankForNudging);
-	}
-        else if(useRandomInit){
-	  l2b->frame.swath.InitRandom();
-	}
-	else
-	{
-	   l2b->frame.swath.InitWithRank(1);
-	}
-  
-#define HURRICANE_MAX_RANK 4
-	if(useHurricaneNudgeField) l2b->frame.swath.HurricaneNudge(HURRICANE_MAX_RANK,&hurricaneCenter,hurricaneRadius);
-	//---------------//
-	// median filter //
-	//---------------//
+    }
+    else if(useRandomInit)
+    {
+        l2b->frame.swath.InitRandom();
+    }
+    else
+    {
+        l2b->frame.swath.InitWithRank(1);
+    }
 
-	int bound;
-	
-        int special=0;
-        switch(wrMethod){
-	case S3:
-	  special=1;
-	  break;
-        case S4:
-	  special=2;
-	  break;
-	default:
-	  special=0;
-          break;
-	}
-        int special_first_pass=special;
-        if(special==1 && ONE_STAGE_WITHOUT_RANGES) special_first_pass=0;
-	if (useNMF) {
-	  bound = 9;
-	  l2b->frame.swath.MedianFilter(medianFilterWindowSize,
-		medianFilterMaxPasses, bound, useAmbiguityWeights,
-					special_first_pass);
-	}
-	bound = 0;
-	l2b->frame.swath.MedianFilter(medianFilterWindowSize,
-	      medianFilterMaxPasses, bound, useAmbiguityWeights,
-				      special_first_pass);
+    #define HURRICANE_MAX_RANK 4
 
-        if(special==1 && ONE_STAGE_WITHOUT_RANGES){
-	  if(medianFilterMaxPasses>0){
-	    l2b->frame.swath.DiscardUnselectedRanges();
-	  }
-	  l2b->frame.swath.MedianFilter(S3_WINDOW_SIZE,
-	      medianFilterMaxPasses, bound, useAmbiguityWeights,special);
-	}
-	//------------//
-	// output l2b //
-	//------------//
+    if (useHurricaneNudgeField)
+    {
+        l2b->frame.swath.HurricaneNudge(HURRICANE_MAX_RANK, &hurricaneCenter,
+            hurricaneRadius);
+    }
 
-	if (! l2b->WriteHeader())
-		return(0);
-	if (! l2b->WriteDataRec())
-		return(0);
-	// l2b->frame.swath.DeleteWVCs();
+    //---------------//
+    // median filter //
+    //---------------//
 
-	return(1);
+    int bound;
+    int special = 0;
+
+    switch(wrMethod)
+    {
+    case S3:
+        special=1;
+        break;
+    case S4:
+        special=2;
+        break;
+    default:
+        special=0;
+        break;
+    }
+
+    int special_first_pass = special;
+    if (special == 1 && ONE_STAGE_WITHOUT_RANGES)
+        special_first_pass = 0;
+    if (useNMF)
+    {
+        bound = 9;
+        l2b->frame.swath.MedianFilter(medianFilterWindowSize,
+            medianFilterMaxPasses, bound, useAmbiguityWeights,
+            special_first_pass);
+    }
+    bound = 0;
+    l2b->frame.swath.MedianFilter(medianFilterWindowSize,
+        medianFilterMaxPasses, bound, useAmbiguityWeights, special_first_pass);
+
+    if (special == 1 && ONE_STAGE_WITHOUT_RANGES)
+    {
+        if (medianFilterMaxPasses > 0)
+        {
+            l2b->frame.swath.DiscardUnselectedRanges();
+        }
+        l2b->frame.swath.MedianFilter(S3_WINDOW_SIZE, medianFilterMaxPasses,
+            bound, useAmbiguityWeights,special);
+    }
+
+    //------------//
+    // output l2b //
+    //------------//
+
+    if (! l2b->WriteHeader())
+        return(0);
+    if (! l2b->WriteDataRec())
+        return(0);
+    // l2b->frame.swath.DeleteWVCs();
+
+    return(1);
 }
 
 //-------------------------------//
@@ -423,41 +455,30 @@ L2AToL2B::Flush(
 
 int
 L2AToL2B::WriteSolutionCurves(
-	L2A*			l2a,
-	GMF*			gmf,
-	Kp*				kp,
-	const char*		output_file)
+    L2A*            l2a,
+    GMF*            gmf,
+    Kp*                kp,
+    const char*        output_file)
 {
-	//------------------//
-	// open output file //
-	//------------------//
+    //------------------//
+    // open output file //
+    //------------------//
 
-	FILE* ofp = fopen(output_file, "w");
-	if (ofp == NULL)
-		return(0);
+    FILE* ofp = fopen(output_file, "w");
+    if (ofp == NULL)
+        return(0);
 
-	//-----------------------//
-	// write solution curves //
-	//-----------------------//
+    //-----------------------//
+    // write solution curves //
+    //-----------------------//
 
-	gmf->WriteSolutionCurves(ofp, &(l2a->frame.measList), kp);
+    gmf->WriteSolutionCurves(ofp, &(l2a->frame.measList), kp);
 
-	//-------------------//
-	// close output file //
-	//-------------------//
+    //-------------------//
+    // close output file //
+    //-------------------//
 
-	fclose(ofp);
+    fclose(ofp);
 
-	return(1);
+    return(1);
 }
-
-
-
-
-
-
-
-
-
-
-
