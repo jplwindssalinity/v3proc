d20b778f699fdd71a29a197b9cd3748937487fac
hudd <>
2000-06-21 20:49:42 +0000

    *** empty log message ***

--- objs/Wind.h
+++ objs/Wind.h
@@ -442,6 +442,7 @@ public:
 
     void  operator-=(const WindSwath& w);
     int   DifferenceFromTruth(WindField* truth);
+    int   ExcessDifference(WindField* truth);
     int   CtdArray(float cross_track_res, float* ctd_array);
     int   DirArray(int number_of_bins, float* dir_array);
 
