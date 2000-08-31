9074129712473b2d68c739f39eb594ed6182ea73
hudd <>
2000-08-31 17:40:23 +0000

    Initial revision

---
+++ programs/best.h
@@ -0,0 +1,32 @@
+//=========================================================//
+// Copyright (C) 2000, California Institute of Technology. //
+// U.S. Government sponsorship acknowledged.               //
+//=========================================================//
+
+#ifndef BEST_H
+#define BEST_H
+
+static const char rcs_id_best_h[] =
+    "@(#) $Id$";
+
+#define FIRST_MIN_VALUE  0.0
+#define FIRST_MAX_VALUE  1.0
+#define FIRST_INDICIES   101
+
+#define NEIGHBOR_MIN_VALUE  0
+#define NEIGHBOR_MAX_VALUE  24
+#define NEIGHBOR_INDICIES   25
+
+#define DIF_RATIO_MIN_VALUE  0.0
+#define DIF_RATIO_MAX_VALUE  1.0
+#define DIF_RATIO_INDICIES   11
+
+#define BALANCE_MIN_VALUE  0.0
+#define BALANCE_MAX_VALUE  1.0
+#define BALANCE_INDICIES   11
+
+#define SPEED_MIN_VALUE  0.0
+#define SPEED_MAX_VALUE  30.0
+#define SPEED_INDICIES   31
+
+#endif
