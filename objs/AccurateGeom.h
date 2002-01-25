41f8b3301e8fba64dbe2c3021ded30e08c66df44
hudd <>
2002-01-25 20:29:22 +0000

    just cleaned it up

--- objs/AccurateGeom.h
+++ objs/AccurateGeom.h
@@ -19,49 +19,42 @@ static const char rcs_id_accurategeom_h[] =
 
 //======================================================================
 // DESCRIPTION
-//		High level instrument geometry functions, which perform
+// High level instrument geometry functions, which perform
 // the precise calculations necessary for calculating Kpr, kfactor, etc.
 //======================================================================
 
+int    IntegrateSlice(Spacecraft* spacecraft, Qscat* qscat, Meas* meas,
+           int num_look_steps_per_slice, double azimuth_integration_range,
+           double azimuth_step_size, int range_gate_clipping, float *X);
 
+double  GetPulseFractionReceived(Qscat* qscat, double range);
 
-int		IntegrateSlice(Spacecraft* spacecraft, Qscat* qscat, Meas* meas,
-            int num_look_steps_per_slice, double azimuth_integration_range,
-            double azimuth_step_size, int range_gate_clipping, float *X);
+int  FindBoxCorners(CoordinateSwitch* antenna_frame_to_gc,
+         Spacecraft* spacecraft, Qscat* qscat, double look1, double look2,
+         double azi1, double azi2, Outline* box);
 
-double   GetPulseFractionReceived(Qscat* qscat, double range);
+int  FindLookAtFreq(CoordinateSwitch* antenna_frame_to_gc,
+         Spacecraft* spacecraft, Qscat* qscat, double target_freq,
+         double freq_tol, double* look, double azimuth);
 
-int		FindBoxCorners(CoordinateSwitch* antenna_frame_to_gc,
-            Spacecraft* spacecraft, Qscat* qscat, double look1, double look2,
-            double azi1, double azi2, Outline* box);
+int  SpectralResponse(Spacecraft* spacecraft, Qscat* qscat, double freq,
+         double azim, double look, double* response);
 
-int		FindLookAtFreq(CoordinateSwitch* antenna_frame_to_gc,
-			Spacecraft* spacecraft, Qscat* qscat, double target_freq,
-            double freq_tol, double* look, double azimuth);
+int  IntegrateFrequencyInterval(Spacecraft* spacecraft, Qscat* qscat,
+         double f1, double centroid_look, double centroid_azimuth, double bw,
+         int num_look_steps_per_slice, double azimuth_integration_range,
+         double azimuth_step_size, int range_gate_clipping, double ftol,
+         double* X);
 
-int         SpectralResponse(Spacecraft* spacecraft, Qscat* qscat, 
-		double freq, double azim, double look, double* response);
+int  GetPeakSpectralResponse(CoordinateSwitch* antenna_frame_to_gc,
+         Spacecraft* spacecraft, Qscat* qscat, double* look, double* azim);
 
-int IntegrateFrequencyInterval( Spacecraft* spacecraft, Qscat* qscat,
-				double f1, double centroid_look,
-				double centroid_azimuth, double bw,
-				int num_look_steps_per_slice,
-				double azimuth_integration_range,
-				double azimuth_step_size, int range_gate_clipping,
-				double ftol, double* X);
-
-int GetPeakSpectralResponse(CoordinateSwitch* antenna_frame_to_gc,
-			     Spacecraft* spacecraft, Qscat* qscat, 
-			     double* look, double* azim);
-
-struct NegSpecParam{
-  Spacecraft* spacecraft;
-  Qscat* qscat;
+struct NegSpecParam
+{
+    Spacecraft*  spacecraft;
+    Qscat*       qscat;
 };
+
 double NegativeSpectralResponse(double* x, void* ptr);
+
 #endif
-
-
-
-
-
