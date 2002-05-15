a1ca466506086466d3d981b0de0524921122ad19
hudd <>
2002-05-15 22:21:56 +0000

    removed trigraph

--- programs/l2a_to_l2b.C
+++ programs/l2a_to_l2b.C
@@ -295,7 +295,7 @@ main(
                 exit(1);
                 break;
             default:
-                fprintf(stderr, "%s: unknown status (???)\n", command);
+                fprintf(stderr, "%s: unknown status\n", command);
                 exit(1);
             }
             break;        // done, exit do loop
