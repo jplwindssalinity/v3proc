85b9559a480768085d25fa54fdd71944dd47478b
hudd <>
2002-11-07 23:24:46 +0000

    *** empty log message ***

--- objs/BitMasks.h
+++ objs/BitMasks.h
@@ -17,7 +17,7 @@ static const char rcs_id_bit_masks_h[] =
 #define L1A_OPERATIONAL_MODE_WOM         0x0e
 #define L1A_OPERATIONAL_MODE_CAL         0x07
 #define L1A_OPERATIONAL_MODE_STB         0x70
-#define L1A_OPERATIONAL_MODE_ROM         0xeo
+#define L1A_OPERATIONAL_MODE_ROM         0xe0
 
 #define L1A_FRAME_INST_STATUS_MODE_MASK  0x00000003
 #define L1A_FRAME_INST_STATUS_WOM        0x00000000
