12d7e034f01b78345fcc748321c9281376d222c2
cvsuser <>
2006-06-16 04:01:56 +0000

    replaced long with off_t for OffSetList template BWS

--- programs/l1a_to_l1b.C
+++ programs/l1a_to_l1b.C
@@ -87,7 +87,7 @@ template class List<Meas>;
 template class List<EarthPosition>;
 template class List<MeasSpot>;
 template class List<WindVectorPlus>;
-template class List<long>;
+template class List<off_t>;
 template class List<OffsetList>;
 template class TrackerBase<unsigned char>;
 template class TrackerBase<unsigned short>;
--- programs/sim.C
+++ programs/sim.C
@@ -101,7 +101,7 @@ template class List<MeasSpot>;
 template class BufferedList<OrbitState>;
 template class List<OrbitState>;
 template class List<WindVectorPlus>;
-template class List<long>;
+template class List<off_t>;
 template class List<OffsetList>;
 template class TrackerBase<unsigned char>;
 template class TrackerBase<unsigned short>;
