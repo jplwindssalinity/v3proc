56b22ba2cd62fb362d52f9545bcbcabc16ae3474
cvsuser <>
2006-06-20 18:17:15 +0000

    Sped up - BWS

--- programs/l2a_to_ascii.C
+++ programs/l2a_to_ascii.C
@@ -117,21 +117,49 @@ main(
 
 
 
-        int frame_number=1;
+        int frame_number=0;
 
 	//---------------------//
 	// copy desired frames //
 	//---------------------//
+        FILE* ifp=l2a.GetInputFp();
+        l2a.ReadHeader();
 
-	while (l2a.ReadDataRec() && frame_number <= end_frame)
+        int cell_header_size=16;
+
+        int valid=1, nm=0;
+        off_t meas_size;
+        while(nm==0){
+          off_t tmp=ftello(ifp);
+	  valid=l2a.ReadDataRec();
+          frame_number++;
+	  nm=l2a.frame.measList.NodeCount();
+	  if(nm>0) meas_size=(ftello(ifp)-tmp-cell_header_size)/nm;
+        }
+	
+	while (valid && frame_number <= end_frame)
 	{
 	  if(frame_number >= start_frame){
 	    l2a.WriteDataRecAscii();
+	    valid=l2a.ReadDataRec();
+	  }
+          else if(frame_number==start_frame-1){
+	    valid=l2a.ReadDataRec();
+	  }
+	  else{
+	    fseeko(ifp,12,SEEK_CUR);
+	    int nm;
+            valid=fread(&nm,sizeof(int),1,ifp);
+            valid=(valid==1);
+	    if(valid){
+	      fseeko(ifp,nm*meas_size,SEEK_CUR);
+	      valid=!(feof(ifp));
+	    }
 	  }
           if(start_frame>=0) frame_number++;
         }
         if(start_frame>=0 && end_frame>frame_number){
-	  fprintf(stderr,"Maximum frame number was %d\n",frame_number);
+	  fprintf(stderr,"Maximum frame number was %d\n",frame_number-1);
 	}
 
         //----------------------//
