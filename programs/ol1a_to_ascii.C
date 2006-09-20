0a9c27f4c96fcc01b76acd8bb82d45b9a606fe4a
cvsuser <>
2006-09-20 01:07:52 +0000

    New programs for converting OVWM data from l1a to l1b. Write out l1a data to ascii.
    Sam

---
+++ programs/ol1a_to_ascii.C
@@ -0,0 +1,193 @@
+//=========================================================//
+// Copyright (C) 1998, California Institute of Technology. //
+// U.S. Government sponsorship acknowledged.               //
+//=========================================================//
+
+//----------------------------------------------------------------------
+// NAME
+//    ol1a_to_ascii
+//
+// SYNOPSIS
+//    ol1a_to_ascii <config_file> <input_file> <output_file>
+//      [ start_frame ] [ end_frame ]
+//
+// DESCRIPTION
+//    Reads frames start_frame through end_frame from a OvwmscatL1A file
+//    and writes them to an ASCII file
+//
+// AUTHORS
+//    James Huddleston (hudd@casket.jpl.nasa.gov)
+//----------------------------------------------------------------------
+
+//-----------------------//
+// Configuration Control //
+//-----------------------//
+
+static const char rcs_id[] =
+    "@(#) $Id$";
+
+//----------//
+// INCLUDES //
+//----------//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include "List.h"
+#include "List.C"
+#include "BufferedList.h"
+#include "BufferedList.C"
+#include "Misc.h"
+#include "ConfigList.h"
+#include "Spacecraft.h"
+#include "ConfigSim.h"
+#include "Array.h"
+#include "Meas.h"
+#include "Ephemeris.h"
+#include "Wind.h"
+#include "Kpm.h"
+#include "Tracking.h"
+#include "Tracking.C"
+#include "OvwmL1A.h"
+#include "OvwmConfig.h"
+
+//-----------//
+// TEMPLATES //
+//-----------//
+
+// Class declarations needed for templates
+// eliminates need to include the entire header file
+class AngleInterval;
+
+template class List<AngleInterval>;
+template class List<StringPair>;
+template class List<Meas>;
+template class List<EarthPosition>;
+template class List<MeasSpot>;
+template class BufferedList<OrbitState>;
+template class List<OrbitState>;
+template class List<WindVectorPlus>;
+template class List<long>;
+template class List<off_t>;
+template class List<OffsetList>;
+template class TrackerBase<unsigned char>;
+template class TrackerBase<unsigned short>;
+template list<string>;
+template map<string,string,Options::ltstr>;
+
+const char* usage_array[] = { "<config_file>", "<input_file>",
+    "<output_file>", "[ start_frame ]", "[ end_frame]", 0};
+
+//--------------//
+// MAIN PROGRAM //
+//--------------//
+
+int
+main(
+    int    argc,
+    char*  argv[])
+{
+    //------------------------//
+    // parse the command line //
+    //------------------------//
+
+    const char* command = no_path(argv[0]);
+    if (argc != 6 && argc!=4)
+        usage(command, usage_array, 1);
+
+    int clidx = 1;
+    const char* config_file = argv[clidx++];
+    const char* input_file = argv[clidx++];
+    const char* output_file = argv[clidx++];
+        int start_frame=-1, end_frame=2;
+        if(argc==6){
+      start_frame=atoi(argv[clidx++]);
+      end_frame=atoi(argv[clidx++]);
+    }
+
+    //---------------------//
+    // read in config file //
+    //---------------------//
+
+    ConfigList config_list;
+    if (! config_list.Read(config_file))
+    {
+        fprintf(stderr, "%s: error reading sim config file %s\n",
+            command, config_file);
+        exit(1);
+    }
+
+    //--------------------//
+    // create Ovwm object //
+    // required for L1A   //
+    //--------------------//
+
+    Ovwm ovwm;
+    if (! ConfigOvwm(&ovwm, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring OVWM\n", command);
+        exit(1);
+    }
+
+    //-----------------------//
+    // create OvwmL1A object //
+    //-----------------------//
+
+    OvwmL1A l1a;
+    if (! ConfigOvwmL1A(&ovwm, &l1a, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring Pscat Level 1A\n", command);
+        exit(1);
+    }
+        
+    //---------------------//
+    // open the input file //
+    //---------------------//
+
+    if (! l1a.OpenForReading(input_file))
+    {
+        fprintf(stderr, "%s: error opening Pscat L1A file %s\n", command,
+            input_file);
+        exit(1);
+    }
+
+    //----------------------//
+    // open the output file //
+    //----------------------//
+
+    FILE* ofp = fopen(output_file, "w");
+    if (ofp == NULL)
+    {
+        fprintf(stderr, "%s: error creating output file %s\n", command,
+            output_file);
+        exit(1);
+    }       
+
+    int frame_number = 0;
+
+    //---------------------//
+    // copy desired frames //
+    //---------------------//
+
+    while (l1a.ReadDataRec())
+    {
+        if (argc==6) frame_number++;
+        if (frame_number < start_frame)
+            continue;
+        if (frame_number > end_frame)
+            break;
+
+        l1a.frame.Unpack(l1a.buffer);
+        l1a.WriteDataRecAscii(ofp);
+    }
+
+    //----------------------//
+    // close files and exit //
+    //----------------------//
+
+    l1a.Close();
+    fclose(ofp);
+    
+    return(0);
+}
---
+++ programs/ol1a_to_l1b.C
@@ -0,0 +1,370 @@
+//==============================================================//
+// Copyright (C) 1997-2001, California Institute of Technology. //
+// U.S. Government sponsorship acknowledged.                    //
+//==============================================================//
+
+//----------------------------------------------------------------------
+// NAME
+//    ol1a_to_l1b
+//
+// SYNOPSIS
+//    ol1a_to_l1b <sim_config_file>
+//
+// DESCRIPTION
+//    Simulates the OVWM 1b ground processing of Level 1A to
+//    Level 1B data.  This program converts the received power
+//    into sigma-0.
+//
+// OPTIONS
+//    None.
+//
+// OPERANDS
+//    The following operand is supported:
+//      <sim_config_file>  The sim_config_file needed listing
+//                           all input parameters, input files, and
+//                           output files.
+//
+// EXAMPLES
+//    An example of a command line is:
+//      % ol1a_to_l1b sws1b.cfg
+//
+// ENVIRONMENT
+//    Not environment dependent.
+//
+// EXIT STATUS
+//    The following exit values are returned:
+//       0  Program executed successfully
+//      >0  Program had an error
+//
+// NOTES
+//    None.
+//
+// AUTHOR
+//    James N. Huddleston (hudd@casket.jpl.nasa.gov)
+//----------------------------------------------------------------------
+
+//-----------------------//
+// Configuration Control //
+//-----------------------//
+
+static const char rcs_id[] =
+    "@(#) $Id$";
+
+//----------//
+// INCLUDES //
+//----------//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "BufferedList.h"
+#include "BufferedList.C"
+#include "List.h"
+#include "List.C"
+#include "Misc.h"
+#include "Ephemeris.h"
+#include "ConfigList.h"
+#include "OvwmL1A.h"
+#include "ConfigSim.h"
+#include "L1B.h"
+#include "OvwmL1AToL1B.h"
+#include "Tracking.h"
+#include "Tracking.C"
+#include "OvwmConfig.h"
+
+//-----------//
+// TEMPLATES //
+//-----------//
+
+// Class declarations needed for templates
+// eliminates need to include the entire header file
+class AngleInterval;
+
+template class List<AngleInterval>;
+template class BufferedList<OrbitState>;
+template class List<OrbitState>;
+template class List<StringPair>;
+template class List<Meas>;
+template class List<EarthPosition>;
+template class List<MeasSpot>;
+template class List<WindVectorPlus>;
+template class List<off_t>;
+template class List<OffsetList>;
+template class TrackerBase<unsigned char>;
+template class TrackerBase<unsigned short>;
+template list<string>;
+template map<string,string,Options::ltstr>;
+
+//-----------//
+// CONSTANTS //
+//-----------//
+
+//--------//
+// MACROS //
+//--------//
+
+//------------------//
+// TYPE DEFINITIONS //
+//------------------//
+
+//-----------------------//
+// FUNCTION DECLARATIONS //
+//-----------------------//
+
+//------------------//
+// OPTION VARIABLES //
+//------------------//
+
+//------------------//
+// GLOBAL VARIABLES //
+//------------------//
+
+const char* usage_array[] = { "<sim_config_file>", 0};
+
+//--------------//
+// MAIN PROGRAM //
+//--------------//
+
+int
+main(
+    int    argc,
+    char*  argv[])
+{
+    //------------------------//
+    // parse the command line //
+    //------------------------//
+
+    const char* command = no_path(argv[0]);
+    if (argc != 2)
+        usage(command, usage_array, 1);
+
+    int clidx = 1;
+    const char* config_file = argv[clidx++];
+
+    //---------------------//
+    // read in config file //
+    //---------------------//
+
+    ConfigList config_list;
+    if (! config_list.Read(config_file))
+    {
+        fprintf(stderr, "%s: error reading sim config file %s\n",
+            command, config_file);
+        exit(1);
+    }
+
+    //---------------------------------//
+    // create and configure spacecraft //
+    //---------------------------------//
+
+    Spacecraft spacecraft;
+    if (! ConfigSpacecraft(&spacecraft, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring spacecraft\n", command);
+        exit(1);
+    }
+
+    //---------------------------//
+    // create and configure OVWM //
+    //---------------------------//
+
+    Ovwm ovwm;
+    if (! ConfigOvwm(&ovwm, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring OVWM\n", command);
+        exit(1);
+    }
+
+    //-------------------------------------//
+    // create and configure level products //
+    //-------------------------------------//
+
+    OvwmL1A l1a;
+    if (! ConfigOvwmL1A(&ovwm, &l1a, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring Level 1A Product\n", command);
+        exit(1);
+    }
+
+    L1B l1b;
+    if (! ConfigL1B(&l1b, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring Level 1B Product\n", command);
+        exit(1);
+    }
+
+
+    //-----------------------------------------------------//
+    // configure topographic map and frequency shift table //
+    //-----------------------------------------------------//
+
+    Topo topo;
+    Topo* topo_ptr = NULL;
+    Stable stable;
+    Stable* stable_ptr = NULL;
+
+    int use_topomap;
+    config_list.ExitForMissingKeywords();
+    config_list.GetInt(USE_TOPOMAP_KEYWORD, &use_topomap);
+    if (use_topomap)
+    {
+        char* topomap_file = config_list.Get(TOPOMAP_FILE_KEYWORD);
+        if (! topo.Read(topomap_file))
+        {
+            fprintf(stderr, "%s: error reading topographic map %s\n",
+                command, topomap_file);
+            exit(1);
+        }
+        topo_ptr = &topo;
+
+        char* stable_file = config_list.Get(STABLE_FILE_KEYWORD);
+        if (! stable.Read(stable_file))
+        {
+            fprintf(stderr, "%s: error reading S Table %s\n", command,
+                stable_file);
+            exit(1);
+        }
+        int stable_mode_id;
+        config_list.GetInt(STABLE_MODE_ID_KEYWORD, &stable_mode_id);
+        stable.SetModeId(stable_mode_id);
+        stable_ptr = &stable;
+    }
+
+    //--------------------------------//
+    // create and configure ephemeris //
+    //--------------------------------//
+
+    Ephemeris ephemeris;
+    if (! ConfigEphemeris(&ephemeris, &config_list))
+    {
+        fprintf(stderr, "%s: error configuring ephemeris\n", command);
+        exit(1);
+    }
+
+    //------------//
+    // open files //
+    //------------//
+
+    if (! l1a.OpenForReading())
+    {
+        fprintf(stderr, "%s: error opening L1A file %s for reading\n", command,
+            l1a.GetInputFilename());
+        exit(1);
+    }
+    if (! l1b.OpenForWriting())
+    {
+        fprintf(stderr, "%s: error opening L1B file %s for writing\n", command,
+            l1b.GetOutputFilename());
+        exit(1);
+    }
+
+    //-----------------//
+    // conversion loop //
+    //-----------------//
+
+    int data_record_number=1;
+    OvwmL1AToL1B l1a_to_l1b;
+    if (! ConfigOvwmL1AToL1B(&l1a_to_l1b, &config_list))
+    {
+        fprintf(stderr,
+            "%s: error configuring Level 1A to Level 1B converter.\n",
+            command);
+        exit(1);
+    }
+
+        int top_of_file=1;
+    do
+    {
+
+
+        //-----------------------------//
+        // read a level 1A data record //
+        //-----------------------------//
+
+        if (! l1a.ReadDataRec())
+        {
+            switch (l1a.GetStatus())
+            {
+            case L1A::OK:        // end of file
+                break;
+            case L1A::ERROR_READING_FRAME:
+                fprintf(stderr, "%s: error reading Level 1A data\n", command);
+                exit(1);
+                break;
+            case L1A::ERROR_UNKNOWN:
+                fprintf(stderr, "%s: unknown error reading Level 1A data\n",
+                    command);
+                exit(1);
+                break;
+            default:
+                fprintf(stderr, "%s: unknown status\n", command);
+                exit(1);
+            }
+            break;        // done, exit do loop
+        }
+
+        //=======================//
+        // FOR FIRST RECORD ONLY //
+        //=======================//
+        //===========================================================//
+        // This part may be omitted if the prev_eqx_time is included //
+        // in the l1a file                                           //
+        //===========================================================//
+
+        if (top_of_file == 1)
+        {
+            top_of_file = 0;
+
+            //----------------------------------//
+            // Quickly simulate the spacecraft  //
+            // to get previous equator crossing //
+            // time.                            //
+            //----------------------------------//
+
+            SpacecraftSim spacecraft_sim;
+            if (! ConfigSpacecraftSim(&spacecraft_sim, &config_list))
+            {
+                fprintf(stderr, "%s: error configuring spacecraft simulator\n",
+                    command);
+                exit(1);
+            }
+
+            //---------------------------//
+            // set the previous Eqx time //
+            //---------------------------//
+
+            l1a.frame.Unpack(l1a.buffer);
+
+            double eqx_time =
+                spacecraft_sim.FindPrevArgOfLatTime(l1a.frame.time,
+                EQX_ARG_OF_LAT, EQX_TIME_TOLERANCE);
+            ovwm.cds.SetEqxTime(eqx_time);
+        }
+
+        //---------//
+        // convert //
+        //---------//
+
+        if (! l1a_to_l1b.Convert(&l1a, &spacecraft, &ovwm, &ephemeris,
+            topo_ptr, stable_ptr, &l1b))
+        {
+            fprintf(stderr, "%s: error converting data record %d\n", command,
+                data_record_number);
+        }
+        else if (! l1b.WriteDataRec())
+        {
+            //------------------------------//
+            // write a level 1B data record //
+            //------------------------------//
+
+            fprintf(stderr, "%s: error writing Level 1B data\n", command);
+            exit(1);
+        }
+
+        data_record_number++;
+    } while (1);
+
+    l1a.Close();
+    l1b.Close();
+
+    return (0);
+}
