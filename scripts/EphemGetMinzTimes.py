1ef9a942fd38b0cd322d994ccd272850fea9b82b
fore <>
2013-12-16 17:13:00 +0000

    *** empty log message ***

--- scripts/EphemGetMinzTimes.py
+++ scripts/EphemGetMinzTimes.py
@@ -39,75 +39,83 @@
 
 rcs_id = "$Id$"
 
-from optparse import OptionParser
 import datetime
 import struct
 import os
 import pdb
 import array
+import sys
 
-parser = OptionParser()
-parser.add_option( "-f", "--file", 
-                   action="store", type="string", dest="ephemfile")
-(options, args) = parser.parse_args()
-
-
-if not options.ephemfile or not os.path.isfile(options.ephemfile):
-  print '%s does not exist' % options.ephemfile
-  exit(1)
-
-ephem_size = os.path.getsize( options.ephemfile )
-n_ephem = ephem_size/(7.0*8.0)
-
-if round(n_ephem) != n_ephem:
-  print '%s does not have correct file size' % options.ephemfile
-  exit(1) # indicate failure and quit
-
-n_ephem = int(n_ephem)
-
-time = array.array('d')
-posx = array.array('d')
-posy = array.array('d')
-posz = array.array('d')
-velx = array.array('d')
-vely = array.array('d')
-velz = array.array('d')
-try:
-  ifp = open(options.ephemfile,"rb")
-except IOError:
-   print 'Error opening file %s' % options.ephemfile
-   exit(1)
-
-for ii in range(0,n_ephem):
-  time.fromfile(ifp,1)
-  posx.fromfile(ifp,1)
-  posy.fromfile(ifp,1)
-  posz.fromfile(ifp,1)
-  velx.fromfile(ifp,1)
-  vely.fromfile(ifp,1)
-  velz.fromfile(ifp,1)
-ifp.close()
-
-t_minz = array.array('d')
-minz_count = 0
-for ii in range(0,n_ephem-1):
-  if velz[ii] < 0 and velz[ii+1] >= 0:
-    minz_count += 1
-    # linear interpolation for time of minimum z position (velz==0)
-    t_minz.append(time[ii] + (time[ii+1]-time[ii])*(-velz[ii])/(velz[ii+1]-velz[ii]))
-
-for ii in range(t_minz.buffer_info()[1]-1):
-  start_time = t_minz[ii]
-  delta      = t_minz[ii+1]-t_minz[ii]
-  n_revs     = round( delta/5571.792 )
-  end_time   = start_time + delta / n_revs
+def EphemGetMinzTimes( ephemfile, outfile ):
+  ephem_size = os.path.getsize( ephemfile )
+  n_ephem = ephem_size/(7.0*8.0)
   
-  dt_start = datetime.datetime(1970,1,1)+datetime.timedelta(0,start_time)
-  dt_end   = datetime.datetime(1970,1,1)+datetime.timedelta(0,end_time)
+  if round(n_ephem) != n_ephem:
+    print>>sys.stderr, '%s does not have correct file size' % ephemfile
+    return 1
   
-#   print dt_start.strftime('%Y-%jT%H:%M:%S.%f'), dt_end.strftime('%Y-%jT%H:%M:%S.%f')
+  n_ephem = int(n_ephem)
   
-  print '%f' % start_time, '%f' % end_time, dt_start.strftime('%Y%j%H%M%S'), dt_end.strftime('%Y%j%H%M%S')
+  time = array.array('d')
+  posx = array.array('d')
+  posy = array.array('d')
+  posz = array.array('d')
+  velx = array.array('d')
+  vely = array.array('d')
+  velz = array.array('d')
+  
+  try:
+    ifp = open(ephemfile,"rb")
+  except IOError:
+     print>>sys.stderr, 'Error opening file %s' % ephemfile
+     return 0
+  
+  for ii in range(0,n_ephem):
+    time.fromfile(ifp,1)
+    posx.fromfile(ifp,1)
+    posy.fromfile(ifp,1)
+    posz.fromfile(ifp,1)
+    velx.fromfile(ifp,1)
+    vely.fromfile(ifp,1)
+    velz.fromfile(ifp,1)
+  ifp.close()
 
-# indicate success.
-exit(0)
+  t_minz = array.array('d')
+  minz_count = 0
+  for ii in range(0,n_ephem-1):
+    if velz[ii] < 0 and velz[ii+1] >= 0:
+      minz_count += 1
+      # linear interpolation for time of minimum z position (velz==0)
+      t_minz.append(time[ii] + (time[ii+1]-time[ii])*(-velz[ii])/(velz[ii+1]-velz[ii]))
+  
+  if outfile:
+    ofp = open(outfile,'w')
+  else:
+    ofp = sys.stdout
+    
+  for ii in range(t_minz.buffer_info()[1]):
+    dt_start   = datetime.datetime(1970,1,1)+datetime.timedelta(0,t_minz[ii])
+    print>>ofp, '%f' % t_minz[ii], dt_start.strftime('%Y%j%H%M%S')
+  
+  if outfile:
+    ofp.close()
+    
+  return 1
+
+if __name__ == '__main__':
+  from optparse import OptionParser
+  parser = OptionParser()
+  parser.add_option( "-f", "--ephemfile", action="store", type="string", dest="ephemfile")
+  parser.add_option( "-o", "--outfile",   action="store", type="string", dest="outfile")
+  (options, args) = parser.parse_args()
+  
+  if not options.ephemfile or not os.path.isfile(options.ephemfile):
+    print>>sys.stderr, '%s does not exist' % options.ephemfile
+    sys.exit(1)
+  
+  EphemGetMinzTimes( options.ephemfile, options.outfile )
+  
+  sys.exit(0)
+
+
+
--- scripts/GenRevList.py
+++ scripts/GenRevList.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 #==============================================================#
 # Copyright (C) 2013, California Institute of Technology.      #
 # U.S. Government sponsorship acknowledged.                    #
@@ -8,12 +8,19 @@
 #    GenRevList.py
 #
 # SYNOPSIS
-#    GenRevList.py -i minztimes.txt -o revlist.csv
+#    GenRevList.py -i minztimes.txt -o revlist.csv -c rdf_file
 #
 # DESCRIPTION
 #    Reads in all minimum z times, writes out a revlist containing:
-#    5 digit rev number, sum start time, sim end time.
-#    Note sim time-tags are the seconds since Jan 1st 0 hours 1970 UTC.
+#    5 digit rev number, sim start time, sim end time.
+# 
+#    Note sim time-tags are the seconds since Jan 1st 1970 0 hours UTC
+# 
+#    An example RDF file contains these three keywords:
+# 
+#    PERIOD_GUESS (min) = 92.8 
+#    TIME_TAG_REF (s)   = 1375289435.359200
+#    REV_NO_REF   (-)   = 1
 #
 # OPTIONS
 #
@@ -39,59 +46,89 @@
 
 rcs_id = "$Id$"
 
-# Functions
-def date_time_from_sim( sim_tt ):
-  return(datetime.datetime(1970,1,1)+datetime.timedelta(0,sim_tt))
-
-def ToCodeB( dt ):
-  string = dt.strftime('%Y-%jT%H:%M:%S') + '.%3.3d' % round(dt.microsecond/1000.)
-  return(string)
-
-# Main
 from optparse import OptionParser
+import rdf
 import datetime
 import numpy
 import pdb
 import sys
 import os
+import subprocess
 
-parser = OptionParser()
-parser.add_option( "-i", "--infile",  action="store", type="string", dest="infile")
-parser.add_option( "-o", "--outfile", action="store", type="string", dest="outfile")
-(options, args) = parser.parse_args()
+# Functions
+def date_time_from_sim( sim_tt ):
+  return(datetime.datetime(1970,1,1)+datetime.timedelta(0,sim_tt))
 
-if not options.infile or not os.path.isfile(options.infile):
-  sys.stderr.write('%s does not exist\n' % options.infile)
-  exit(1)
+def ToCodeB( dt ):
+  millisecs = round(dt.microsecond/1000.0)
+  dt_out = dt + datetime.timedelta(0,0,millisecs*1000-dt.microsecond)
+  string = dt_out.strftime('%Y-%jT%H:%M:%S') + '.%3.3d' % millisecs
+  return(string)
 
-if not options.outfile:
-  sys.stderr.write( 'outfile required\n')
-  exit(1)
-
-minz_tt = numpy.loadtxt( options.infile, delimiter=' ', usecols=(0,) )
-
-period_guess = 92.8*60.0
-rev_no_ref   = 1
-# tt_ref       = 1357002995.680623
-tt_ref       = 1375289435.359200
-
-ofp = open(options.outfile,'w')
-for irev in range(minz_tt.size):
+def GenRevList(config_file):
+  if not config_file or not os.path.isfile(config_file):
+    print>>sys.stderr, 'Config file, %s, does not exist' % config_file
+    return 0
   
-  this_rev_no   = rev_no_ref + round((minz_tt[irev]-tt_ref)/period_guess)
-  this_tt_start = minz_tt[irev]
+  try:
+    rdf_data = rdf.parse(config_file)
+    rev_no_ref   = rdf_data["REV_NO_REF"]
+    tt_ref       = rdf_data["TIME_TAG_REF"]
+    period_guess = rdf_data["PERIOD_GUESS"]
+    revlist      = rdf_data["REVLIST"]
+    minz_dir     = rdf_data["MINZ_TIMES_DIR"]
+  except KeyError:
+    print>>sys.stderr, 'Required keywords not found in rdf file: %s\n' % config_file
+    return 0
   
-  if irev < minz_tt.size-1 and minz_tt[irev+1]-this_tt_start < period_guess*1.1:
-    this_tt_end  = minz_tt[irev+1]
-  else:
-    this_tt_end  = this_tt_start + period_guess
+  if not os.path.isdir(minz_dir):
+    print>>sys.stderr, '%s is not a directory' % minz_dir
+    return 0
   
-  rev_no_ref = this_rev_no
-  tt_ref     = this_tt_start
+  # Make temp file for all minz times
+  tmpfile = subprocess.check_output('mktemp').rstrip('\n')
   
-  ofp.write( '%5.5d,%f,%f,%s,%s\n' % ( this_rev_no, this_tt_start, this_tt_end, 
-                                 ToCodeB(date_time_from_sim(this_tt_start)),
-                                 ToCodeB(date_time_from_sim(this_tt_end))))
-ofp.close()
+  # Cat all minz times together, sort em, remove duplicates, and write em to tempfile
+  subprocess.call('cat %s/minz_times_* | sort | uniq > %s' % ( minz_dir, tmpfile ),shell=True)
+  
+  # Load rev min z times
+  minz_tt = numpy.loadtxt( tmpfile, delimiter=' ', usecols=(0,) )
+  
+  # Delete tempfile
+  subprocess.call('rm -f %s' % tmpfile,shell=True)
+  
+  ofp = open(revlist,'w')
+  for irev in range(minz_tt.size):
+    
+    this_rev_no   = rev_no_ref + round((minz_tt[irev]-tt_ref)/period_guess)
+    this_tt_start = minz_tt[irev]
+    
+    if irev < minz_tt.size-1 and minz_tt[irev+1]-this_tt_start < period_guess*1.1:
+      this_tt_end  = minz_tt[irev+1]
+    else:
+      this_tt_end  = this_tt_start + period_guess
+    
+    rev_no_ref = this_rev_no
+    tt_ref     = this_tt_start
+    
+    ofp.write( '%5.5d,%f,%f,%s,%s\n' % ( this_rev_no, this_tt_start, this_tt_end, 
+                                         ToCodeB(date_time_from_sim(this_tt_start)),
+                                         ToCodeB(date_time_from_sim(this_tt_end))))
+  ofp.close()
+  return 1
 
-exit(0)
+
+if __name__=='__main__':
+  # Parse command line
+  parser = OptionParser()
+  parser.add_option( "-c", "--rdffile", action="store", type="string", dest="rdffile")
+  (options, args) = parser.parse_args()
+  
+  if not options.rdffile or not os.path.isfile(options.rdffile):
+    print>>sys.stderr, 'Usage: GenRevList.py -c config.rdf'
+    sys.exit(1)
+  
+  if GenRevList( options.rdffile )==0:
+    print>>sys.stderr, 'Error in GenRevList'
+    sys.exit(1)
+  sys.exit(0)
