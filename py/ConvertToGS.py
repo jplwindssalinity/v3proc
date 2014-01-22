173fb7b4e5f7bcf6f8d04223f4770d3593b111e3
fore <>
2014-01-22 18:07:34 +0000

    added code to write out the DIB format range / doppler tables

--- py/ConvertToGS.py
+++ py/ConvertToGS.py
@@ -50,6 +50,8 @@ import rdf
 import numpy
 import subprocess
 import time_funcs
+import Tracking
+import pm.database.revs
 from pm.utils.helper import find_files
 
 def ConvertEphemToGS( config_file ):
@@ -63,14 +65,13 @@ def ConvertEphemToGS( config_file ):
     ephem_gs_gaps_dir = rdf_data["GS_EPHEM_GAPFILLED"]
     att_gs_dir        = rdf_data["GS_ATT_DIR"]
     att_gs_gaps_dir   = rdf_data["GS_ATT_GAPFILLED"]    
-    revlist           = rdf_data["REVLIST"]
+    revlist_db        = rdf_data["REVLIST_DB"]
   except KeyError:
     print>>sys.stderr, 'Required keywords not found in rdf file: %s\n' % \
-           options.rdffile
+           config_file
     return(0)
   
-  rev_no      = numpy.loadtxt( revlist, delimiter=',', usecols=(0,) )
-  rev_t_start = numpy.loadtxt( revlist, delimiter=',', usecols=(1,) )
+  rev_db = pm.database.revs.RevDataBase(revlist_db)
   
   # Convert the Gap-free ephemeris/quaternion files
   for ephem_file in find_files(ephem_dir,"RS_EPHEM_*"):
@@ -82,9 +83,11 @@ def ConvertEphemToGS( config_file ):
     if os.path.isfile(ephem_file) and os.path.isfile(quats_file) and \
        not ( os.path.isfile(out_ephem_file) and os.path.isfile(out_att_file)):
       
-      idx = rev_no==int(revtag)
-      leap_secs = time_funcs.leap_seconds( 
-        time_funcs.date_time_from_sim( rev_t_start[idx][0] ) )
+      try:
+        leap_secs = time_funcs.leap_seconds(rev_db[int(revtag)].start)
+      except TypeError:
+        print>>sys.stderr,'Rev: %s is not in database!'%revtag
+        return(0)
       
       subprocess.call('ephem_to_gs -o %s -l %d %s' % \
                      (out_ephem_file, leap_secs, ephem_file), shell=True )
@@ -102,9 +105,11 @@ def ConvertEphemToGS( config_file ):
     if( os.path.isfile(ephem_file) and os.path.isfile(quats_file) and 
         not (os.path.isfile(out_ephem_file) and os.path.isfile(out_att_file))):
       
-      idx = rev_no==int(revtag)
-      leap_secs = time_funcs.leap_seconds( 
-        time_funcs.date_time_from_sim( rev_t_start[idx][0] ) )
+      try:
+        leap_secs = time_funcs.leap_seconds(rev_db[int(revtag)].start)
+      except TypeError:
+        print>>sys.stderr,'Rev: %s is not in database!'%revtag
+        return(0)
       
       subprocess.call('ephem_to_gs -o %s -l %d %s' % \
                      (out_ephem_file, leap_secs, ephem_file), shell=True )
@@ -113,6 +118,50 @@ def ConvertEphemToGS( config_file ):
                      (out_att_file, leap_secs, quats_file), shell=True )
   
   return(1)
+
+def ConvertRangeDopplerToDIB(config_file):
+  if not config_file or not os.path.isfile(config_file):
+    print>>sys.stderr, '%s not right' % config_file
+    return 0
+  try:
+    rdf_data = rdf.parse(config_file)
+    rng_in_dir  = rdf_data['RANGE_TABLE_DIR']
+    dop_in_dir  = rdf_data['DOPPLER_TABLE_DIR']
+    rng_dib_dir = rdf_data['DIB_RANGE_TABLE_DIR']
+    dop_dib_dir = rdf_data['DIB_DOPPLER_TABLE_DIR']
+    revlist_db  = rdf_data["REVLIST_DB"]
+  except KeyError:
+    print>>sys.stderr, 'Required keywords not found in rdf file: %s\n' % \
+           config_file
+    return(0)
+  
+  rev_db = pm.database.revs.RevDataBase(revlist_db)
+  for rev in rev_db:
+    rev_no = rev.rev
+    
+    if rev_no < 0:
+      continue
+    
+    for ib in range(2):
+      # Convert range tables
+      rng_in_table  = os.path.join(rng_in_dir,"RGC_%5.5d.%d"%(rev_no,ib+1))
+      rng_dib_table = os.path.join(rng_dib_dir,"RGC_%5.5d.%d"%(rev_no,ib+1))
+      
+      if os.path.isfile(rng_in_table) and not os.path.isfile(rng_dib_table):
+        rng_track = Tracking.RangeTracker()
+        rng_track.ReadSimBinary(rng_in_table)
+        rng_track.CreateDIBData(ib,rev_no)
+        rng_track.WriteDIBBinary(rng_dib_table)
+      
+      # Convert doppler tables
+      dop_in_table  = os.path.join(dop_in_dir,"DTC_%5.5d.%d"%(rev_no,ib+1))
+      dop_dib_table = os.path.join(dop_dib_dir,"DTC_%5.5d.%d"%(rev_no,ib+1))
+      
+      if os.path.isfile(dop_in_table) and not os.path.isfile(dop_dib_table):
+        dop_track = Tracking.DopplerTracker()
+        dop_track.ReadSimBinary(dop_in_table)
+        dop_track.CreateDIBData(ib,rev_no)
+        dop_track.WriteDIBBinary(dop_dib_table)
 
 def ConvertRangeDopplerToGS(config_file):
   if not config_file or not os.path.isfile(config_file):
@@ -124,15 +173,15 @@ def ConvertRangeDopplerToGS(config_file):
     dop_in_dir = rdf_data['DOPPLER_TABLE_DIR']
     rng_gs_dir = rdf_data['GS_RANGE_TABLE_DIR']
     dop_gs_dir = rdf_data['GS_DOPPLER_TABLE_DIR']
-    revlist    = rdf_data["REVLIST"]
+    revlist_db = rdf_data["REVLIST_DB"]
   except KeyError:
     print>>sys.stderr, 'Required keywords not found in rdf file: %s\n' % \
-           options.rdffile
+           config_file
     return(0)
   
-  rev_nos = numpy.loadtxt(revlist, delimiter=',', usecols=(0,))
-  
-  for rev_no in rev_nos:
+  rev_db = pm.database.revs.RevDataBase(revlist_db)
+  for rev in rev_db:
+    rev_no        = rev.rev
     rng_in_table1 = '%s/RGC_%5.5d.1' % (rng_in_dir, rev_no)
     rng_in_table2 = '%s/RGC_%5.5d.2' % (rng_in_dir, rev_no)
     rng_gs_table  = '%s/RGC_%5.5d'   % (rng_gs_dir, rev_no)
@@ -157,6 +206,7 @@ def ConvertToGS( config_file ):
     return 0
   ConvertEphemToGS(config_file)
   ConvertRangeDopplerToGS(config_file)
+  ConvertRangeDopplerToDIB(config_file)
   return(1)
 
 def main():
