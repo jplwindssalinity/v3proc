7320edd9cc8600d57e986c1b389cd2686128657b
fore <>
2014-02-04 21:10:11 +0000

    *** empty log message ***

--- py/util/rot.py
+++ py/util/rot.py
@@ -9,7 +9,7 @@ Notes:
 1: quaternions are in [w,x,y,z] order.
 2: This is the rotation order used in the QuikSCAT SDS.
 3: Makes heavy use of the cspice libraries to not reinvent wheels and stuff.
-4: By convetion spice consdiers rotations of the coordinate system, 
+4: By convention spice considers rotations of the coordinate system, 
 not of vectors.  We use opposite convention for attitude rotations.
 5: Read spice docs: http://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/req/rotation.html
 """
@@ -17,6 +17,8 @@ __version__ = '$Id$'
 
 import spice
 import numpy as np
+
+DEFAULT_ORDER = (2,1,3)
 
 def q2m(quat):
     """Quaternion to rotation matrix"""
@@ -26,28 +28,28 @@ def m2q(mat):
     """Rotation matrix to Quaternion"""
     return spice.m2q(mat)
 
-def m2eul(mat,order=(2,1,3)):
+def m2eul(mat,order=DEFAULT_ORDER):
     """Converts a rotation matrix to Euler angles"""
     return spice.m2eul(mat,*order)
 
-def eul2m(angles,order=(2,1,3)):
+def eul2m(angles,order=DEFAULT_ORDER):
     """Converts Euler angles to a rotation matrix."""
     # Why can't I use (*angles,*order)??  De-uglify this
     return spice.eul2m(angles[0],angles[1],angles[2],*order)
 
-def q2eul(quat,order=(2,1,3)):
+def q2eul(quat,order=DEFAULT_ORDER):
     """Converts a quaterion = [w,x,y,z] to Euler angles"""
     return m2eul(q2m(quat),order)
 
-def eul2q(angles,order=(2,1,3)):
+def eul2q(angles,order=DEFAULT_ORDER):
     """Converts Euler angles to a quaternion"""
     return m2q(eul2m(angles,order))
 
-def q2att(quat,order=(2,1,3)):
+def q2att(quat,order=DEFAULT_ORDER):
     """Converts Euler angles to attitude angles (opposite sign)"""
     return [-item for item in q2eul(quat,order)]
 
-def att2q(angles,order=(2,1,3)):
+def att2q(angles,order=DEFAULT_ORDER):
     """Converts attitude angles to Euler angles (opposite sign)"""
     new_angles = [-item for item in angles]
     return eul2q(new_angles,order)
