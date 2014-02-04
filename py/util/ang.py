b69d83ffa427aeb8fc4f10172d5b5ef52296132b
fore <>
2014-02-04 21:38:50 +0000

    *** empty log message ***

---
+++ py/util/ang.py
@@ -0,0 +1,7 @@
+
+__version__ = '$Id$'
+def wrap(angle):
+    """Wraps an angle to [-180,180) interval"""
+    while angle >= 180.0: angle -= 360
+    while angle < -180.0: angle += 360
+    return angle
