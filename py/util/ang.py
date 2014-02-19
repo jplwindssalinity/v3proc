677f60cc7e0b8ff0235618c6c8fb9a2991233f05
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2014-02-19 08:04:15 -0800

    added code so wrap works with iterables and scalars

--- py/util/ang.py
+++ py/util/ang.py
@@ -1,6 +1,16 @@
+import numpy as np
 
 def wrap(angle):
     """Wraps an angle to [-180,180) interval"""
-    while angle >= 180.0: angle -= 360
-    while angle < -180.0: angle += 360
+    try:
+        angle = _wrap(angle)
+    except TypeError:
+        while angle >= 180.0: angle -= 360
+        while angle < -180.0: angle += 360
     return angle
+    
+def _wrap(angles):
+    """Wraps an iterable container of angles to [-180,180) interval"""
+    angles = np.asarray([wrap(item) for item in angles])
+    return angles
+    
