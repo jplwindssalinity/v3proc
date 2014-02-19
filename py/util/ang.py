5486e905e206f589ea58267273f4d9cb20087cb2
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2014-02-19 08:07:44 -0800

    better version

--- py/util/ang.py
+++ py/util/ang.py
@@ -1,16 +1,21 @@
 import numpy as np
 
-def wrap(angle):
-    """Wraps an angle to [-180,180) interval"""
+def wrap(angles):
+    """
+    Wraps scalar angle or iterable container of angles to [-180,180) 
+    interval.
+    """
     try:
-        angle = _wrap(angle)
+        angles = np.asarray([_wrap(item) for item in angles])
     except TypeError:
-        while angle >= 180.0: angle -= 360
-        while angle < -180.0: angle += 360
+        angles = _wrap(angles)
+    return angles
+
+
+def _wrap(angle):
+    """Wraps a scalar angle to [-180,180) interval."""
+    while angle >= 180.0: angle -= 360
+    while angle < -180.0: angle += 360
     return angle
     
-def _wrap(angles):
-    """Wraps an iterable container of angles to [-180,180) interval"""
-    angles = np.asarray([wrap(item) for item in angles])
-    return angles
     
