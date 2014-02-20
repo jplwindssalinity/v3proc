cf49cd49756761fa4eb51daed217b73615b66140
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2014-02-20 06:51:04 -0800

    comments

--- py/util/ang.py
+++ py/util/ang.py
@@ -2,9 +2,10 @@ import numpy as np
 
 def wrap(angles):
     """
-    Wraps scalar angle or iterable container of angles to [-180,180) 
+    Wraps scalar angle or iterable container of angles to [-180,180)
     interval.
     """
+    ## Not sure how to make the out class the same class as the in class???
     try:
         angles = np.asarray([_wrap(item) for item in angles])
     except TypeError:
@@ -17,5 +18,4 @@ def _wrap(angle):
     while angle >= 180.0: angle -= 360
     while angle < -180.0: angle += 360
     return angle
-    
-    
+
