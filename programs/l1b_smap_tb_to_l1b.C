b4ea8c2a977623cfa3ce53f1e757e82972789ba0
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2015-02-17 18:06:13 +0000

    will need to be an l1b converter, not direct to l2a

--- programs/l1b_smap_tb_to_l1b.C
+++ programs/l1b_smap_tb_to_l1b.C
@@ -5,6 +5,7 @@
 
 #define L1B_TB_LORES_ASC_FILE_KEYWORD "L1B_TB_LORES_ASC_FILE"
 #define L1B_TB_LORES_DEC_FILE_KEYWORD "L1B_TB_LORES_DEC_FILE"
+#define SURFACE_TEMP_ANC_FILE_KEYWORD "SURFACE_TEMP_ANC_FILE"
 
 //----------//
 // INCLUDES //
@@ -60,6 +61,26 @@ template class std::map<string,string,Options::ltstr>;
 
 const char* usage_array[] = {"config_file", NULL};
 
+int determine_l1b_sizes(char* l1b_tbfiles[], int nframes[], int nfootprints[]){
+
+    for(int ipart=0; ipart<2; ++ipart) {
+        hid_t id = H5Fopen(l1b_tbfiles[ipart], H5F_ACC_RDONLY, H5P_DEFAULT);
+        if(id<0) return(0);
+
+        hsize_t dims[2];
+        H5T_class_t class_id;
+        size_t type_size;
+
+        if(H5LTget_dataset_info(id, "/Brightness_Temperature/tb_h",
+            dims, &class_id, &type_size))
+            return(0);
+
+        nframes[ipart] = dims[0];
+        nfootprints[ipart] = dims[1];
+    }
+    return(1);
+}
+
 int main(int argc, char* argv[]){
     //-----------//
     // variables //
@@ -76,13 +97,40 @@ int main(int argc, char* argv[]){
     }
 
     char* l1b_tbfiles[2] = {NULL, NULL};
+
     // These ones are required
     config_list.ExitForMissingKeywords();
     l1b_tbfiles[0] = config_list.Get(L1B_TB_LORES_ASC_FILE_KEYWORD);
     l1b_tbfiles[1] = config_list.Get(L1B_TB_LORES_DEC_FILE_KEYWORD);
     printf("l1b_tbfiles: %s %s\n", l1b_tbfiles[0], l1b_tbfiles[1]);
 
+    // Determine number of frames in both portions of orbit
+    int nframes[2] = {0, 0};
+    int nfootprints[2] = {0, 0};
+
+    if(!determine_l1b_sizes(l1b_tbfiles, nframes, nfootprints)) {
+        fprintf(stderr, "Unable to determine array sizes.");
+        exit(1);
+    }
+
+    // Iterate over ascending / decending portions of orbit
+    for(int ipart = 0; ipart < 2; ++ipart){
+        hid_t id = H5Fopen(l1b_tbfiles[ipart], H5F_ACC_RDONLY, H5P_DEFAULT);
+        // do stuff... 
+        // meas->value = emissivity == (TB/Tphys)
+        // meas->EnSlice = TB*KB
+        H5Fclose(id);
+    }
 
     return(0);
 }
+
+
+
+
+
+
+
+
+
 
