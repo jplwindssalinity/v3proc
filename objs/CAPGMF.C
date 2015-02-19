e0eea8c4eab4b66583d22fd6af0fa72793c8cea0
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2015-02-19 22:09:40 +0000

    added CAPGMF class

---
+++ objs/CAPGMF.C
@@ -0,0 +1,17 @@
+#include "CAPGMF.h"
+
+CAPGMF::CAPGMF() {
+    return;
+}
+
+CAPGMF::~CAPGMF() {
+    return;
+}
+
+int CAPGMF::tb_flat(double sss, double sst, double inc, double frequency,
+                    int model, double* Tv, double* Th) {
+
+    // Stub for flat surface emmissivity model
+    return(1);
+}
+
---
+++ objs/CAPGMF.h
@@ -0,0 +1,8 @@
+
+class CAPGMF {
+    public:
+        CAPGMF();
+        ~CAPGMF();
+        int tb_flat(double sss, double sst, double inc, double frequency,
+                    int model, double* Tv, double* Th);
+};
