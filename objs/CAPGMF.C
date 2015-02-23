20ed15ca51de287951b3c3d132374aea20af312e
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2015-02-23 18:38:26 +0000

    added methods to read TB flat look up table

--- objs/CAPGMF.C
+++ objs/CAPGMF.C
@@ -1,15 +1,61 @@
 #include <math.h>
 #include "CAPGMF.h"
-
+#include "Meas.h"
+#include "Array.h"
 
 // Will have two look up tables for passive:
 // flat TB as a function of sss, sst, inc, pol
 // excess surface emmisivity as a function of spd, dir, swh, inc, pol
 
-CAPGMF::CAPGMF() {
+CAPGMF::CAPGMF() :_tbflat(NULL) {
     return;
 }
 
 CAPGMF::~CAPGMF() {
+    _Deallocate();
     return;
+}
+
+int CAPGMF::ReadFlat(const char*  filename) {
+
+    if(!_tbflat)
+        _Allocate();
+
+    FILE* ifp = fopen(filename, "r");
+
+    for(int met_idx = 0; met_idx < _metCount; ++met_idx) {
+        for(int sss_idx = 0; sss_idx < _sssCount; ++sss_idx) {
+            for(int sst_idx = 0; sst_idx < _sstCount; ++sst_idx) {
+                float values[_incCount];
+                if(fread(&values, sizeof(float), _incCount, ifp) != _incCount) {
+                    fclose(ifp);
+                    return(0);
+                }
+                for(int inc_idx = 0; inc_idx < _incCount; ++inc_idx) {
+                    _tbflat[inc_idx][sst_idx][sss_idx][met_idx] = values[inc_idx];
+                }
+            }
+        }
+    }
+    fclose(ifp);
+    return(1);
+}
+
+int CAPGMF::_Allocate() {
+    _tbflat = (float ****)make_array(
+        sizeof(float), 4, _metCount, _sssCount, _sstCount, _incCount);
+
+    if(!_tbflat)
+        return(0);
+
+    return(1);
+}
+
+int CAPGMF::_Deallocate() {
+    if (_tbflat) {
+        free_array(
+            (void *)_tbflat, 4, _metCount, _sssCount, _sstCount, _incCount);
+        _tbflat = NULL;
+    }
+    return(1);
 }
--- objs/CAPGMF.h
+++ objs/CAPGMF.h
@@ -1,7 +1,42 @@
+#include "Meas.h"
+#include "Array.h"
 
 class CAPGMF {
-    public:
-        CAPGMF();
-        ~CAPGMF();
+public:
+    CAPGMF();
+    ~CAPGMF();
 
+    int ReadFlat(const char* filename);
+    int ReadRough(const char* filename);
+
+    int GetTBFlat(
+        Meas::MeasTypeE met, float inc, float sst, float sss, float* tbflat);
+
+    int GetDTB(
+        Meas::MeasTypeE met, float inc, float wspd, float relazi, float* dtb);
+
+    int GetTB(
+        Meas::MeasTypeE met, float inc, float sst, float sss, float wspd,
+        float relazi, float* tb);
+
+protected:
+
+    int _Allocate();
+    int _Deallocate();
+
+    static const int _incCount = 11;
+    static const float _incMin = 35;
+    static const float _incStep = 1.0;
+
+    static const int _sstCount = 43;
+    static const float _sstMin = -2.0;
+    static const float _sstStep = 1.0;
+
+    static const int _sssCount = 41;
+    static const float _sssMin = 0.0;
+    static const float _sssStep = 1.0;
+
+    static const int _metCount = 2;
+
+    float**** _tbflat;
 };
