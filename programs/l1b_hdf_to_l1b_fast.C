db84ef4ac050a6985abe2dc9d7f5f0a2ea9c65ef
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2016-08-26 11:46:53 -0700

    fixup on the totally messed up merge commit of develp->land-frac

--- programs/l1b_hdf_to_l1b_fast.C
+++ programs/l1b_hdf_to_l1b_fast.C
@@ -53,6 +53,8 @@ static const char rcs_id[] =
 
 #define QS_LANDMAP_FILE_KEYWORD         "QS_LANDMAP_FILE"
 #define QS_ICEMAP_FILE_KEYWORD          "QS_ICEMAP_FILE"
+#define USE_COMPOSITING_KEYWORD         "USE_COMPOSITING"
+#define USE_FOOTPRINTS_KEYWORD          "USE_FOOTPRINTS"
 #define DO_COASTAL_PROCESSING_KEYWORD   "DO_COASTAL_PROCESSING"
 #define COASTAL_METHOD_KEYWORD          "COASTAL_PROCESSING_METHOD"
 #define COASTAL_DISTANCE_FILE_KEYWORD   "COASTAL_DISTANCE_FILE"
@@ -61,8 +63,6 @@ static const char rcs_id[] =
 #define LCR_THRESHOLD_FLAG_KEYWORD      "LCR_THRESHOLD_FLAG"
 #define LCRES_THRESHOLD_FLAG_KEYWORD    "LCRES_THRESHOLD_FLAG"
 #define LCRES_THRESHOLD_CORR_KEYWORD    "LCRES_THRESHOLD_CORR"
-#define USE_COMPOSITING_KEYWORD         "USE_COMPOSITING"
-#define USE_FOOTPRINTS_KEYWORD          "USE_FOOTPRINTS"
 
 //----------//
 // INCLUDES //
@@ -72,7 +72,6 @@ static const char rcs_id[] =
 #include <unistd.h>
 #include <stdlib.h>
 #include <string>
-#include <fstream>
 #include "Misc.h"
 #include "Ephemeris.h"
 #include "ConfigList.h"
@@ -147,12 +146,12 @@ int read_SDS( int32 sd_id, const char* sds_name, void* data_buffer )
     if( SDgetinfo( sds_id, sds_name1, &rank, &dimsizes[0], &data_type, &num_attrs ) )
       return(0);
     
-//     printf("in * sds_index, sds_id: %d %d\n",sds_index, sds_id);
-//     printf("in * sds_name: %s\n",sds_name1);
-//     printf("rank:     %d\n",rank);
-//     printf("dimsizes: %d %d %d\n",dimsizes[0],dimsizes[1],dimsizes[2]);
-//     printf("data_type: %d\n",data_type);
-//     printf("num_attrs: %d\n",num_attrs);
+    //printf("in * sds_index, sds_id: %d %d\n",sds_index, sds_id);
+    //printf("in * sds_name: %s\n",sds_name1);
+    //printf("rank:     %d\n",rank);
+    //printf("dimsizes: %d %d %d\n",dimsizes[0],dimsizes[1],dimsizes[2]);
+    //printf("data_type: %d\n",data_type);
+    //printf("num_attrs: %d\n",num_attrs);
         
     for( int ii = 0; ii < 32; ++ii )
     {
@@ -497,9 +496,10 @@ main(
     char*        attenmap_file   = NULL;
     char*        qslandmap_file  = NULL;
     char*        qsicemap_file   = NULL;
+    
     int          compute_land_frac = 0;
     float        sigma0_adjust = 0;
-
+    
     ConfigList   config_list;
     AttenMap     attenmap;
     QSLandMap    qs_landmap;
@@ -547,13 +547,22 @@ main(
     //----------------------------------//
     // check for config file parameters //
     //----------------------------------//
-    config_list.ExitForMissingKeywords();
-    ConfigKp(&kp, &config_list);
-
+    config_list.DoNothingForMissingKeywords();
+        
+    if (! ConfigKp(&kp, &config_list)) {
+      fprintf(stderr, "%s: error configuring Kp\n", command);
+      exit(1);
+    }
+    
     // Check for QS_LANDMAP and QS_ICEMAP keywords
     qslandmap_file = config_list.Get(QS_LANDMAP_FILE_KEYWORD);
     qsicemap_file  = config_list.Get(QS_ICEMAP_FILE_KEYWORD);
-
+    
+    if( qslandmap_file == NULL || qsicemap_file == NULL ) {
+      fprintf(stderr,"%s: Must specify QS_LANDMAP_FILE and QS_ICEMAP_FILE in config!\n",command);
+      exit(1);
+    }
+    
     int do_coastal_processing = 0;
     CoastalProcMethod coastal_method = NONE;
     float land_frac_threshold = 1;
@@ -631,7 +640,7 @@ main(
 
         config_list.DoNothingForMissingKeywords();
     }
-
+    
     fprintf(stdout,"%s: Using QS LANDMAP %s\n",command,qslandmap_file);
     fprintf(stdout,"%s: Using QS ICEMAP %s\n",command,qsicemap_file);
     qs_landmap.Read( qslandmap_file );
@@ -688,13 +697,12 @@ main(
     printf("%s: Use compositing flag: %d\n", command, do_composite);
 
     int do_footprint = 0;
-    if( !config_list.GetInt(USE_FOOTPRINTS_KEYWORD, &do_footprint) ) {
-      fprintf(stderr,"%s: Error reading from config file\n", command);
-      exit(1);
-    }
+    config_list.DoNothingForMissingKeywords();
+    config_list.GetInt(USE_FOOTPRINTS_KEYWORD, &do_footprint);
+    config_list.ExitForMissingKeywords();
 
     // Check for Coastal maps & configure landmap if commanded
-    if(coastal_method != NONE) {
+    if( compute_land_frac ) {
       if(!ConfigLandMap(&lmap,&config_list)) {
         fprintf(stderr,"%s: Error configuring LandMap\n",command);
         exit(1);
@@ -704,12 +712,12 @@ main(
         exit(1);
       }
     }
-
+    
     //
     //------Done reading from config file.-------------------------------------
     //
 
-    L1B l1b;
+    L1B l1b;    
     // Prepare to write
     if (l1b.OpenForWriting(output_file) == 0) {
         fprintf(stderr, "%s: cannot open l1b file %s for output\n", command, output_file);
@@ -1231,82 +1239,57 @@ main(
                delete new_meas;
                continue;
              }
-
-           // Stick this meas in the measSpot
-           new_meas_spot->Append(new_meas);
-        }
-
-        // Compute land fractions for various coastal_methos
-        if(coastal_method != NONE) {
-
-          float this_frequency_shift = (float)frequency_shift[pulse_ind];
-          double spot_lon = double(cell_lon[pulse_ind])*dtr;
-          double spot_lat = double(cell_lat[pulse_ind])*dtr;
-
-          new_meas_spot->ComputeLandFraction( &lmap, coast_dist,
-                                              &antenna, this_frequency_shift,
-                                              spot_lon, spot_lat, lcres_accum,
-                                              lcres_map_tiles);
-
-          // Loop through Meas in this MeasSpot, decide to discard or not.
-          Meas* this_meas = new_meas_spot->GetHead();
-          while(this_meas) {
-            int remove_it = 0;
-
-            // Land contamination ratio
-            float lcr = this_meas->bandwidth;
-            float lcres = this_meas->txPulseWidth;
-
-            switch (coastal_method) {
-                case LCR:
-                    if(lcr > land_frac_threshold) remove_it = 1;
-                    break;
-
-                case LCRES_FLAG:
-                    if(lcres > lcres_thresh_flag) remove_it = 1;
-                    break;
-
-                case LCRES_CORR:
-                    if(lcres > lcres_thresh_corr) {
-                        remove_it = 1;
-                    } else {
-                        double sos = this_meas->EnSlice / this_meas->XK;
-                        this_meas->value = (this_meas->value - 1.75 * lcres);
-                        this_meas->C += 0.5 * pow(10*lcres/sos, 2);
-
-                        if(fabs(this_meas->value) > 1.0e5)
-                            remove_it = 1;
-                    }
-                    break;
-
-            }
-
-            if(remove_it) {
-              this_meas = new_meas_spot->RemoveCurrent();
-              delete this_meas;
-              this_meas = new_meas_spot->GetCurrent();
-            } else {
-              // unset land flag
-              this_meas->landFlag &= ~(1<<0);
-              this_meas = new_meas_spot->GetNext();
-
+           
              // Stick this meas in the measSpot
              new_meas_spot->Append(new_meas);
           }
-        
-          // Optionally compute land fractions
-          if( compute_land_frac ) {
+
+          // Compute land fractions for various coastal_methos
+          if(coastal_method != NONE && !do_footprint) {
+
             float this_frequency_shift = (float)frequency_shift[pulse_ind];
             double spot_lon = double(cell_lon[pulse_ind])*dtr;
             double spot_lat = double(cell_lat[pulse_ind])*dtr;
 
-            new_meas_spot->ComputeLandFraction( &lmap, &qs_landmap, 
+            new_meas_spot->ComputeLandFraction( &lmap, coast_dist,
                                                 &antenna, this_frequency_shift,
-                                                spot_lon, spot_lat);
-          
+                                                spot_lon, spot_lat, lcres_accum,
+                                                lcres_map_tiles);
+
+            // Loop through Meas in this MeasSpot, decide to discard or not.
             Meas* this_meas = new_meas_spot->GetHead();
-            while( this_meas ) {
-              if( this_meas->bandwidth > land_frac_threshold ) {
+            while(this_meas) {
+              int remove_it = 0;
+
+              // Land contamination ratio
+              float lcr = this_meas->bandwidth;
+              float lcres = this_meas->txPulseWidth;
+
+              switch (coastal_method) {
+                  case LCR:
+                      if(lcr > land_frac_threshold) remove_it = 1;
+                      break;
+
+                  case LCRES_FLAG:
+                      if(lcres > lcres_thresh_flag) remove_it = 1;
+                      break;
+
+                  case LCRES_CORR:
+                      if(lcres > lcres_thresh_corr) {
+                          remove_it = 1;
+                      } else {
+                          double sos = this_meas->EnSlice / this_meas->XK;
+                          this_meas->value = (this_meas->value - 1.75 * lcres);
+                          this_meas->C += 0.5 * pow(10*lcres/sos, 2);
+
+                          if(fabs(this_meas->value) > 1.0e5)
+                              remove_it = 1;
+                      }
+                      break;
+
+              }
+
+              if(remove_it) {
                 this_meas = new_meas_spot->RemoveCurrent();
                 delete this_meas;
                 this_meas = new_meas_spot->GetCurrent();
@@ -1317,8 +1300,9 @@ main(
               }
             }
           }
-        }
 
+
+        }
         // Stick this measSpot in the spotList.
         l1b.frame.spotList.Append(new_meas_spot);
       }
@@ -1346,10 +1330,7 @@ main(
         printf("Wrote %d frames of %d\n", i_frame, num_l1b_frames);
         //if( i_frame == 1000 ) exit(1);
     }
-
-    if(coastal_method == LCRES_ACCUM)
-        lcres_accum->Write(config_list.Get(LCRES_ACCUM_FILE_KEYWORD));
-
+    
     fclose(eph_fp);
     
     //Free the calloc-ed arrays.
@@ -1395,6 +1376,8 @@ main(
       fprintf(stderr,"ERROR closing hdf file %s.\n",l1b_hdf_file);
       exit(1);
     }
+
+    
     
     return 0;
     
